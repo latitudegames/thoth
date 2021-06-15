3eed65bf548b9687597686f52cf674e4fb3c8009
Michael Sharpe <m.sharpe@project89.org>
2021-06-15 13:53:55 -0700

    Render in main app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import init from "./editor";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello world</p>
+      <h1>THOTH</h1>
+      <h2>Multishot Builder</h2>
+      <div style={{ textAlign: "left", width: "100vw", height: "70vh" }}>
+        <div ref={(el) => init(el)} />
+      </div>
     </div>
   );
 }
