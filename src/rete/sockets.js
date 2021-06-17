ffdea8c97d7162c8f4ce719f8925e3c9cec054ca
Michael Sharpe <m.sharpe@project89.org>
2021-06-17 16:21:05 -0700

    Add boolean socket

--- src/rete/sockets.js
+++ src/rete/sockets.js
@@ -1,6 +1,7 @@
 import Rete from "rete";
 
 export const numSocket = new Rete.Socket("Number");
+export const booleanSocket = new Rete.Socket("Boolean");
 export const stringSocket = new Rete.Socket("String");
 export const actionSocket = new Rete.Socket("Action");
 export const actionTypeSocket = new Rete.Socket("ActionType");
