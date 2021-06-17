b1841d15983d0f33b250cfd5c479d1d60886d3a9
Michael Sharpe <m.sharpe@project89.org>
2021-06-17 15:53:10 -0700

    Add entity socket

--- src/rete/sockets.js
+++ src/rete/sockets.js
@@ -4,3 +4,4 @@ export const numSocket = new Rete.Socket("Number");
 export const stringSocket = new Rete.Socket("String");
 export const actionSocket = new Rete.Socket("Action");
 export const actionTypeSocket = new Rete.Socket("ActionType");
+export const entitySocket = new Rete.Socket("Entities");
