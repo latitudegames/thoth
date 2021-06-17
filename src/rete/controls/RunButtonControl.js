0070d5c3c7d5ccce4a09891e64949f0e4d4f11a4
Michael Sharpe <m.sharpe@project89.org>
2021-06-17 15:18:16 -0700

    Run Button Control

---
+++ src/rete/controls/RunButtonControl.js
@@ -0,0 +1,23 @@
+import { Control } from "rete";
+
+const ReactRunButton = (props) => {
+  const onButton = () => {
+    props.emitter.trigger("process");
+  };
+
+  return <button onClick={onButton}>RUN</button>;
+};
+
+export class RunButtonControl extends Control {
+  constructor({ key, emitter, defaultDisplay = "" }) {
+    super(key);
+    this.render = "react";
+    this.key = key;
+    this.component = ReactRunButton;
+
+    // we define the props that are passed into the rendered react component here
+    this.props = {
+      emitter,
+    };
+  }
+}
