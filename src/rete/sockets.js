b8e90ae6bdab92b62a11b8d049dd63d1f09c3bd2
Michael Sharpe <m.sharpe@project89.org>
2021-06-17 15:31:50 -0700

    Add action type socket

--- src/rete/sockets.js
+++ src/rete/sockets.js
@@ -3,3 +3,4 @@ import Rete from "rete";
 export const numSocket = new Rete.Socket("Number");
 export const stringSocket = new Rete.Socket("String");
 export const actionSocket = new Rete.Socket("Action");
+export const actionTypeSocket = new Rete.Socket("ActionType");
