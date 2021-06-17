45cb48ef25162e32f6c284f3ec18a5cbbeec0572
Michael Sharpe <m.sharpe@project89.org>
2021-06-17 10:57:15 -0700

    add seralize button to get editor json

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import "./App.css";
 import init from "./rete/editor";
 
 function App() {
+  let editor;
+
+  const buildEditor = async (el) => {
+    editor = await init(el);
+  };
+
+  const serialize = () => {
+    console.log(JSON.stringify(editor.toJSON()));
+  };
+
   return (
     <div className="App">
       <h1>THOTH</h1>
       <h2>Multishot Builder</h2>
+      <button onClick={serialize}>serialize</button>
       <div style={{ textAlign: "left", width: "100vw", height: "70vh" }}>
-        <div ref={(el) => init(el)} />
+        <div ref={(el) => buildEditor(el)} />
       </div>
     </div>
   );
