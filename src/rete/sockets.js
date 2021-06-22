98f624e8684f32fbffe7bd3705761ac538abc21f
Michael Sharpe <m.sharpe@project89.org>
2021-06-22 15:37:19 -0700

    Create an 'any' socket

--- src/rete/sockets.js
+++ src/rete/sockets.js
@@ -1,8 +1,23 @@
 import Rete from "rete";
 
+export const anySocket = new Rete.Socket("Any type");
 export const numSocket = new Rete.Socket("Number");
 export const booleanSocket = new Rete.Socket("Boolean");
 export const stringSocket = new Rete.Socket("String");
 export const actionSocket = new Rete.Socket("Action");
 export const actionTypeSocket = new Rete.Socket("ActionType");
 export const entitySocket = new Rete.Socket("Entities");
+
+const sockets = [
+  numSocket,
+  booleanSocket,
+  stringSocket,
+  actionSocket,
+  actionTypeSocket,
+  entitySocket,
+];
+
+sockets.forEach((socket) => {
+  anySocket.combineWith(socket);
+  socket.combineWith(anySocket);
+});
