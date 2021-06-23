f792bfe90d6958c525bc80f4e2f496d300ea8f71
Michael Sharpe <m.sharpe@project89.org>
2021-06-23 16:26:32 -0700

    Add data socket

--- src/rete/sockets.js
+++ src/rete/sockets.js
@@ -7,6 +7,7 @@ export const stringSocket = new Rete.Socket("String");
 export const actionSocket = new Rete.Socket("Action");
 export const actionTypeSocket = new Rete.Socket("ActionType");
 export const entitySocket = new Rete.Socket("Entities");
+export const dataSocket = new Rete.Socket("Data");
 
 const sockets = [
   numSocket,
