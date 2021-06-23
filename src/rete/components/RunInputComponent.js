f75efadf97126be20161ac0974760300b2df588f
Michael Sharpe <m.sharpe@project89.org>
2021-06-23 16:28:22 -0700

    Refactor to use tasks

--- src/rete/components/RunInputComponent.js
+++ src/rete/components/RunInputComponent.js
@@ -1,6 +1,6 @@
 import Rete from "rete";
 import { TextInputControl } from "../controls/TextInputControl";
-import { stringSocket } from "../sockets";
+import { stringSocket, dataSocket } from "../sockets";
 import { RunButtonControl } from "../controls/RunButtonControl";
 
 export class RunInputComponent extends Rete.Component {
@@ -11,9 +11,10 @@ export class RunInputComponent extends Rete.Component {
     this.task = {
       outputs: {
         text: "output",
+        data: "option",
       },
       init: (task) => {
-        console.log("TASK", task);
+        console.log("INIT");
         this.initialTask = task;
         console.log("this", this);
       },
@@ -21,7 +22,10 @@ export class RunInputComponent extends Rete.Component {
   }
 
   run() {
-    this.initialTask.run();
+    const initialData = {
+      foo: "foo",
+    };
+    this.initialTask.run(initialData);
   }
 
   // the builder is used to "assemble" the node component.
@@ -30,8 +34,7 @@ export class RunInputComponent extends Rete.Component {
   builder(node) {
     // create inputs here. First argument is th ename, second is the type (matched to other components sockets), and third is the socket the i/o will use
     const out = new Rete.Output("text", "String", stringSocket);
-
-    console.log("BUILD");
+    const data = new Rete.Output("data", "Data", dataSocket);
 
     // controls are the internals of the node itself
     // This default control simple has a tet field.
@@ -41,23 +44,24 @@ export class RunInputComponent extends Rete.Component {
       value: "Input text",
     });
 
-    console.log("INITIAL TASK", this.initialTask);
-
     const run = new RunButtonControl({
       emitter: this.editor,
       key: "run",
       run: this.run.bind(this),
     });
 
-    return node.addOutput(out).addControl(input).addControl(run);
+    return node
+      .addOutput(data)
+      .addOutput(out)
+      .addControl(input)
+      .addControl(run);
   }
 
   // the worker contains the main business logic of the node.  It will pass those results
   // to the outputs to be consumed by any connecte components
-  async worker(node, inputs, outputs) {
-    console.log("outputs", outputs);
-    console.log("data", node);
-    // outputs["text"] = node.data.text;
+  async worker(node, inputs, data) {
+    console.log("node", node);
+    console.log("data", data);
     return {
       text: node.data.text,
     };
