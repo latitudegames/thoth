b6cd12202e9cf8396a6b270df1a865176a6377d4
pgull <preston@latitude.io>
2021-06-25 11:36:00 -0600

    cleanup

--- src/components/ThothSidePanel/SpellBrowser.js
+++ src/components/ThothSidePanel/SpellBrowser.js
@@ -8,17 +8,8 @@ const SpellBrowser = ({nodeList, nodeMap, editor,  ...props}) => {
     let nodeScreen = editor()
     let spellMap = nodeMap()
 
-    console.log(spellMap.get("Input"))
-    spellMap.forEach(async (value, key) => {
-        // if(key === "Input") nodeScreen.addNode(await createNode(value, { x: 0, y: 0 }))
-    });
-
     return (
         <div className={css['node-grid']}>
-            {/* {spellMap.forEach((value, key) => {
-                return <div className={css['node-grid-item']} key={key} onClick={()=> { nodeScreen.addNode(value)}}>{value.name}</div>
-            })} */}
-
             {Object.keys(spellList).map((item, index)=>{
                 return <div className={css['node-grid-item']} key={item} onClick={async ()=> { nodeScreen.addNode(await createNode(spellMap.get(spellList[item].name), { x: 0, y: 0 }))}}>{spellList[item].name}</div>
             })}
