28640860f956c6b32ce584e6c85af19bc8f5779d
pgull <preston@latitude.io>
2021-06-25 10:02:53 -0600

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,25 +4,20 @@ import ThothPageWrapper from "./components/ThothPage/ThothPageWrapper";
 import gridimg from './grid.png'
 import './dds-globals/dds-globals.css'
 import "./App.css";
-import { useEffect, useState } from "react";
 
 function App() {
   let editor;
-  let nodes = {};
 
   const buildEditor = async (el) => {
     editor = await init(el)
-    // nodes = await Object.fromEntries(editor.components)
   };
 
   const getNodes = () => {
     return Object.fromEntries(editor.components)
   }
-
-  useEffect(() => {
-    nodes = Object.fromEntries(editor.components)
-    console.log(nodes)
-  })
+  const getEditor = () => {
+    return editor
+  }
 
   const serialize = () => {
     console.log(JSON.stringify(editor.toJSON()));
@@ -31,7 +26,7 @@ function App() {
   const toolbar = (<><button>Load</button><button onClick={serialize}>Export</button><button onClick={serialize}>Create New</button></>)
 
   return (
-    <ThothPageWrapper toolbarItems={toolbar} nodeList={getNodes}>
+    <ThothPageWrapper toolbarItems={toolbar} nodeList={getNodes} editor={getEditor}>
       <div style={{ textAlign: "left", width: "100vw", height: "100vh", position: 'absolute', backgroundImage: `url('${gridimg}')` }}>
         <div ref={(el) => buildEditor(el)} />
       </div>
