29dad08944f3783bccd36d581a4713113b9fbb3b
pgull <preston@latitude.io>
2021-06-24 21:48:15 -0600

    tabbable tabs

--- src/components/ThothSidePanel/ThothSidePanel.js
+++ src/components/ThothSidePanel/ThothSidePanel.js
@@ -4,11 +4,30 @@ import css from './sidepanel.module.css'
 
 const ThothSidePanel = ({...props}) => {
     const [activeTab, setActiveTab] = useState('spellBrowser')
+
+    const playTest = ''
+    const spellBrowser = ''
+
+    const tabs = {
+        spellBrowser: {
+            title: "Spell Browser",
+            component: spellBrowser
+        },
+        playtest: {
+            title: "Playtest",
+            component: playTest
+        }
+    }
+
     return (
     <div className={css['th-sidepanel']}>
         <div className={css['tabs']}>
-            <div className={css['tab']}>SPELL BROWSER</div>
-            <div className={css['tab']}>PLAYTEST</div>
+            {Object.keys(tabs).map((item) => {
+                console.log(item)
+                return <div onClick={() => {
+                    setActiveTab(item)
+                }}className={`${css['tab']} ${css[activeTab === item ? 'active' : '']}`}>{tabs[item].title}</div>
+            })}
         </div>
         <div className={css['tab-page']}>
             hi
