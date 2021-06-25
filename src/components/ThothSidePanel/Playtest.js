70c890aef586430a12a6fd658f00d8b85969671c
Michael Sharpe <m.sharpe@project89.org>
2021-06-25 12:03:50 -0700

    Implement pubsub in playtest

--- src/components/ThothSidePanel/Playtest.js
+++ src/components/ThothSidePanel/Playtest.js
@@ -1,19 +1,58 @@
-import React from 'react'
+import { useState, useEffect, useCallback } from "react";
+import { usePubSub } from "../../contexts/PubSub";
 
-import css from './sidepanel.module.css'
+import css from "./sidepanel.module.css";
 
-const Playtest = ({...props}) => {
-    return (
-        <>
-            <div className={css['playtest-output']}>
-            This is example output.
-            </div>
-            <div className={css['input']}>
-                <input type="text"></input>
-                <button className="primary">Send</button>
-            </div>
-        </>
-    )
-}
+const Playtest = ({ ...props }) => {
+  const [history, setHistory] = useState([]);
+  const [value, setValue] = useState("");
 
-export default Playtest
+  const { publish, subscribe, events } = usePubSub();
+
+  const { INPUT_CONSOLE, PRINT_CONSOLE } = events;
+
+  const printToConsole = useCallback(
+    (_, text) => {
+      console.log("message received!");
+      const newHistory = [...history, text];
+      setHistory(newHistory);
+    },
+    [history]
+  );
+
+  useEffect(() => {
+    const unsubscribe = subscribe(PRINT_CONSOLE, printToConsole);
+
+    // return a clean up function
+    return unsubscribe;
+  }, [subscribe, printToConsole, PRINT_CONSOLE]);
+
+  const printItem = (text, key) => <p key={key}>{text}</p>;
+
+  const publishInput = () => {
+    const newHistory = [...history, `You: ${value}`];
+    setHistory(newHistory);
+    publish(INPUT_CONSOLE, value);
+    setValue("");
+  };
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div className={css["playtest-output"]}>
+        <ul>{history.map(printItem)}</ul>
+      </div>
+      <div className={css["input"]}>
+        <input type="text" value={value} onChange={onChange}></input>
+        <button className="primary" onClick={publishInput}>
+          Send
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Playtest;
