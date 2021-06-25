90198158fd5350504c2d49873a6949fc028fcc2b
Michael Sharpe <m.sharpe@project89.org>
2021-06-25 11:23:19 -0700

    Add basic pubsub context

---
+++ src/contexts/PubSub.js
@@ -0,0 +1,39 @@
+import PubSub from "pubsub-js";
+import { useContext, createContext } from "react";
+
+const Context = createContext({
+  publish: () => {},
+  subscribe: () => {},
+  PubSub: () => {},
+  events: {},
+});
+
+export const usePubSub = () => useContext(Context);
+
+const events = {
+  INPUT_CONSOLE: "consoleInput",
+  PRINT_CONSOLE: "printConsole",
+};
+
+const PubSubProvider = ({ children }) => {
+  const publish = (event, data) => {
+    return PubSub.publish(event, data);
+  };
+
+  const subscribe = (event, callback) => {
+    return PubSub.subscribe(event, callback);
+  };
+
+  const publicInterface = {
+    publish,
+    subscribe,
+    events,
+    PubSub,
+  };
+
+  return (
+    <Context.Provider value={publicInterface}>{children}</Context.Provider>
+  );
+};
+
+export default PubSubProvider;
