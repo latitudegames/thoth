8decebb2065c6a3123c8ae5ec2c4d61473215dce
pgull <preston@latitude.io>
2021-06-25 09:52:53 -0600

    render spell list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import init from "./rete/editor";
-import "./App.css";
 
 import ThothPageWrapper from "./components/ThothPage/ThothPageWrapper";
 import gridimg from './grid.png'
 import './dds-globals/dds-globals.css'
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   let editor;
+  let nodes = {};
 
   const buildEditor = async (el) => {
-    editor = await init(el);
+    editor = await init(el)
+    // nodes = await Object.fromEntries(editor.components)
   };
+
+  const getNodes = () => {
+    return Object.fromEntries(editor.components)
+  }
+
+  useEffect(() => {
+    nodes = Object.fromEntries(editor.components)
+    console.log(nodes)
+  })
 
   const serialize = () => {
     console.log(JSON.stringify(editor.toJSON()));
   };
 
-  const toolbar = (<><button>Load</button><button onClick={serialize}>Export</button><button onClick={serialize}>Create New</button><button className={'primary'}>Run</button></>)
+  const toolbar = (<><button>Load</button><button onClick={serialize}>Export</button><button onClick={serialize}>Create New</button></>)
 
   return (
-    <ThothPageWrapper toolbarItems={toolbar} editor={editor}>
-      <div style={{ textAlign: "left", width: "100vw", height: "100vh", position: 'absolute' }}>
-        <div style={{backgroundImage: `url('${gridimg}')`}} ref={(el) => buildEditor(el)} />
+    <ThothPageWrapper toolbarItems={toolbar} nodeList={getNodes}>
+      <div style={{ textAlign: "left", width: "100vw", height: "100vh", position: 'absolute', backgroundImage: `url('${gridimg}')` }}>
+        <div ref={(el) => buildEditor(el)} />
       </div>
     </ThothPageWrapper>
   )
