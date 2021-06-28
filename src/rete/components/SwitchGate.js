9ee3ce5d8cf362499dadd10986e4f827ebf05146
Michael Sharpe <m.sharpe@project89.org>
2021-06-27 20:48:44 -0700

    Render sockets from data outputs if they exist

--- src/rete/components/SwitchGate.js
+++ src/rete/components/SwitchGate.js
@@ -20,13 +20,17 @@ export class SwitchGate extends Rete.Component {
 
   procesOutput;
 
-  // We might need this issue when we want to save and restore the dynamic sockets:
-  // https://github.com/retejs/rete/issues/439
+  // TODO refactor this function into smaller class functions
+  // note: might be possible to abstract this into a parent class to be used by anyone
+  // that wants to make components with dynamic outputs.
   builder(node) {
     this.node = node;
 
     const setOutputs = (outputs) => {
       this.dynamicOutputs = outputs;
+
+      // save these to the nodes data
+      this.node.data.outputs = outputs;
 
       const existingOutputs = [];
 
@@ -48,11 +52,13 @@ export class SwitchGate extends Rete.Component {
         (out) => !existingOutputs.includes(out)
       );
 
+      // Here we are running over and ensuring that the outputs are in the task
       this.task.outputs = this.dynamicOutputs.reduce((acc, out) => {
         acc[out] = "option";
         return acc;
       }, {});
 
+      // From these new outputs, we iterate and add an output socket to the node
       newOutputs.forEach((output) => {
         const newOutput = new Rete.Output(
           output,
@@ -62,24 +68,48 @@ export class SwitchGate extends Rete.Component {
         this.node.addOutput(newOutput);
       });
 
+      // Update the node so these changes take effect
+      console.log("RUNNING NODE UPDATE");
       node.update();
-      this.editor.trigger("process");
     };
 
     const switchControl = new OutputGenerator({
+      defaultOutputs: node.data.outputs || ["default"],
       setOutputs: (outputs) => setOutputs.call(this, outputs),
       key: "dynamicOutput",
     });
 
     const input = new Rete.Input("input", "Input", anySocket);
     const dataInput = new Rete.Input("data", "Data", dataSocket);
-    const defaultOutput = new Rete.Input("default", "Default", dataSocket);
+    const defaultOutput = new Rete.Output("default", "Default", dataSocket);
 
     node
       .addInput(input)
       .addInput(dataInput)
       .addOutput(defaultOutput)
       .addControl(switchControl);
+
+    // Handle outputs in the nodes data to repopulate when loading from JSON
+    if (node.data.outputs && node.data.outputs.length !== 0) {
+      console.log("Output data found");
+      node.data.outputs.forEach((key) => {
+        const output = new Rete.Output(
+          key,
+          capitalizeFirstLetter(key),
+          dataSocket
+        );
+        node.addOutput(output);
+      });
+
+      // Add the data outputs to the tasks outputs
+      this.task.outputs = node.data.outputs.reduce(
+        (acc, out) => {
+          acc[out] = "option";
+          return acc;
+        },
+        { ...this.task.outputs }
+      );
+    }
 
     return node;
   }
