ab73ca82ea4b5f3b05b5959d35458c3e3462814d
Michael Sharpe <m.sharpe@project89.org>
2021-06-29 12:02:29 -0700

    Add save functionality to toolbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useRete, Editor } from "./contexts/Rete";
+import { useThoth } from "./contexts/Thoth";
 import ThothPageWrapper from "./components/ThothPage/ThothPageWrapper";
 
 import "./dds-globals/dds-globals.css";
@@ -6,9 +7,17 @@ import "./App.css";
 
 function App() {
   const { serialize } = useRete();
+  const { saveCurrentSpell } = useThoth();
+
+  const onSave = () => {
+    const serialized = serialize();
+    console.log("saving");
+    saveCurrentSpell({ graph: serialized });
+  };
 
   const toolbar = (
     <>
+      <button onClick={onSave}>Save</button>
       <button>Load</button>
       <button onClick={serialize}>Export</button>
       <button onClick={serialize}>Create New</button>
