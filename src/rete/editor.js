c80ff7bc51cafa0ad625db66b107a59278bdc825
Michael Sharpe <m.sharpe@project89.org>
2021-06-29 09:23:00 -0700

    Add load method to editor.

--- src/rete/editor.js
+++ src/rete/editor.js
@@ -50,7 +50,7 @@ const editor = async function (container, pubSub) {
   editor.pubSub = pubSub;
 
   // PLUGINS
-  //https://github.com/retejs/comment-plugin
+  // https://github.com/retejs/comment-plugin
   // connection plugin is used to render conections between nodes
   editor.use(ConnectionPlugin);
 
@@ -62,7 +62,6 @@ const editor = async function (container, pubSub) {
 
   // renders a context menu on right click that shows available nodes
   editor.use(ContextMenuPlugin);
-
   editor.use(TaskPlugin);
 
   // The engine is used to process/run the rete graph
@@ -91,15 +90,12 @@ const editor = async function (container, pubSub) {
     }
   );
 
-  const defaultState =
-    '{"id":"demo@0.1.0","nodes":{"1":{"id":1,"data":{"text":"Input text here","undefined":"Sam"},"inputs":{},"outputs":{"text":{"connections":[{"node":12,"input":"name","data":{}}]}},"position":[74.40924045994934,79.08276852760682],"name":"Input"},"9":{"id":9,"data":{"display":"true"},"inputs":{"string":{"connections":[{"node":13,"output":"text","data":{}}]},"data":{"connections":[{"node":13,"output":"data","data":{}}]}},"outputs":{"boolean":{"connections":[{"node":11,"input":"boolean","data":{}}]},"data":{"connections":[{"node":11,"input":"data","data":{}}]}},"position":[-199.3843239822832,-205.0910954299351],"name":"Safety Verifier"},"11":{"id":11,"data":{},"inputs":{"boolean":{"connections":[{"node":9,"output":"boolean","data":{}}]},"data":{"connections":[{"node":9,"output":"data","data":{}}]}},"outputs":{"true":{"connections":[{"node":12,"input":"data","data":{}}]},"false":{"connections":[]}},"position":[99.11344037443256,-410.6986301163858],"name":"Boolean Gate"},"12":{"id":12,"data":{"display":"Joe walks into the bar."},"inputs":{"data":{"connections":[{"node":11,"output":"true","data":{}}]},"text":{"connections":[{"node":13,"output":"text","data":{}}]},"name":{"connections":[{"node":1,"output":"text","data":{}}]}},"outputs":{"action":{"connections":[]},"data":{"connections":[]}},"position":[426.32063666846057,-244.95964440421992],"name":"Tense Transformer"},"13":{"id":13,"data":{},"inputs":{},"outputs":{"text":{"connections":[{"node":9,"input":"string","data":{}},{"node":12,"input":"text","data":{}}]},"data":{"connections":[{"node":9,"input":"data","data":{}}]}},"position":[-567.2821504111539,-206.93077213491136],"name":"Console Input"}}}';
-
-  // a default editor graph state
-  editor.fromJSON(JSON.parse(defaultState));
-
-  editor.view.resize();
-  AreaPlugin.zoomAt(editor);
-  editor.trigger("process");
+  editor.loadGraph = (graph) => {
+    editor.fromJSON(graph);
+    editor.view.resize();
+    AreaPlugin.zoomAt(editor);
+    editor.trigger("process");
+  };
 
   return editor;
 };
