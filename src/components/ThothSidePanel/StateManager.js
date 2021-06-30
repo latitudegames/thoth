ffa08b303db1c68a0b605fb5826f7c0bf6209b14
Michael Sharpe <m.sharpe@project89.org>
2021-06-29 17:33:59 -0700

    Add working code editor to state manager

--- src/components/ThothSidePanel/StateManager.js
+++ src/components/ThothSidePanel/StateManager.js
@@ -1,12 +1,53 @@
+import { useState, useEffect } from "react";
+import jsonFormat from "json-format";
+import Editor from "react-simple-code-editor";
+
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-json";
+import "prism-themes/themes/prism-synthwave84.css";
+
 import css from "./sidepanel.module.css";
 
+import { useThoth } from "../../contexts/Thoth";
+
 const StateManager = ({ ...props }) => {
+  const { currentGameState, updateCurrentGameState } = useThoth();
+  const [code, setCode] = useState("{}");
+
+  useEffect(() => {
+    if (currentGameState) setCode(jsonFormat(currentGameState));
+  }, [currentGameState]);
+
+  const onClear = () => {
+    const reset = `{}`;
+
+    setCode(reset);
+  };
+
+  const onSave = () => {
+    updateCurrentGameState(JSON.parse(code));
+  };
+
   return (
     <>
-      <div className={css["playtest-output"]}></div>
+      <div className={css["code-editor"]}>
+        <Editor
+          value={code}
+          onValueChange={setCode}
+          highlight={(code) => {
+            return highlight(code, languages.json);
+          }}
+          padding={10}
+        />
+      </div>
       <div className={css["input"]}>
-        <input type="text"></input>
-        <button className="primary">Send</button>
+        <button className="secondary" onClick={onClear}>
+          Clear
+        </button>
+        <button className="primary" onClick={onSave}>
+          Save
+        </button>
       </div>
     </>
   );
