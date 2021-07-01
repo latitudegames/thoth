1321ce203dd2bd00a69b8f2fca8d5d7a148a5759
Michael Sharpe <m.sharpe@project89.org>
2021-07-01 15:05:43 -0700

    Add array socket

--- src/rete/sockets.js
+++ src/rete/sockets.js
@@ -3,13 +3,15 @@ import Rete from "rete";
 export const anySocket = new Rete.Socket("Any type");
 export const numSocket = new Rete.Socket("Number");
 export const booleanSocket = new Rete.Socket("Boolean");
+export const arraySocket = new Rete.Socket("Aarray");
 export const stringSocket = new Rete.Socket("String");
+export const dataSocket = new Rete.Socket("Data");
+
 export const actionSocket = new Rete.Socket("Action");
 export const actionTypeSocket = new Rete.Socket("ActionType");
 export const actionDifficultySocket = new Rete.Socket("Action Difficulty");
 export const entitySocket = new Rete.Socket("Entities");
 export const timeDetectorSocket = new Rete.Socket("Time Detected");
-export const dataSocket = new Rete.Socket("Data");
 export const itemTypeSocket = new Rete.Socket("Item Detected");
 
 const sockets = [
@@ -22,6 +24,7 @@ const sockets = [
   itemTypeSocket,
   timeDetectorSocket,
   actionDifficultySocket,
+  arraySocket,
 ];
 
 sockets.forEach((socket) => {
