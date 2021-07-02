30254c1a8e58485d9b225f6e035020247f18c90a
Michael Sharpe <m.sharpe@project89.org>
2021-07-02 16:02:44 -0700

    Fix linting errors

--- src/components/ThothPage/ThothPageWrapper.js
+++ src/components/ThothPage/ThothPageWrapper.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ThothSidePanel from "../ThothSidePanel/ThothSidePanel";
+// import ThothSidePanel from "../ThothSidePanel/ThothSidePanel";
 
 import Toolbar from "../Toolbar/Toolbar";
 import "./pagewrapper.module.css";
