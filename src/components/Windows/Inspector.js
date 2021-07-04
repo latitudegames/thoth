3cf1773c43c4e440ea0d4e992d8a8809e4062eb7
Michael Sharpe <m.sharpe@project89.org>
2021-07-03 23:32:30 -0700

    Wire up inspector with code editor and pubsub

--- src/components/Windows/Inspector.js
+++ src/components/Windows/Inspector.js
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import jsonFormat from "json-format";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-json";
+import "prism-themes/themes/prism-synthwave84.css";
+
+import { usePubSub } from "../../contexts/PubSub";
+
 const Inspector = () => {
-  return <p>Inspector</p>;
+  const { publish, subscribe, events } = usePubSub();
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    subscribe(events.INSPECTOR_SET, (event, data) => {
+      console.log("INSPECTOR RECEIVED DATA", data);
+      setData(jsonFormat(data));
+    });
+  });
+
+  const onSave = () => {
+    const parsed = JSON.parse(data);
+    publish(events.NODE_SET(parsed.nodeId), parsed);
+  };
+
+  return (
+    <>
+      <p>Component Data:</p>
+      <Editor
+        value={data}
+        onValueChange={setData}
+        highlight={(code) => {
+          return highlight(code, languages.json);
+        }}
+        padding={10}
+      />
+      <button className="primary" onClick={onSave}>
+        Save
+      </button>
+    </>
+  );
 };
 
 export default Inspector;
