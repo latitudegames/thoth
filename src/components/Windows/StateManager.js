d270475794814c6d84824b53a87e288eda323db5
Michael Sharpe <m.sharpe@project89.org>
2021-07-04 15:01:58 -0700

    Refactor into windows folder

--- src/components/Windows/StateManager.js
+++ src/components/Windows/StateManager.js
@@ -1,19 +1,25 @@
 import { useState, useEffect } from "react";
 import jsonFormat from "json-format";
-import Editor from "react-simple-code-editor";
+import Editor from "@monaco-editor/react";
+import { Flex, Box } from "rebass";
 
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-json";
-import "prism-themes/themes/prism-synthwave84.css";
-
-import css from "./sidepanel.module.css";
+import css from "./windows.module.css";
 
 import { useThoth } from "../../contexts/Thoth";
 
 const StateManager = ({ ...props }) => {
   const { currentGameState, rewriteCurrentGameState } = useThoth();
   const [code, setCode] = useState("{}");
+
+  const editorOptions = {
+    lineNumbers: false,
+    minimap: {
+      enabled: false,
+    },
+    suggest: {
+      preview: false,
+    },
+  };
 
   useEffect(() => {
     if (currentGameState) setCode(jsonFormat(currentGameState));
@@ -30,8 +36,17 @@ const StateManager = ({ ...props }) => {
   };
 
   return (
-    <>
-      <div className={css["code-editor"]}>
+    <Flex flexDirection="column" css={{ height: "100%" }}>
+      <Editor
+        height="90vh"
+        theme="vs-dark"
+        defaultLanguage="json"
+        value={code}
+        options={editorOptions}
+        defaultValue={code}
+        onChange={setCode}
+      />
+      {/* <CustomScroll flex="8">
         <Editor
           value={code}
           onValueChange={setCode}
@@ -40,16 +55,16 @@ const StateManager = ({ ...props }) => {
           }}
           padding={10}
         />
-      </div>
-      <div className={css["input"]}>
+      </CustomScroll> */}
+      <Box className={css["input"]} flex={1}>
         <button className="secondary" onClick={onClear}>
           Clear
         </button>
         <button className="primary" onClick={onSave}>
           Save
         </button>
-      </div>
-    </>
+      </Box>
+    </Flex>
   );
 };
 
