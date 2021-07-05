d2208c7963bc2303dd8e3b69bff5b85fc03ee6d6
Michael Sharpe <m.sharpe@project89.org>
2021-07-05 01:02:20 -0700

    Start working on various inputs for inspector.

--- src/components/Windows/Inspector.js
+++ src/components/Windows/Inspector.js
@@ -1,12 +1,63 @@
 import { useEffect, useState } from "react";
-import jsonFormat from "json-format";
-import Editor from "react-simple-code-editor";
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-json";
-import "prism-themes/themes/prism-synthwave84.css";
+import { Flex, Box } from "rebass";
+import TextareaAutosize from "react-textarea-autosize";
+import {
+  Label,
+  Input as InputComponent,
+  Textarea as TextareaComponent,
+} from "@rebass/forms";
 
 import { usePubSub } from "../../contexts/PubSub";
+
+const Input = (props) => {
+  const onChange = (e) => {
+    const update = {
+      [props.name]: e.target.value,
+    };
+    props.updateData(update);
+  };
+
+  return (
+    <Box>
+      <Label htmlFor="email">Email</Label>
+      <InputComponent
+        id={props.name}
+        name={props.name}
+        type={props.name}
+        onChange={onChange}
+      />
+    </Box>
+  );
+};
+
+const Textarea = (props) => {
+  const onChange = (e) => {
+    const update = {
+      [props.name]: e.target.value,
+    };
+    props.updateData(update);
+  };
+
+  return (
+    <Box>
+      <Label htmlFor="comment">Comment</Label>
+      <TextareaAutosize
+        onChange={onChange}
+        id={props.name}
+        value={props.value}
+        style={{ resize: "vertical" }}
+      />
+    </Box>
+  );
+};
+
+// const Slider = () => {};
+
+// const Switch = () => {};
+
+// const Radio = () => {};
+
+// const select = () => {};
 
 const Inspector = () => {
   const { publish, subscribe, events } = usePubSub();
@@ -14,7 +65,8 @@ const Inspector = () => {
 
   useEffect(() => {
     subscribe(events.INSPECTOR_SET, (event, data) => {
-      setData(jsonFormat(data));
+      console.log("DATA", data);
+      setData(data);
     });
   }, [events, subscribe]);
 
@@ -23,20 +75,42 @@ const Inspector = () => {
     publish(events.NODE_SET(parsed.nodeId), parsed);
   };
 
+  const updateData = (update) => {
+    const newData = {
+      ...data,
+      ...update,
+    };
+
+    setData(newData);
+  };
+
   return (
     <>
       <p>Component Data:</p>
-      <Editor
-        value={data}
-        onValueChange={setData}
-        highlight={(code) => {
-          return highlight(code, languages.json);
-        }}
-        padding={10}
-      />
-      <button className="primary" onClick={onSave}>
-        Save
-      </button>
+      <Flex>
+        {data.dataControls &&
+          Object.entries(data.dataControls).map(([key, value], i) => {
+            console.log("switching!", value);
+            switch (value.type) {
+              case "textarea":
+                return (
+                  <Textarea
+                    key={i + key}
+                    name={key}
+                    value={data.data[key] || ""}
+                    updateData={updateData}
+                  />
+                );
+              default:
+                return <></>;
+            }
+          })}
+        <Box flex={8}>
+          <button className="primary" onClick={onSave}>
+            Save
+          </button>
+        </Box>
+      </Flex>
     </>
   );
 };
