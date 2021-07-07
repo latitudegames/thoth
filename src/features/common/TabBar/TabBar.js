64c7bca1b2d333ea55eb9764db176387717e4cb9
Michael Sharpe <m.sharpe@project89.org>
2021-07-07 12:01:11 -0700

    Style active and inactive tabs

--- src/features/common/TabBar/TabBar.js
+++ src/features/common/TabBar/TabBar.js
@@ -1,15 +1,36 @@
 import React from "react";
+import classnames from "classnames";
+import { VscClose } from "react-icons/vsc";
 
 import css from "./tabBar.module.css";
 import thothlogo from "./thoth.png";
 
-const TabBar = ({ ...props }) => {
+const Tab = (props) => {
+  const title = `${props.type}- ${props.name}`;
+  const tabClass = classnames({
+    [css["tabbar-tab"]]: true,
+    [css["active"]]: props.active,
+    [css["inactive"]]: !props.active,
+  });
+  return (
+    <div className={tabClass}>
+      <p>{title}</p>
+      <span>
+        <VscClose />
+      </span>
+    </div>
+  );
+};
+
+const TabBar = ({ tabs }) => {
   return (
     <div className={css["th-tabbar"]}>
       <div className={css["tabbar-section"]}>
         <img className={css["thoth-logo"]} alt="Thoth logo" src={thothlogo} />
       </div>
-      <div className={css["tabbar-section"]}>{props.children}</div>
+      <div className={css["tabbar-section"]}>
+        {tabs && tabs.map((tab) => <Tab {...tab} />)}
+      </div>
     </div>
   );
 };
