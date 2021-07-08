63d02a92687e477a40d44bbb99371c52ed357a1e
Michael Sharpe <m.sharpe@project89.org>
2021-07-08 00:39:07 -0700

    Pass down options

--- src/features/common/ThothPage/ThothPageWrapper.js
+++ src/features/common/ThothPage/ThothPageWrapper.js
@@ -5,11 +5,11 @@ import TabBar from "../TabBar/TabBar";
 import css from "./pagewrapper.module.css";
 import Toolbar from "../Toolbar/Toolbar";
 
-const ThothPageWrapper = ({ toolbarItems, tabs, ...props }) => {
+const ThothPageWrapper = ({ toolbarItems, tabs, options, ...props }) => {
   return (
     <div className={css["wrapper"]}>
       <TabBar tabs={tabs} />
-      <Toolbar>{toolbarItems}</Toolbar>
+      <Toolbar toolbar={toolbarItems} options={options} />
       {/* <ThothSidePanel /> */}
       <div className={css["view-container"]}>{props.children}</div>
     </div>
