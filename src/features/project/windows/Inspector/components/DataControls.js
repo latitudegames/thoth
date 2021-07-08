c9e78b6a9dfdcd9567620cbb56d51c27e6fb0875
Michael Sharpe <m.sharpe@project89.org>
2021-07-08 10:51:29 -0700

    Fix errors in data controls

--- src/features/project/windows/Inspector/components/DataControls.js
+++ src/features/project/windows/Inspector/components/DataControls.js
@@ -1,43 +1,65 @@
 import { SimpleAccordion } from "../../../../common/Accordion";
 
+// const Textarea = (props) => {
+//   const [value, setValue] = useState();
+
+//   useEffect(() => {
+//     setValue(props.initialValue);
+//   }, [props.initialValue]);
+
+//   const onChange = (e) => {
+//     const update = {
+//       [props.name]: e.target.value,
+//     };
+//     setValue(e.target.value);
+//     props.updateData(update);
+//   };
+
+//   return (
+//     <div></div>
+//   );
+// }
+
+const StubComponent = (props) => <div>{props.name}</div>;
+
+// const LongText = props => {
+//   return (
+
+//   )
+// }
+
 const controlMap = {
-  longText: <></>,
-  input: <></>,
-  slider: <></>,
-  dial: <></>,
+  longText: () => StubComponent,
+  input: () => StubComponent,
+  slider: () => StubComponent,
+  dial: () => StubComponent,
 };
 
-const isEmpty = (obj) =>
-  obj && Object.keys(obj).length === 0 && obj.constructor === Object;
+const DataControls = ({ dataControls, updateData, width, data, ...props }) => {
+  if (!dataControls) return <p>Select a component.</p>;
 
-const DataControls = ({
-  dataControls,
-  updateData,
-  width,
-  initialValue,
-  data,
-  ...props
-}) => {
-  if (!dataControls && isEmpty(dataControls)) return <p>Loading...</p>;
+  return (
+    <>
+      {Object.entries(dataControls).map(([key, value]) => {
+        // Default props to pass through to every data control
+        const controlProps = {
+          width,
+          key,
+          name: key,
+          initialValue: data[key] || "",
+          updateData,
+        };
 
-  return Object.entries(dataControls).map(([key, value]) => {
-    // Default props to pass through to every data control
-    const controlProps = {
-      width,
-      key,
-      name: key,
-      initialValue: data.data[key] || "",
-      updateData,
-    };
+        const Component = controlMap[value.type] || StubComponent;
 
-    const Component = controlMap[value.type] || <></>;
-
-    return (
-      <SimpleAccordion>
-        <Component {...controlProps} />;
-      </SimpleAccordion>
-    );
-  });
+        return (
+          <SimpleAccordion heading={key} key={key}>
+            <Component {...controlProps} />
+          </SimpleAccordion>
+        );
+      })}
+    </>
+  );
 };
 
 export default DataControls;
