292757b7e291cdb3ecbcf990a66be045daa0e800
Michael Sharpe <m.sharpe@project89.org>
2021-07-08 00:38:57 -0700

    Add options to toolbar

--- src/features/common/Toolbar/Toolbar.js
+++ src/features/common/Toolbar/Toolbar.js
@@ -5,7 +5,8 @@ import css from "./toolbar.module.css";
 const Toolbar = ({ ...props }) => {
   return (
     <div className={css["th-toolbar"]}>
-      <div className={css["toolbar-section"]}>{props.children}</div>
+      <div className={css["toolbar-section"]}>{props.toolbar}</div>
+      <div className={css["toolbar-section"]}>{props.options}</div>
     </div>
   );
 };
