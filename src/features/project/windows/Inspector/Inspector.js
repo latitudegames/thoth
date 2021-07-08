8061572e7b73cec1192a12122c8c12d8ab4b75e3
Michael Sharpe <m.sharpe@project89.org>
2021-07-08 10:51:07 -0700

    Refactor inspector to use data control component

--- src/features/project/windows/Inspector/Inspector.js
+++ src/features/project/windows/Inspector/Inspector.js
@@ -1,56 +1,22 @@
 import { useEffect, useState } from "react";
-import Collapsible from "react-collapsible";
-import TextareaAutosize from "react-textarea-autosize";
 
 import Window from "../../../common/Window/Window";
 import { usePubSub } from "../../../../contexts/PubSub";
-import { SimpleAccordion } from "../../../common/Accordion";
-
-const Textarea = (props) => {
-  const [value, setValue] = useState();
-
-  useEffect(() => {
-    setValue(props.initialValue);
-  }, [props.initialValue]);
-
-  const onChange = (e) => {
-    const update = {
-      [props.name]: e.target.value,
-    };
-    setValue(e.target.value);
-    props.updateData(update);
-  };
-
-  return (
-    <Collapsible trigger={props.name}>
-      <TextareaAutosize
-        onChange={onChange}
-        id={props.name}
-        value={value}
-        style={{ resize: "vertical", width: props.width }}
-      />
-    </Collapsible>
-  );
-};
+import DataControls from "./components/DataControls";
 
 const Inspector = (props) => {
   const { publish, subscribe, events } = usePubSub();
   const [data, setData] = useState("");
-  const [height, setHeight] = useState();
   const [width, setWidth] = useState();
 
-  const bottomHeight = 50;
-
   useEffect(() => {
-    if (props?.node?._rect?.height) {
-      setHeight(props.node._rect.height - bottomHeight);
+    if (props?.node?._rect?.width) {
       setWidth(props.node._rect.width);
     }
 
     // this is to dynamically set the appriopriate height so that Monaco editor doesnt break flexbox when resizing
     props.node.setEventListener("resize", (data) => {
       setTimeout(() => {
-        setHeight(data.rect.height - bottomHeight);
         setWidth(data.rect.width);
       }, 0);
     });
@@ -85,23 +51,12 @@ const Inspector = (props) => {
 
   return (
     <Window toolbar={toolbar}>
-      <SimpleAccordion />
-      {data.dataControls &&
-        Object.entries(data.dataControls).map(([key, value], i) => {
-          const controlProps = {
-            width,
-            key,
-            name: key,
-            initialValue: data.data[key] || "",
-            updateData,
-          };
-          switch (value.type) {
-            case "textarea":
-              return <Textarea {...controlProps} />;
-            default:
-              return <></>;
-          }
-        })}
+      <DataControls
+        dataControls={data.dataControls}
+        data={data.data}
+        width={width}
+        updateData={updateData}
+      />
     </Window>
   );
 };
