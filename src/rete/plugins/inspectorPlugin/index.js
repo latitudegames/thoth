e912a40b939eb13d0c916f1a0379d96ac29e87bd
Michael Sharpe <m.sharpe@project89.org>
2021-07-08 11:48:56 -0700

    Add builder override in inspector plugin

--- src/rete/plugins/inspectorPlugin/index.js
+++ src/rete/plugins/inspectorPlugin/index.js
@@ -1,35 +1,51 @@
 function install(editor) {
+  // Track all main data controls
+
   // handle publishing and subscribing to inspector
   editor.on("nodeselect", (node) => {
-    const component = editor.getComponent(node.name);
     const { publish, subscribe, events } = editor.pubSub;
 
     publish(events.INSPECTOR_SET, {
       name: node.name,
       nodeId: node.id,
-      dataControls: component.dataControls,
+      dataControls: node.dataControls,
       data: node.data,
     });
 
     // we set up a subscribe to that nodes channel when it saves data.
     subscribe(events.NODE_SET(node.id), (event, { data }) => {
       node.data = data;
+      node.onData(data);
 
-      console.log("WORKING");
-
-      if (node.onInspector) {
-        node.onInspector(data);
-      }
       node.update();
     });
   });
 
   editor.on("componentregister", (component) => {
-    // nodes in builder have access to an 'addDataControl' function
-    // they can add dataControls as an object?
-    // callback can be added
-    // dataControl is registered on the node
-    // data control subscribes to
+    const builder = component.builder;
+
+    // we are going to override the default builder with our own, and will invoke the original builder inside it.
+    component.builder = (node) => {
+      // Maybe make this a set?
+      node.dataControls = {};
+
+      node.addDataControl = (dataControl, callback = () => {}) => {
+        // Maybe perform some checks here?
+        // Maybe make dataController a class?
+        node.dataControls = {
+          ...node.dataControls,
+          ...dataControl,
+        };
+
+        // Attach a callback to the node when data comes in.
+        node.onData = callback;
+      };
+
+      // stubbed function for now as reminder
+      node.removeDataControl = () => {};
+
+      builder.call(component, node);
+    };
   });
 }
 
