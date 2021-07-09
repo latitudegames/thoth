0e5bb898cd7805315da89c48299fcd9e2b44e8ef
Michael Sharpe <m.sharpe@project89.org>
2021-07-09 11:28:08 -0700

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "flexlayout-react/style/dark.css";
 
-import { useRete, Editor } from "./contexts/Rete";
-import { useThoth } from "./contexts/Thoth";
 import ThothPageWrapper from "./features/common/ThothPage/ThothPageWrapper";
 import Project from "./features/project/Project";
 
@@ -9,39 +7,6 @@ import "./dds-globals/dds-globals.css";
 import "./App.css";
 
 function App() {
-  const { serialize } = useRete();
-  const { saveCurrentSpell } = useThoth();
-
-  const onSave = () => {
-    const serialized = serialize();
-    saveCurrentSpell({ graph: serialized });
-  };
-
-  const onSerialize = () => {
-    const serialized = serialize();
-    console.log(JSON.stringify(serialized));
-  };
-
-  const toolbar = (
-    <>
-      <button className="option" onClick={onSave}>
-        Save
-      </button>
-      <button className="option">Load</button>
-      <button className="option" onClick={onSerialize}>
-        Export
-      </button>
-    </>
-  );
-
-  const options = (
-    <>
-      <button className="option">State Manager</button>
-      <button className="option">Playtest</button>
-      <button className="option">Inspector</button>
-    </>
-  );
-
   const tabs = [
     {
       name: "My Project",
@@ -56,7 +21,7 @@ function App() {
   ];
 
   return (
-    <ThothPageWrapper toolbarItems={toolbar} tabs={tabs} options={options}>
+    <ThothPageWrapper tabs={tabs}>
       <Project />
     </ThothPageWrapper>
   );
