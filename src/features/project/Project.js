4d6f94e7b630e114a3de2d25be7f9ccf479abb58
Michael Sharpe <m.sharpe@project89.org>
2021-07-09 11:26:00 -0700

    Rerfactor page into Project component

---
+++ src/features/project/Project.js
@@ -0,0 +1,71 @@
+import { Layout, Model } from "flexlayout-react";
+import { useRete, Editor } from "../../contexts/Rete";
+import { useThoth } from "../../contexts/Thoth";
+import Toolbar from "../common/Toolbar/Toolbar";
+import StateManager from "./windows/StateManager";
+import Playtest from "./windows/Playtest";
+import Inspector from "./windows/Inspector/Inspector";
+
+import defaultJson from "./layout.json";
+import TabLayout from "../common/TabLayout/TabLayout";
+
+const model = Model.fromJson(defaultJson);
+
+const Project = () => {
+  const { serialize } = useRete();
+  const { saveCurrentSpell } = useThoth();
+
+  const onSave = () => {
+    const serialized = serialize();
+    saveCurrentSpell({ graph: serialized });
+  };
+
+  const onSerialize = () => {
+    const serialized = serialize();
+    console.log(JSON.stringify(serialized));
+  };
+
+  const toolbar = (
+    <>
+      <button className="option" onClick={onSave}>
+        Save
+      </button>
+      <button className="option">Load</button>
+      <button className="option" onClick={onSerialize}>
+        Export
+      </button>
+    </>
+  );
+
+  const options = (
+    <>
+      <button className="option">State Manager</button>
+      <button className="option">Playtest</button>
+      <button className="option">Inspector</button>
+    </>
+  );
+
+  const factory = (node) => {
+    const component = node.getComponent();
+    switch (component) {
+      case "editor":
+        return <Editor />;
+      case "stateManager":
+        return <StateManager node={node} />;
+      case "playtest":
+        return <Playtest />;
+      case "inspector":
+        return <Inspector node={node} />;
+      default:
+        return <p></p>;
+    }
+  };
+
+  return (
+    <TabLayout options={options} toolbar={toolbar}>
+      <Layout model={model} factory={factory} />
+    </TabLayout>
+  );
+};
+
+export default Project;
