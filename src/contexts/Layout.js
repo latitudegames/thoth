9a2689f9083e06ce1fd1e94f629605491247ab98
Michael Sharpe <m.sharpe@project89.org>
2021-07-09 10:16:26 -0700

    Basic persistent layout context

---
+++ src/contexts/Layout.js
@@ -0,0 +1,40 @@
+import { useContext, createContext, useState } from "react";
+import { useDB } from "react-pouchdb";
+
+const Context = createContext({
+  getLayout: () => {},
+  setLayout: () => {},
+  currentLayout: "",
+});
+
+export const useLayout = () => useContext(Context);
+
+const LayoutProvider = ({ children }) => {
+  const db = useDB();
+
+  const getLayout = async (tab) => {
+    const layout = await db.get(`layout:${tab}`);
+    return layout;
+  };
+
+  const setLayout = async (layout) => {
+    try {
+      const savedLayout = await db.put(layout);
+      return savedLayout;
+    } catch (err) {
+      console.error("Error setting layout", err);
+      return undefined;
+    }
+  };
+
+  const publicInterface = {
+    getLayout,
+    setLayout,
+  };
+
+  return (
+    <Context.Provider value={publicInterface}>{children}</Context.Provider>
+  );
+};
+
+export default LayoutProvider;
