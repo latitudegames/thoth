0151fd5aa3570d74d1fef46820e95e6f3f68d0cc
Michael Sharpe <m.sharpe@project89.org>
2021-07-09 10:39:08 -0700

    Move compose provider inside app providers

--- src/contexts/AppProviders.jsx
+++ src/contexts/AppProviders.jsx
@@ -3,7 +3,32 @@ import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import ReteProvider from "./Rete";
 import PubSubProvider from "./PubSub";
 import ThothProvider from "./Thoth";
-import ComposeProviders from "./ComposeProvider";
+
+/**
+ * Provided that a list of providers [P1, P2, P3, P4] is passed as props,
+ * it renders
+ *
+ *    <P1>
+        <P2>
+          <P3>
+            <P4>
+              {children}
+            </P4>
+          </P3>
+        </P2>
+      </P1>
+ *
+ */
+
+function ComposeProviders({ providers, children }) {
+  return providers.reduce((acc, current) => {
+    const [Provider, props] = Array.isArray(current)
+      ? [current[0], current[1]]
+      : [current, {}];
+
+    return <Provider {...props}>{acc}</Provider>;
+  }, children);
+}
 
 const darkTheme = createTheme({
   palette: {
