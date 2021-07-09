d8d168951f5ac331e0b593a22225ac2d6ea17c59
Michael Sharpe <m.sharpe@project89.org>
2021-07-08 17:01:18 -0700

    onData is optional in constructor

--- src/rete/plugins/inspectorPlugin/DataControl.js
+++ src/rete/plugins/inspectorPlugin/DataControl.js
@@ -1,11 +1,12 @@
 export class DataControl {
   inspector = null;
+  editor = null;
 
-  constructor({ data, name, controls, onData }) {
+  constructor({ data, name, controls, ...rest }) {
     this.key = data;
     this.name = name;
     this.controls = controls;
-    this.onData = onData;
+    this.onData = rest.onData || this.onData;
   }
 
   //Serializer to esail extract the data controls information for publishing
