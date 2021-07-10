162ec6ee93b462173546c7637edd0b50caa6fc1b
Michael Sharpe <m.sharpe@project89.org>
2021-07-10 15:40:10 -0700

    Empty out layout context

--- src/contexts/Layout.jsx
+++ src/contexts/Layout.jsx
@@ -1,36 +1,14 @@
 import { useContext, createContext } from "react";
-import { useDB } from "react-pouchdb";
+// import { useDB } from "./Database";
 
-const Context = createContext({
-  getLayout: () => {},
-  setLayout: () => {},
-  currentLayout: "",
-});
+const Context = createContext({});
 
 export const useLayout = () => useContext(Context);
 
 const LayoutProvider = ({ children }) => {
-  const db = useDB();
+  // const db = useDB();
 
-  const getLayout = async (tab) => {
-    const layout = await db.get(`layout:${tab}`);
-    return layout;
-  };
-
-  const setLayout = async (layout) => {
-    try {
-      const savedLayout = await db.put(layout);
-      return savedLayout;
-    } catch (err) {
-      console.error("Error setting layout", err);
-      return undefined;
-    }
-  };
-
-  const publicInterface = {
-    getLayout,
-    setLayout,
-  };
+  const publicInterface = {};
 
   return (
     <Context.Provider value={publicInterface}>{children}</Context.Provider>
