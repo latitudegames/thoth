63e01e1b61cf07d21a422152cf0047c07a8e60de
Michael Sharpe <m.sharpe@project89.org>
2021-07-11 00:16:37 -0700

    Basic longtext component with text editor open in it.

--- src/features/spell/windows/Inspector/components/DataControls.js
+++ src/features/spell/windows/Inspector/components/DataControls.js
@@ -1,4 +1,5 @@
 import { SimpleAccordion } from "../../../../common/Accordion";
+import { usePubSub } from "../../../../../contexts/PubSub";
 
 // const Textarea = (props) => {
 //   const [value, setValue] = useState();
@@ -22,27 +23,47 @@ import { SimpleAccordion } from "../../../../common/Accordion";
 
 const StubComponent = (props) => <div>{props.name}</div>;
 
-// const LongText = props => {
-//   return (
+const LongText = ({ initialValue, name, nodeId }) => {
+  const { events, publish } = usePubSub();
 
-//   )
-// }
+  const onClick = () => {
+    const data = {
+      data: initialValue,
+      nodeId,
+      key: name,
+      name,
+    };
+    publish(events.TEXT_EDITOR_SET, data);
+  };
+
+  return <button onClick={onClick}>Open in text editor</button>;
+};
 
 const controlMap = {
-  longText: StubComponent,
+  longText: LongText,
   input: StubComponent,
   slider: StubComponent,
   dial: StubComponent,
 };
 
-const DataControls = ({ dataControls, updateData, width, data, ...props }) => {
+const DataControls = ({
+  dataControls,
+  updateData,
+  width,
+  data,
+  nodeId,
+  ...props
+}) => {
   if (!dataControls) return <p>Select a component.</p>;
+
+  console.log("Data controls", dataControls);
 
   return (
     <>
       {Object.entries(dataControls).map(([key, value]) => {
         // Default props to pass through to every data control
         const controlProps = {
+          nodeId,
           width,
           key,
           name: key,
