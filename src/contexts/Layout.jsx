9953c1d0a50aad496457c2a6ea96305499c91963
Michael Sharpe <m.sharpe@project89.org>
2021-07-11 12:38:08 -0700

    More helper functions on layout context

--- src/contexts/Layout.jsx
+++ src/contexts/Layout.jsx
@@ -1,18 +1,42 @@
-import { useContext, createContext, useEffect, useState } from "react";
+import { useContext, createContext, useEffect, useState, useRef } from "react";
+import {
+  Layout as LayoutComponent,
+  Model,
+  Actions,
+  DockLocation,
+} from "flexlayout-react";
 import { usePubSub } from "./PubSub";
 // import { useDB } from "./Database";
+import LoadingScreen from "../features/common/LoadingScreen/LoadingScreen";
+
+const componentTypes = {
+  TEXT_EDITOR: "textEditor",
+  INSPECTOR: "inspector",
+  STATE_MANAGER: "stateManager",
+  EDITOR: "editor",
+  PLAYTEST: "playtest",
+};
+
+// helpful resources
+// https://github.com/edemaine/comingle/blob/726d42e975307beb5281fddbf576591c36c1022d/client/Room.coffee#L365-L384
+// https://github.com/caplin/FlexLayout/blob/master/examples/demo/App.tsx
 
 const Context = createContext({
   inspectorData: {},
   textEditorData: {},
-  setCurrentMode: {},
+  createModel: () => {},
   currentModel: {},
+  currentRef: {},
+  setCurrentRef: () => {},
   saveInspector: () => {},
   saveTextEditor: () => {},
+  createOrFocus: () => {},
+  addWindow: () => {},
   addTextEditor: () => {},
   addInspector: () => {},
   addStateManager: () => {},
   addPlaytest: () => {},
+  componentTypes: {},
 });
 
 export const useLayout = () => useContext(Context);
@@ -20,7 +44,8 @@ export const useLayout = () => useContext(Context);
 const LayoutProvider = ({ children }) => {
   const { subscribe, publish, events } = usePubSub();
 
-  const [currentModel, setCurrentModel] = useState({});
+  const [currentModel, setCurrentModel] = useState(null);
+  const [currentRef, setCurrentRef] = useState(null);
   const [inspectorData, setInspectorData] = useState({});
   const [textEditorData, setTextEditorData] = useState({});
 
@@ -64,12 +89,63 @@ const LayoutProvider = ({ children }) => {
     }
   };
 
+  const createModel = (json) => {
+    const model = Model.fromJson(json);
+    setCurrentModel(model);
+
+    return model;
+  };
+
+  const addTabset = () => {
+    const json = {
+      type: "tabset",
+      weight: 12,
+    };
+    return currentModel.doAction(
+      Actions.addNode(json, "#root", DockLocation.RIGHT, 0)
+    );
+  };
+
+  const addWindow = (componentType, title) => {
+    // For now we always add a new window onto the root container
+    const json = {
+      type: "tabset",
+      // weight: 12,
+      name: title,
+    };
+
+    // Mimic this issue
+    // https://github.com/caplin/FlexLayout/issues/54
+
+    currentModel.doAction(
+      Actions.addNode(json, "#root", DockLocation.RIGHT, 0)
+    );
+  };
+
+  const createOrFocus = (componentName, title) => {
+    const component = Object.entries(currentModel._idMap).find(
+      ([key, value]) => {
+        return value._attributes?.component === componentName;
+      }
+    );
+
+    // the nodeId is stored in the zeroth index of the find
+    if (component) currentModel.doAction(Actions.selectTab(component[0]));
+
+    if (!component) addWindow(componentName, title);
+  };
+
   const publicInterface = {
     inspectorData,
     textEditorData,
     saveTextEditor,
     setCurrentModel,
     currentModel,
+    createModel,
+    createOrFocus,
+    componentTypes,
+    currentRef,
+    setCurrentRef,
   };
 
   return (
@@ -77,14 +153,24 @@ const LayoutProvider = ({ children }) => {
   );
 };
 
-export const Layout = ({ model, factory }) => {
-  const { setCurrentModel } = useLayout();
+export const Layout = ({ json, factory }) => {
+  const { currentModel, createModel, setCurrentRef } = useLayout();
+  const layoutRef = useRef(null);
 
   useEffect(() => {
-    setCurrentModel(model);
-  }, [model, setCurrentModel]);
+    if (!json || currentModel) return;
+    createModel(json);
+  }, [json, createModel, currentModel]);
 
-  return <Layout model={model} factory={factory} />;
+  useEffect(() => {
+    setCurrentRef(layoutRef);
+  }, [layoutRef, setCurrentRef]);
+
+  if (!currentModel) return <LoadingScreen />;
+
+  return (
+    <LayoutComponent ref={layoutRef} model={currentModel} factory={factory} />
+  );
 };
 
 export default LayoutProvider;
