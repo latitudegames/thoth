ef92cfaed11ef468475fb02f479210569a53101c
Michael Sharpe <m.sharpe@project89.org>
2021-07-11 17:55:33 -0700

    Handle dynamic sockets on build with plugin

---
+++ src/features/rete/plugins/socketGenerator/index.js
@@ -0,0 +1,39 @@
+import Rete from "rete";
+import { anySocket } from "../../sockets";
+
+function install(editor) {
+  editor.on("componentregister", (component) => {
+    const builder = component.builder;
+
+    // we are going to override the default builder with our own, and will invoke the original builder inside it.
+    component.builder = (node) => {
+      // Handle outputs in the nodes data to repopulate when loading from JSON
+      console.log("checking node data for outputs");
+      if (node.data.outputs && node.data.outputs.length !== 0) {
+        node.data.outputs.forEach((key) => {
+          const output = new Rete.Output(key, key, anySocket);
+          node.addOutput(output);
+        });
+      }
+
+      if (node.data.outputs && node.data.outputs.length > 0) {
+        component.task.outputs = node.data.outputs.reduce(
+          (acc, out) => {
+            acc[out] = "output";
+            return acc;
+          },
+          { ...component.task.outputs }
+        );
+      }
+
+      builder.call(component, node);
+    };
+  });
+}
+
+const defaultExport = {
+  name: "socketGenerator",
+  install,
+};
+
+export default defaultExport;
