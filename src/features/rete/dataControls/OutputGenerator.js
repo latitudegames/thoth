c0d5e9f4ace66cf5be70a1f8190d134228cb6532
Michael Sharpe <m.sharpe@project89.org>
2021-07-11 17:57:55 -0700

    Create output generator data control

---
+++ src/features/rete/dataControls/OutputGenerator.js
@@ -0,0 +1,68 @@
+import Rete from "rete";
+import { anySocket } from "../sockets";
+
+import { DataControl } from "../plugins/inspectorPlugin";
+
+export class OutputGeneratorControl extends DataControl {
+  constructor(defaultOutputs) {
+    const options = {
+      data: "outputs",
+      name: "Data Outputs",
+      controls: {
+        component: "outputGenerator",
+        data: {
+          defaultOutputs,
+        },
+      },
+    };
+
+    super(options);
+  }
+
+  onData(outputs) {
+    this.node.data.outputs = outputs;
+
+    const existingOutputs = [];
+
+    this.node.outputs.forEach((out) => {
+      existingOutputs.push(out.key);
+    });
+
+    // Any outputs existing on the current node that arent incoming have been deleted
+    // and need to be removed.
+    existingOutputs
+      .filter((out) => !outputs.includes(out))
+      .forEach((key) => {
+        const output = this.node.outputs.get(key);
+
+        this.node
+          .getConnections()
+          .filter((con) => con.output.key === key)
+          .forEach((con) => {
+            this.editor.removeConnection(con);
+          });
+
+        this.node.removeOutput(output);
+      });
+
+    // any incoming outputs not already on the node are new and will be added.
+    const newOutputs = outputs.filter((out) => !existingOutputs.includes(out));
+
+    // Here we are running over and ensuring that the outputs are in the task
+    this.component.task.outputs = this.node.data.outputs.reduce(
+      (acc, out) => {
+        acc[out] = "output";
+        return acc;
+      },
+      { ...this.component.task.outputs }
+    );
+
+    // From these new outputs, we iterate and add an output socket to the node
+    newOutputs.forEach((output) => {
+      const newOutput = new Rete.Output(output, output, anySocket);
+      this.node.addOutput(newOutput);
+    });
+
+    this.node.update();
+  }
+}
