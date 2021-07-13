5af828aa839b987035da08dc716df7d49b7cd5e1
Michael Sharpe <m.sharpe@project89.org>
2021-07-13 16:21:08 -0700

    Fix accumulating subscriptions

--- src/features/rete/plugins/inspectorPlugin/index.js
+++ src/features/rete/plugins/inspectorPlugin/index.js
@@ -4,6 +4,7 @@ function install(editor) {
   const { publish, subscribe, events } = editor.pubSub;
 
   let currentNode = null;
+  let unsubscribe = null;
 
   editor.on("componentregister", (component) => {
     const builder = component.builder;
@@ -20,16 +21,21 @@ function install(editor) {
   // handle publishing and subscribing to inspector
   editor.on("nodeselect", (node) => {
     if (node === currentNode) return;
+    if (unsubscribe) unsubscribe();
 
     currentNode = node;
-    // clear text editor
+    // clear spaces first
     publish(events.TEXT_EDITOR_SET, {});
+
+    // Set inspector
     publish(events.INSPECTOR_SET, node.inspector.data());
 
     // we set up a subscribe to that nodes channel when it saves data.
-    subscribe(events.NODE_SET(node.id), (event, data) => {
+    unsubscribe = subscribe(events.NODE_SET(node.id), (event, data) => {
       node.inspector.handleData(data);
     });
+
+    // we need to unsibscribe when a new node is selected
   });
 }
 
