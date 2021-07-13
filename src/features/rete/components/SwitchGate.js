75a1c49a829d648b04b86093cabebe3faa3242d1
Michael Sharpe <m.sharpe@project89.org>
2021-07-13 16:22:15 -0700

    use data socket on switch component

--- src/features/rete/components/SwitchGate.js
+++ src/features/rete/components/SwitchGate.js
@@ -1,6 +1,6 @@
 import Rete from "rete";
 import { anySocket, dataSocket } from "../sockets";
-import { OutputGenerator } from "../controls/OutputGenerator";
+import { OutputGeneratorControl } from "../dataControls/OutputGenerator";
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -18,101 +18,17 @@ export class SwitchGate extends Rete.Component {
 
   node = {};
 
-  // TODO refactor this function into smaller class functions
-  // note: might be possible to abstract this into a parent class to be used by anyone
-  // that wants to make components with dynamic outputs.
   builder(node) {
-    const setOutputs = (outputs) => {
-      // save these to the nodes data
-      node.data.outputs = outputs;
-
-      const existingOutputs = [];
-
-      node.outputs.forEach((out) => {
-        existingOutputs.push(out.key);
-      });
-
-      // Any outputs existing on the current node that arent incoming have been deleted
-      // and need to be removed.
-      existingOutputs
-        .filter((out) => !outputs.includes(out))
-        .forEach((key) => {
-          const output = node.outputs.get(key);
-
-          node
-            .getConnections()
-            .filter((con) => con.output.key === key)
-            .forEach((con) => {
-              this.editor.removeConnection(con);
-            });
-
-          node.removeOutput(output);
-        });
-
-      // any incoming outputs not already on the node are new and will be added.
-      const newOutputs = outputs.filter(
-        (out) => !existingOutputs.includes(out)
-      );
-
-      // From these new outputs, we iterate and add an output socket to the node
-      newOutputs.forEach((output) => {
-        const newOutput = new Rete.Output(
-          output,
-          capitalizeFirstLetter(output),
-          dataSocket
-        );
-        node.addOutput(newOutput);
-      });
-
-      this.task.outputs = node.data.outputs.reduce(
-        (acc, out) => {
-          acc[out] = "option";
-          return acc;
-        },
-        { ...this.task.outputs }
-      );
-
-      node.update();
-    };
-
-    node.onInspector = (data) => {
-      setOutputs(data.outputs);
-      console.log("ON INSPECTOR");
-    };
-
-    const switchControl = new OutputGenerator({
-      defaultOutputs: node.data.outputs || ["default"],
-      setOutputs: (outputs) => setOutputs.call(this, outputs),
-      key: "dynamicOutput",
-    });
+    const outputGenerator = new OutputGeneratorControl(
+      node.data.outputs,
+      "dataSocket"
+    );
+    node.inspector.add(outputGenerator);
 
     const input = new Rete.Input("input", "Input", anySocket);
     const dataInput = new Rete.Input("data", "Data", dataSocket);
 
-    node.addInput(input).addInput(dataInput).addControl(switchControl);
-
-    // Handle outputs in the nodes data to repopulate when loading from JSON
-    if (node.data.outputs && node.data.outputs.length !== 0) {
-      node.data.outputs.forEach((key) => {
-        const output = new Rete.Output(
-          key,
-          capitalizeFirstLetter(key),
-          dataSocket
-        );
-        node.addOutput(output);
-      });
-
-      // Add the data outputs to the tasks outputs
-      if (node.data.outputs && node.data.outputs.length > 0) {
-        this.task.outputs = node.data.outputs.reduce(
-          (acc, out) => {
-            acc[out] = "option";
-            return acc;
-          },
-          { ...this.task.outputs }
-        );
-      }
-    }
+    node.addInput(input).addInput(dataInput);
 
     return node;
   }
