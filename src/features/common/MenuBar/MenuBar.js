ae64d0c8c1a2c326656c0239b10371ae9ad098a7
pgull <preston@latitude.io>
2021-07-13 21:01:38 -0600

    recurse menu items

--- src/features/common/MenuBar/MenuBar.js
+++ src/features/common/MenuBar/MenuBar.js
@@ -7,6 +7,9 @@ import css from "./menuBar.module.css";
 import thothlogo from "./thoth.png";
 
 const MenuBar = ({ tabs }) => {
+
+  //Menu bar functions
+
   const { serialize } = useRete();
   const { saveCurrentSpell } = useSpell();
 
@@ -19,6 +22,10 @@ const MenuBar = ({ tabs }) => {
     const serialized = serialize();
     console.log(JSON.stringify(serialized));
   };
+
+
+  //Menu bar entries
+
 
   const menuBarItems = {
     save: {
@@ -59,13 +66,36 @@ const MenuBar = ({ tabs }) => {
     }
   }
 
+  //Menu bar rendering
+
+  const ListItem = ({ item, label }) => {
+    // console.log(Object.keys(item.items)[index])
+    let children = null;
+    if (item.items && Object.keys(item.items)) {
+      children = (
+        <ul>
+          {Object.keys(item.items).map((i, x) => {
+            return <ListItem item={item?.items[i]} label={Object.keys(item.items)[x]}/>
+          })}
+        </ul>
+      );
+    }
+
+    return (
+      <li>
+        {label && label}
+        {children}
+      </li>
+    );
+  }
+
   return (
-    <div className={css['menu-bar']}>
+    <ul className={css['menu-bar']}>
         <img className={css["thoth-logo"]} alt="Thoth logo" src={thothlogo} />
-        {Object.keys(menuBarItems).map((item, index) => {
-          return <div className={css['menu-bar-item']}> {item} </div>
-        })}
-    </div>
+          {
+            Object.keys(menuBarItems).map((item, index) => <ListItem item={menuBarItems[item]} label={Object.keys(menuBarItems)[index]}/>)
+          }
+    </ul>
   );
 };
 
