797d6ca035dea12e3e09fb74923122f4a14527a6
Michael Sharpe <m.sharpe@project89.org>
2021-07-14 16:26:18 -0700

    Add save shortcut

--- src/features/common/MenuBar/MenuBar.js
+++ src/features/common/MenuBar/MenuBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 
 import { useRete } from "../../../contexts/Rete";
 import { useSpell } from "../../../contexts/Spell";
@@ -7,13 +8,13 @@ import css from "./menuBar.module.css";
 import thothlogo from "./thoth.png";
 
 const MenuBar = ({ tabs }) => {
-
   //Menu bar functions
 
   const { serialize } = useRete();
   const { saveCurrentSpell } = useSpell();
 
   const onSave = () => {
+    console.log("saving");
     const serialized = serialize();
     saveCurrentSpell({ graph: serialized });
   };
@@ -23,23 +24,39 @@ const MenuBar = ({ tabs }) => {
     console.log(JSON.stringify(serialized));
   };
 
+  //Menu bar hotkeys
+
+  useHotkeys("cmd+s, crtl+s", (event) => {
+    event.preventDefault();
+    onSave();
+  });
 
   //Menu bar entries
-
 
   const menuBarItems = {
     file: {
       items: {
         new: {
-          onClick: () => {alert('you clicked new!')}
+          onClick: () => {
+            alert("you clicked new!");
+          },
         },
         save: {
-          onClick: () => {alert('you clicked save!')}
+          onClick: () => {
+            onSave();
+          },
+        },
+        serialize: {
+          onClick: () => {
+            onSerialize();
+          },
         },
         load: {
-          onClick: () => {alert('you clicked load!')}
+          onClick: () => {
+            alert("you clicked load!");
+          },
         },
-      }
+      },
     },
     edit: {
       items: {
@@ -47,7 +64,7 @@ const MenuBar = ({ tabs }) => {
         redo: {},
         copy: {},
         paste: {},
-      }
+      },
     },
     studio: {
       items: {
@@ -62,53 +79,67 @@ const MenuBar = ({ tabs }) => {
                 fewshots: {},
                 serialization: {},
                 preamble: {},
-              }
-            }
+              },
+            },
           },
         },
         change_layout: {
           items: {
             multishot_editing: {},
             enki_fewshot_editing: {},
-            node_editing: {}
-          }
+            node_editing: {},
+          },
         },
-      }
+      },
     },
-  }
+  };
 
   //Menu bar rendering
 
   const ListItem = ({ item, label, topLevel, onClick }) => {
-    label = label.replace(/_/g, ' ')
-    console.log(topLevel ? 'menu-bar-item' : 'list-item')
+    label = label.replace(/_/g, " ");
     let children = null;
     if (item.items && Object.keys(item.items)) {
       children = (
-        <ul className={css['menu-panel']}>
+        <ul className={css["menu-panel"]}>
           {Object.keys(item.items).map((i, x) => {
-            return <ListItem item={item?.items[i]} label={Object.keys(item.items)[x]} topLevel={false} onClick={item?.items[i].onClick}/>
+            return (
+              <ListItem
+                item={item?.items[i]}
+                label={Object.keys(item.items)[x]}
+                topLevel={false}
+                onClick={item?.items[i].onClick}
+              />
+            );
           })}
         </ul>
       );
     }
 
     return (
-      <li className={`${css[topLevel ? 'menu-bar-item' : 'list-item']}`} onClick={onClick}>
+      <li
+        className={`${css[topLevel ? "menu-bar-item" : "list-item"]}`}
+        onClick={onClick}
+      >
         {label}
-        {children && <div className={css['folder-arrow']}> ❯ </div>}
+        {children && <div className={css["folder-arrow"]}> ❯ </div>}
         {!topLevel && <br />}
         {children}
       </li>
     );
-  }
+  };
 
   return (
-    <ul className={css['menu-bar']}>
-        <img className={css["thoth-logo"]} alt="Thoth logo" src={thothlogo} />
-          {
-            Object.keys(menuBarItems).map((item, index) => <ListItem item={menuBarItems[item]} label={Object.keys(menuBarItems)[index]} topLevel={true} onClick={menuBarItems[item].onClick}/>)
-          }
+    <ul className={css["menu-bar"]}>
+      <img className={css["thoth-logo"]} alt="Thoth logo" src={thothlogo} />
+      {Object.keys(menuBarItems).map((item, index) => (
+        <ListItem
+          item={menuBarItems[item]}
+          label={Object.keys(menuBarItems)[index]}
+          topLevel={true}
+          onClick={menuBarItems[item].onClick}
+        />
+      ))}
     </ul>
   );
 };
