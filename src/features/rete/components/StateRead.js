b43b587a1afb27575f8213a0016679a2629fb95f
Michael Sharpe <m.sharpe@project89.org>
2021-07-14 15:21:22 -0700

    Clean up state read

--- src/features/rete/components/StateRead.js
+++ src/features/rete/components/StateRead.js
@@ -10,11 +10,6 @@ export class StateRead extends Rete.Component {
     };
   }
 
-  node = {};
-
-  // TODO refactor this function into smaller class functions
-  // note: might be possible to abstract this into a parent class to be used by anyone
-  // that wants to make components with dynamic outputs.
   builder(node) {
     const outputGenerator = new OutputGeneratorControl(node.data.outputs);
     node.inspector.add(outputGenerator);
