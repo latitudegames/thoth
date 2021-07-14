d2b7f0c8a95ace955188affcf007e63a31c03a0f
Michael Sharpe <m.sharpe@project89.org>
2021-07-14 12:23:20 -0700

    use data key in fewshot control

--- src/features/rete/dataControls/FewshotControl.js
+++ src/features/rete/dataControls/FewshotControl.js
@@ -3,7 +3,7 @@ import { DataControl } from "../plugins/inspectorPlugin";
 export class FewshotControl extends DataControl {
   constructor() {
     const options = {
-      data: "fewshot",
+      dataKey: "fewshot",
       name: "Fewshot",
       controls: {
         component: "longText",
