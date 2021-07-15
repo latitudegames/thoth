fbf5115db12078ad6a26f8f6ac2ad71f59a77426
Sean Gillespie <info@spg.business>
2021-07-15 11:18:34 -0400

    Add services folder to house game-api directory, add entry for Enki with getEnkiPrompt as it's first function export

---
+++ src/services/game-api/enki.js
@@ -0,0 +1,25 @@
+export const getEnkiPrompt = async (taskName) => {
+    // const url = 'https://latitude-game-api.herokuapp.com'
+    const url = "http://localhost:3000";
+    try {
+      const response = await fetch(url + `/enki/${taskName}`, {
+        method: "GET",
+        prompt,
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.REACT_APP_GAME_KEY,
+        },
+      });
+  
+      console.log("response", response);
+      const parsed = await response.text();
+  
+      console.log("parsed", parsed);
+  
+      return parsed;
+    } catch (err) {
+      console.log("fetch error", err);
+    }
+  };
+  
