119a04b5365147d7fd9b9f09655847dbf1829d19
Michael Sharpe <m.sharpe@project89.org>
2021-07-15 00:43:18 -0700

    Process template and run through openAI

--- src/features/rete/components/Generator.js
+++ src/features/rete/components/Generator.js
@@ -1,7 +1,9 @@
 import Rete from "rete";
-import { dataSocket } from "../sockets";
+import Handlebars from "handlebars";
+import { dataSocket, stringSocket } from "../sockets";
 import { InputGeneratorControl } from "../dataControls/InputGenerator";
 import { FewshotControl } from "../dataControls/FewshotControl";
+import { completion } from "../../../utils/openaiHelper";
 
 export class Generator extends Rete.Component {
   constructor() {
@@ -17,11 +19,12 @@ export class Generator extends Rete.Component {
   builder(node) {
     const dataIn = new Rete.Input("data", "Data", dataSocket, true);
     const dataOut = new Rete.Output("data", "Data", dataSocket, true);
+    const resultOut = new Rete.Output("result", "Result", stringSocket, true);
 
     const inputGenerator = new InputGeneratorControl({
       ignored: [
         {
-          name: "data",
+          name: "Data",
           socketType: "dataSocket",
         },
       ],
@@ -33,8 +36,29 @@ export class Generator extends Rete.Component {
 
     node.inspector.add(inputGenerator).add(fewshotControl);
 
-    return node.addInput(dataIn).addOutput(dataOut);
+    return node.addInput(dataIn).addOutput(dataOut).addOutput(resultOut);
   }
 
-  async worker(node, inputs, { element }) {}
+  async worker(node, rawInputs, { element }) {
+    const inputs = Object.entries(rawInputs).reduce((acc, [key, value]) => {
+      acc[key] = value[0];
+      return acc;
+    }, {});
+
+    const template = Handlebars.compile(node.data.fewshot);
+    const prompt = template(inputs);
+
+    const body = {
+      prompt,
+      stop: ["\n"],
+      maxTokens: 100,
+      temperature: 0.0,
+    };
+    const raw = await completion(body);
+    const result = raw.trim();
+
+    return {
+      result,
+    };
+  }
 }
