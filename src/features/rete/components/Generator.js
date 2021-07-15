6b3733ae1d85f8ae75cf9514e64fcdb608b2ac35
Michael Sharpe <m.sharpe@project89.org>
2021-07-15 01:13:46 -0700

    Use input control in generator for optional parameters

--- src/features/rete/components/Generator.js
+++ src/features/rete/components/Generator.js
@@ -2,6 +2,7 @@ import Rete from "rete";
 import Handlebars from "handlebars";
 import { dataSocket, stringSocket } from "../sockets";
 import { InputGeneratorControl } from "../dataControls/InputGenerator";
+import { InputControl } from "../dataControls/InputControl";
 import { FewshotControl } from "../dataControls/FewshotControl";
 import { completion } from "../../../utils/openaiHelper";
 
@@ -34,7 +35,27 @@ export class Generator extends Rete.Component {
       language: "handlebars",
     });
 
-    node.inspector.add(inputGenerator).add(fewshotControl);
+    const stopControl = new InputControl({
+      dataKey: "stop",
+      name: "Stop",
+    });
+
+    const temperatureControl = new InputControl({
+      dataKey: "temp",
+      name: "Temperature",
+    });
+
+    const maxTokenControl = new InputControl({
+      dataKey: "maxTokens",
+      name: "Max Tokens",
+    });
+
+    node.inspector
+      .add(inputGenerator)
+      .add(fewshotControl)
+      .add(stopControl)
+      .add(temperatureControl)
+      .add(maxTokenControl);
 
     return node.addInput(dataIn).addOutput(dataOut).addOutput(resultOut);
   }
@@ -48,13 +69,25 @@ export class Generator extends Rete.Component {
     const template = Handlebars.compile(node.data.fewshot);
     const prompt = template(inputs);
 
+    const stop = node?.data?.stop
+      ? node.data.stop.split(",").map((i) => i.trim())
+      : ["/n"];
+
+    const temperature = node?.data?.temp ? parseFloat(node.data.temp) : 0.0;
+    const maxTokens = node?.data?.maxTokens
+      ? parseFloat(node.data.maxTokens)
+      : 0.0;
+
     const body = {
       prompt,
-      stop: ["\n"],
-      maxTokens: 100,
-      temperature: 0.0,
+      stop,
+      maxTokens,
+      temperature,
     };
     const raw = await completion(body);
+
+    console.log("RAW", raw);
+
     const result = raw.trim();
 
     return {
