b5c77089390b41d0548b58cdf2dbeb91c1560243
Sean Gillespie <info@spg.business>
2021-07-15 11:58:09 -0400

    Add getEnkis to game-api enki service directory

--- src/services/game-api/enki.js
+++ src/services/game-api/enki.js
@@ -23,3 +23,28 @@ export const getEnkiPrompt = async (taskName) => {
     }
   };
   
+
+  export const getEnkis = async () => {
+    // const url = 'https://latitude-game-api.herokuapp.com'
+    const url = "http://localhost:3000";
+    try {
+      const response = await fetch(url + `/enki`, {
+        method: "GET",
+        prompt,
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.REACT_APP_GAME_KEY,
+        },
+      });
+  
+      console.log("response", response);
+      const parsed = await response.text();
+  
+      console.log("parsed", parsed);
+  
+      return parsed;
+    } catch (err) {
+      console.log("fetch error", err);
+    }
+  };
