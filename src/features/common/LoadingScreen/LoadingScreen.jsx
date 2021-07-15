d7642ec27bb16ea8d35f29a99567b739656ea189
pgull <preston@latitude.io>
2021-07-15 13:10:12 -0600

    loading...

--- src/features/common/LoadingScreen/LoadingScreen.jsx
+++ src/features/common/LoadingScreen/LoadingScreen.jsx
@@ -1,3 +1,18 @@
-const LoadingScreen = (props) => <p>Loading...</p>;
+import ankh from "./ankh.gif";
+
+const LoadingScreen = (props) => (
+  <div
+    style={{
+      height: "100%",
+      width: "100%",
+    }}
+  >
+    <div style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center'}}>
+      <img style={{height: 'var(--7)', width: 'var(--7)', display: 'inline-block'}} src={ankh} alt={"loading"} />
+      <p>Please wait...</p>
+    </div>
+    
+  </div>
+);
 
 export default LoadingScreen;
