71164abc0458f5b0bea41f0a1247aaa11d4381d9
Michael Sharpe <m.sharpe@project89.org>
2021-07-15 14:31:50 -0700

    Subscrube to DB tabs in tab manager

--- src/contexts/TabManager.jsx
+++ src/contexts/TabManager.jsx
@@ -1,31 +1,42 @@
-import { useContext, createContext, useEffect } from "react";
-import { useDB } from './Database';
+import { useContext, createContext, useEffect, useState } from "react";
+import { useDB } from "./Database";
 
 const Context = createContext({
+  tabs: [],
+  activeTab: {},
 });
 
 export const useTabManager = () => useContext(Context);
 
 const TabManager = ({ children }) => {
-  const db = useDB();
+  const { db } = useDB();
+
+  const [tabs, setTabs] = useState([]);
+  const [activeTab, setActiveTab] = useState({});
 
   useEffect(() => {
-    if (!db) return
+    if (!db) return;
 
-    (async () => {
-      const 
+    db.tabs.find().$.subscribe((results) => {
+      setTabs(results);
+    });
 
-    })()
-  })
+    db.tabs.findOne({ selector: { active: true } }).$.subscribe((result) => {
+      if (!result) return;
+      console.log("active tab found", result);
+      setActiveTab(result);
+    });
+  });
 
-  const openTab = () => {}
+  // const openTab = () => {};
 
-  const closeTab = () => {}
+  // const closeTab = () => {};
 
-  const switchTab = () => {}
+  // const switchTab = () => {};
 
   const publicInterface = {
-
+    tabs,
+    activeTab,
   };
 
   return (
