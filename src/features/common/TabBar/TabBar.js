769fd49e5a28315700aeefbad2d835d5fdb0adda
Michael Sharpe <m.sharpe@project89.org>
2021-07-14 17:29:01 -0700

    Fix linter errors

--- src/features/common/TabBar/TabBar.js
+++ src/features/common/TabBar/TabBar.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { useRete } from "../../../contexts/Rete";
-import { useSpell } from "../../../contexts/Spell";
 import classnames from "classnames";
 import { VscClose } from "react-icons/vsc";
-import MenuBar from '../MenuBar/MenuBar'
+import MenuBar from "../MenuBar/MenuBar";
 
 import css from "./tabBar.module.css";
 
@@ -25,58 +23,6 @@ const Tab = (props) => {
 };
 
 const TabBar = ({ tabs }) => {
-  const { serialize } = useRete();
-  const { saveCurrentSpell } = useSpell();
-
-  const onSave = () => {
-    const serialized = serialize();
-    saveCurrentSpell({ graph: serialized });
-  };
-
-  const onSerialize = () => {
-    const serialized = serialize();
-    console.log(JSON.stringify(serialized));
-  };
-
-  const menuBarItems = {
-    save: {
-      onClick: onSave
-    },
-    load: {},
-    export: {
-      onClick: onSerialize,
-    },
-    studio: {
-      items: {
-        text_editor: {
-          onClick: null
-        },
-        state_manager: {
-          onClick: null
-        },
-        playtest: {
-          onClick: null
-        },
-        inspector: {
-          onClick: null
-        },
-        enki: {
-          items: {
-            serialization: {
-              onClick: null
-            },
-            preamble: {
-              onClick: null
-            },
-            fewshot_data: {
-              onClick: null
-            },
-          }
-        }
-      }
-    }
-  }
-
   return (
     <div className={css["th-tabbar"]}>
       <div className={css["tabbar-section"]}>
