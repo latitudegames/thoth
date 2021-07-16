5d13445581d9b088aa9f21377a73352fc0b015c4
Michael Sharpe <m.sharpe@project89.org>
2021-07-16 14:51:29 -0700

    Add routes to main app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import "flexlayout-react/style/dark.css";
+import "./dds-globals/dds-globals.css";
+import "./App.css";
 
-import { useRoutes } from "hookrouter";
-
+import { Route, Switch, Redirect } from "wouter";
 import ThothPageWrapper from "./features/common/ThothPage/ThothPageWrapper";
 import Thoth from "./features/Thoth/Thoth";
 import StartScreen from "./features/StartScreen/StartScreen";
 
-import "./dds-globals/dds-globals.css";
-import "./App.css";
 import { useTabManager } from "./contexts/TabManager";
-
-const routes = {
-  "/": () => <Thoth />,
-  "/home": () => <StartScreen />,
-};
+import LoadingScreen from "./features/common/LoadingScreen/LoadingScreen";
 
 function App() {
   // Use our routes
-  const match = useRoutes(routes);
   const { tabs } = useTabManager();
+
+  console.log("rendeirng app");
+
+  if (!tabs) return <LoadingScreen />;
 
   return (
     <ThothPageWrapper tabs={tabs}>
-      {/* TODO better not found page  */}
-      {match || <h1>NOT FOUND</h1>}
+      <Switch>
+        <Route path="/thoth" component={Thoth} />
+        <Route path="/home" component={StartScreen} />
+        <Route path="/">
+          {tabs.length === 0 ? (
+            <Redirect to="/home" />
+          ) : (
+            <Redirect to="/thoth" />
+          )}
+        </Route>
+      </Switch>
     </ThothPageWrapper>
   );
 }
