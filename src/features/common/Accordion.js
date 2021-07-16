cb0b57395a4991a1af7aac22351fd04d4ca7d5b6
Michael Sharpe <m.sharpe@project89.org>
2021-07-16 09:52:19 -0700

    Fix weird bug in accordion

--- src/features/common/Accordion.js
+++ src/features/common/Accordion.js
@@ -6,42 +6,42 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-import css from './accordion.module.css'
-import './accordion-overrides.css'
+import css from "./accordion.module.css";
+import "./accordion-overrides.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
     backgroundColor: "var(--dark-3)",
-    boxShadow: 'none',
-    padding: '0',
+    boxShadow: "none",
+    padding: "0",
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
     fontWeight: theme.typography.fontWeightRegular,
-    margin: 0
+    margin: 0,
   },
   accordionSummary: {
     flexDirection: "row-reverse",
-    margin: 0
+    margin: 0,
   },
   expandIcon: {
     marginRight: 8,
-    padding: 0
+    padding: 0,
   },
   detailsRoot: {
     backgroundColor: "var(--dark-2)",
   },
   summaryContent: {
-    margin: 0
-  }
+    margin: 0,
+  },
 }));
 
 export const SimpleAccordion = (props) => {
   const classes = useStyles();
 
   return (
-    <div className={css['accordion']}>
+    <div className={css["accordion"]}>
       <Accordion square={true} elevation={0}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
@@ -50,7 +50,7 @@ export const SimpleAccordion = (props) => {
           classes={{
             root: classes.root,
             expandIcon: classes.expandIcon,
-            accordionSummaryContent: classes.summaryContent
+            // accordionSummaryContent: classes.summaryContent
           }}
           id="panel1a-header"
         >
