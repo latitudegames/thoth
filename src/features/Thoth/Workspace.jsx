bdbfa01e927d26296ac857cba57780eff69cf3d1
Michael Sharpe <m.sharpe@project89.org>
2021-07-18 15:47:34 -0700

    Refactor into workspace component

---
+++ src/features/Thoth/Workspace.jsx
@@ -0,0 +1,36 @@
+import { Editor } from "../../contexts/Rete";
+import { Layout } from "../../contexts/Layout";
+import StateManager from "./windows/StateManager";
+import Playtest from "./windows/Playtest";
+import Inspector from "./windows/Inspector/Inspector";
+import EditorWindow from "./windows/EditorWindow/EditorWindow";
+
+import TextEditor from "./windows/TextEditor";
+
+const Workspace = ({ tab }) => {
+  const factory = (tab) => {
+    return (node) => {
+      const component = node.getComponent();
+      switch (component) {
+        case "editor":
+          return <Editor />;
+        case "stateManager":
+          return <StateManager node={node} />;
+        case "playtest":
+          return <Playtest />;
+        case "inspector":
+          return <Inspector node={node} />;
+        case "textEditor":
+          return <TextEditor node={node} />;
+        case "editorWindow":
+          return <EditorWindow tab={tab} />;
+        default:
+          return <p></p>;
+      }
+    };
+  };
+
+  return <Layout json={tab.layoutJson} factory={factory(tab)} tab={tab} />;
+};
+
+export default Workspace;
