b719786cc961d6c61efa5dc22752555dab33f8e9
Sean Gillespie <info@spg.business>
2021-07-18 02:08:40 -0400

    Add getEnkiCompletion to enki services, reformat file

--- src/services/game-api/enki.js
+++ src/services/game-api/enki.js
@@ -1,50 +1,75 @@
 export const getEnkiPrompt = async (taskName) => {
-    // const url = 'https://latitude-game-api.herokuapp.com'
-    const url = "http://localhost:3000";
-    try {
-      const response = await fetch(url + `/enki/${taskName}`, {
-        method: "GET",
-        prompt,
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key": process.env.REACT_APP_GAME_KEY,
-        },
-      });
-  
-      console.log("response", response);
-      const parsed = await response.json();
-  
-      console.log("parsed", parsed);
-  
-      return parsed;
-    } catch (err) {
-      console.log("fetch error", err);
-    }
-  };
-  
+  // const url = 'https://latitude-game-api.herokuapp.com'
+  const url = "http://localhost:3000";
+  try {
+    const response = await fetch(url + `/enki/${taskName}`, {
+      method: "GET",
+      prompt,
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": process.env.REACT_APP_GAME_KEY,
+      },
+    });
 
-  export const getEnkis = async () => {
-    // const url = 'https://latitude-game-api.herokuapp.com'
-    const url = "http://localhost:3000";
-    try {
-      const response = await fetch(url + `/enki`, {
-        method: "GET",
-        prompt,
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key": process.env.REACT_APP_GAME_KEY,
-        },
-      });
-  
-      console.log("response", response);
-      const parsed = await response.json();
-  
-      console.log("parsed", parsed);
-  
-      return parsed;
-    } catch (err) {
-      console.log("fetch error", err);
-    }
-  };
+    console.log("response", response);
+    const parsed = await response.json();
+
+    console.log("parsed", parsed);
+
+    return parsed;
+  } catch (err) {
+    console.log("fetch error", err);
+  }
+};
+
+export const getEnkis = async () => {
+  // const url = 'https://latitude-game-api.herokuapp.com'
+  const url = "http://localhost:3000";
+  try {
+    const response = await fetch(url + `/enki`, {
+      method: "GET",
+      prompt,
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": process.env.REACT_APP_GAME_KEY,
+      },
+    });
+
+    console.log("response", response);
+    const parsed = await response.json();
+
+    console.log("parsed", parsed);
+
+    return parsed;
+  } catch (err) {
+    console.log("fetch error", err);
+  }
+};
+
+export const getEnkiCompletion = async (taskName, inputs) => {
+  // const url = 'https://latitude-game-api.herokuapp.com'
+  const url = "http://localhost:3000";
+  try {
+    const response = await fetch(url + `/enki/${taskName}/completion`, {
+      method: "POST",
+      prompt,
+      mode: "cors",
+      body: JSON.stringify({ inputs }),
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": process.env.REACT_APP_GAME_KEY,
+      },
+    });
+
+    console.log("response", response);
+    const parsed = await response.json();
+
+    console.log("parsed", parsed);
+
+    return parsed;
+  } catch (err) {
+    console.log("fetch error", err);
+  }
+};
