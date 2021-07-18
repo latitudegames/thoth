41da6076ec7d91c7133ab07ecec4ab3d3d797054
Michael Sharpe <m.sharpe@project89.org>
2021-07-18 15:40:29 -0700

    Pass tab in to layout factory

--- src/features/Thoth/Thoth.js
+++ src/features/Thoth/Thoth.js
@@ -12,33 +12,43 @@ import { useTabManager } from "../../contexts/TabManager";
 import LoadingScreen from "../common/LoadingScreen/LoadingScreen";
 
 const Thoth = ({ empty, workspace = "default" }) => {
-  const { activeTab } = useTabManager();
+  const { activeTab, tabs } = useTabManager();
 
-  const factory = (node) => {
-    const component = node.getComponent();
-    switch (component) {
-      case "editor":
-        return <Editor />;
-      case "stateManager":
-        return <StateManager node={node} />;
-      case "playtest":
-        return <Playtest />;
-      case "inspector":
-        return <Inspector node={node} />;
-      case "textEditor":
-        return <TextEditor node={node} />;
-      case "editorWindow":
-        return <EditorWindow />;
-      default:
-        return <p></p>;
-    }
+  const factory = (tab) => {
+    return (node) => {
+      const component = node.getComponent();
+      switch (component) {
+        case "editor":
+          return <Editor />;
+        case "stateManager":
+          return <StateManager node={node} />;
+        case "playtest":
+          return <Playtest />;
+        case "inspector":
+          return <Inspector node={node} />;
+        case "textEditor":
+          return <TextEditor node={node} />;
+        case "editorWindow":
+          return <EditorWindow tab={tab} />;
+        default:
+          return <p></p>;
+      }
+    };
   };
 
   if (!activeTab) return <LoadingScreen />;
 
   return (
-    <TabLayout>
-      {!empty && <Layout json={activeTab.layoutJson} factory={factory} />}
+    <TabLayout data-layout>
+      {!empty &&
+        tabs.map((tab, i) => (
+          <Layout
+            json={tab.layoutJson}
+            factory={factory(tab)}
+            tab={tab}
+            key={i}
+          />
+        ))}
     </TabLayout>
   );
 };
