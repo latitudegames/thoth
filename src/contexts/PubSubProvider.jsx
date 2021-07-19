c2b9f1928cda25d87e35294c2b1e248a9f42db9a
Michael Sharpe <m.sharpe@project89.org>
2021-07-19 13:09:29 -0700

    Add events to trigger functionality in the workspace

--- src/contexts/PubSubProvider.jsx
+++ src/contexts/PubSubProvider.jsx
@@ -21,8 +21,14 @@ export const events = {
   TEXT_EDITOR_SET: "textEditorSet",
   SAVE_CURRENT_SPELL: "saveCurrentSpell",
   $NODE_SET: (nodeId) => `nodeSet:${nodeId}`,
-  // gloabl events
+  // app to tab workspace events
+  $SAVE_SPELL: (tabId) => `saveSpell:${tabId}`,
   $CREATE_STATE_MANAGER: (tabId) => `createStateManage:${tabId}`,
+  $CREATE_PLAYTEST: (tabId) => `createPlaytest:${tabId}`,
+  $CREATE_INSPECTOR: (tabId) => `createInspector:${tabId}`,
+  $CREATE_TEXT_EDITOR: (tabId) => `createStateManage:${tabId}`,
+  $SERIALIZE: (tabId) => `serialize:${tabId}`,
+  $EXPORT: (tabId) => `export:${tabId}`,
 };
 
 const PubSubProvider = ({ children }) => {
