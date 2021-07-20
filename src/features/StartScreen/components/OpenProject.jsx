b1f2c85c23e3fa83cfe7a717ea88878039383282
Michael Sharpe <m.sharpe@project89.org>
2021-07-20 01:02:17 -0700

    Finish file import flow.

--- src/features/StartScreen/components/OpenProject.jsx
+++ src/features/StartScreen/components/OpenProject.jsx
@@ -6,7 +6,7 @@ import ProjectRow from "./ProjectRow";
 import FileInput from "./FileInput";
 import thothBanner from "../version-banner-0.0.0beta.jpg";
 import Icon from "../../common/Icon/Icon";
-import { useSpell } from "../../../contexts/SpellProvider";
+import { useSpell, getSpell } from "../../../contexts/SpellProvider";
 import { useLocation } from "wouter";
 import { useTabManager } from "../../../contexts/TabManagerProvider";
 
@@ -20,12 +20,26 @@ const projects = [
 const OpenProject = () => {
   const { tabs } = useTabManager();
   const [selectedProject, setSelectedProject] = useState(null);
-  const { getThothVersion } = useSpell();
+  const { getThothVersion, newSpell, getSpell } = useSpell();
+  const { openTab } = useTabManager();
   // eslint-disable-next-line no-unused-vars
   const [location, setLocation] = useLocation();
 
+  const onReaderLoad = async (event) => {
+    const spellData = JSON.parse(event.target.result);
+    // TODO check for proper values here and throw errors
+
+    let existingSpell = await getSpell(spellData.name);
+    const spell = existingSpell ? existingSpell : await newSpell(spellData);
+
+    await openTab({ name: spell.name, spellId: spell.name });
+  };
+
   const loadFile = (selectedFile) => {
-    alert(`To open: ${selectedFile.name}\nType: ${selectedFile.type}`);
+    const reader = new FileReader();
+    reader.onload = onReaderLoad;
+    console.log("selected file");
+    reader.readAsText(selectedFile);
   };
 
   return (
