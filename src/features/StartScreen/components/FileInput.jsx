983bd85cc972c4a2edd7365f16668cd84e72e163
Michael Sharpe <m.sharpe@project89.org>
2021-07-20 01:02:01 -0700

    File input formatting

--- src/features/StartScreen/components/FileInput.jsx
+++ src/features/StartScreen/components/FileInput.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import Icon from '../../common/Icon/Icon'
+import React from "react";
+import Icon from "../../common/Icon/Icon";
 
-const FileInput = ({ loadFile }) => {  
+const FileInput = ({ loadFile }) => {
   const hiddenFileInput = React.useRef(null);
-  
-  const handleClick = event => {
+
+  const handleClick = (event) => {
     hiddenFileInput.current.click();
   };
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     const fileUploaded = event.target.files[0];
-    console.log(fileUploaded)
-    loadFile(fileUploaded)
-
-  };  return (
+    loadFile(fileUploaded);
+  };
+  return (
     <>
       <button onClick={handleClick}>
-        <Icon name="folder" style={{marginRight: 'var(--extraSmall)'}}/>
+        <Icon name="folder" style={{ marginRight: "var(--extraSmall)" }} />
         Browse...
       </button>
       <input
         type="file"
         ref={hiddenFileInput}
         onChange={handleChange}
-        style={{display: 'none'}}
+        style={{ display: "none" }}
       />
     </>
   );
-}
+};
 
 export default FileInput;
