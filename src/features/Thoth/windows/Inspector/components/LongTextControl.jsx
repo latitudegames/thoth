b01103f5275ef7ec6a704da1a9125dee33f5f7d5
Michael Sharpe <m.sharpe@project89.org>
2021-07-20 21:32:08 -0700

    Move long text into its own component.

---
+++ src/features/Thoth/windows/Inspector/components/LongTextControl.jsx
@@ -0,0 +1,22 @@
+import { usePubSub } from "../../../../../contexts/PubSubProvider";
+import { useLayout } from "../../../../../contexts/LayoutProvider";
+
+const LongText = ({ initialValue, name, dataKey, nodeId }) => {
+  const { events, publish } = usePubSub();
+  const { createOrFocus, windowTypes } = useLayout();
+
+  const onClick = () => {
+    const data = {
+      data: initialValue,
+      nodeId,
+      dataKey,
+      name,
+    };
+    publish(events.TEXT_EDITOR_SET, data);
+    createOrFocus(windowTypes.TEXT_EDITOR, "Text Editor");
+  };
+
+  return <button onClick={onClick}>Open in text editor</button>;
+};
+
+export default LongText;
