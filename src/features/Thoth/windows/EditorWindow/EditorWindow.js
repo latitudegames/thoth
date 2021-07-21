d0e6ecae4c68c9730678e2c478f6ee3e435f8595
pgull <preston@latitude.io>
2021-07-21 08:48:50 -0600

    formatting

--- src/features/Thoth/windows/EditorWindow/EditorWindow.js
+++ src/features/Thoth/windows/EditorWindow/EditorWindow.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
 import { Editor, useRete } from "../../../../contexts/ReteProvider";
 import { createNode } from "rete-context-menu-plugin/src/utils";
-import Select from "../../../common/Select/Select"
+import Select from "../../../common/Select/Select";
 
 import css from "./editorwindow.module.css";
 
@@ -16,33 +16,45 @@ const EditorWindow = ({ tab, ...props }) => {
     return [value, toggle];
   };
 
-  const [menuVisibility, togglemenuVisibility] = useToggle();
   const nodeList = getNodes();
   const nodeMap = getNodeMap();
 
-  const selectRef = useRef(null);
-
   const handleNodeSelect = async (e) => {
-    if(editor) editor.addNode(
-      await createNode(nodeMap.get(e.value), {
-        x: 0,
-        y: 0,
-      })
-    )
-  }
+    if (editor)
+      editor.addNode(
+        await createNode(nodeMap.get(e.value), {
+          x: 0,
+          y: 0,
+        })
+      );
+  };
 
   const getNodeOptions = () => {
-    const arr = []
-    if(nodeList) Object.keys(nodeList).map((item, index) => {
-      return arr.push({label: nodeList[item].name, value: nodeList[item].name})
-    })
-    return arr
-  }
+    const arr = [];
+    if (nodeList)
+      Object.keys(nodeList).map((item, index) => {
+        return arr.push({
+          label: nodeList[item].name,
+          value: nodeList[item].name,
+        });
+      });
+    return arr;
+  };
 
   const EditorToolbar = () => {
     return (
       <>
-      <Select searchable placeholder={'add node...'} onChange={ async (e) => {handleNodeSelect(e)}} options={getNodeOptions()} style={{width: '50%'}} value={null} focusKey="space"/>
+        <Select
+          searchable
+          placeholder={"add node..."}
+          onChange={async (e) => {
+            handleNodeSelect(e);
+          }}
+          options={getNodeOptions()}
+          style={{ width: "50%" }}
+          value={null}
+          focusKey="space"
+        />
       </>
     );
   };
