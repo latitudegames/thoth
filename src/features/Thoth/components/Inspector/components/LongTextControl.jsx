006dc35acedc1e62595ba906eed39cd85bd5b47a
Michael Sharpe <m.sharpe@project89.org>
2021-07-21 15:11:21 -0700

    Rename windows to components

--- src/features/Thoth/components/Inspector/components/LongTextControl.jsx
+++ src/features/Thoth/components/Inspector/components/LongTextControl.jsx
@@ -1,19 +1,9 @@
-import { usePubSub } from "../../../../../contexts/PubSubProvider";
 import { useLayout } from "../../../../../contexts/LayoutProvider";
 
 const LongText = ({ initialValue, name, control, dataKey, nodeId }) => {
-  const { events, publish } = usePubSub();
   const { createOrFocus, windowTypes } = useLayout();
 
   const onClick = () => {
-    const data = {
-      data: initialValue,
-      nodeId,
-      dataKey,
-      name,
-      control,
-    };
-    publish(events.TEXT_EDITOR_SET, data);
     createOrFocus(windowTypes.TEXT_EDITOR, "Text Editor");
   };
 
