1ce0025ac4fa84665ed49ae8bb5a2f8590731e42
Michael Sharpe <m.sharpe@project89.org>
2021-07-20 21:32:08 -0700

    Move long text into its own component.

--- src/features/Thoth/windows/Inspector/components/DataControls.js
+++ src/features/Thoth/windows/Inspector/components/DataControls.js
@@ -1,6 +1,5 @@
 import { SimpleAccordion } from "../../../../common/Accordion";
-import { usePubSub } from "../../../../../contexts/PubSubProvider";
-import { useLayout } from "../../../../../contexts/LayoutProvider";
+import LongText from "./LongTextControl";
 import Input from "./Input";
 import OutputGenerator from "./OutputGenerator";
 import InputGenerator from "./InputGenerator";
@@ -8,24 +7,6 @@ import EnkiSelect from "./EnkiSelect";
 import css from "./datacontrols.module.css";
 
 const StubComponent = (props) => <div>{props.name}</div>;
-
-const LongText = ({ initialValue, name, dataKey, nodeId }) => {
-  const { events, publish } = usePubSub();
-  const { createOrFocus, windowTypes } = useLayout();
-
-  const onClick = () => {
-    const data = {
-      data: initialValue,
-      nodeId,
-      dataKey,
-      name,
-    };
-    publish(events.TEXT_EDITOR_SET, data);
-    createOrFocus(windowTypes.TEXT_EDITOR, "Text Editor");
-  };
-
-  return <button onClick={onClick}>Open in text editor</button>;
-};
 
 const controlMap = {
   enkiSelect: EnkiSelect,
