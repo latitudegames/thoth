c74bbdd03b07d51227d8e5430391bf8d0e43169f
Michael Sharpe <m.sharpe@project89.org>
2021-07-21 14:51:21 -0700

    Move nodeinspectpr pubsub into pre-builder in plugin

--- src/features/rete/plugins/inspectorPlugin/index.js
+++ src/features/rete/plugins/inspectorPlugin/index.js
@@ -3,16 +3,24 @@ import { Inspector } from "./Inspector";
 function install(editor) {
   const { publish, subscribe, events } = editor.pubSub;
 
-  let currentNode = null;
-  let unsubscribe = null;
-
   editor.on("componentregister", (component) => {
     const builder = component.builder;
 
     // we are going to override the default builder with our own, and will invoke the original builder inside it.
     component.builder = (node) => {
+      // This will unsubscribe us
+      if (node.subscription) node.subscription();
       // Inspector class which will handle regsistering data controls, serializing, etc.
       node.inspector = new Inspector({ component, editor, node });
+
+      node.subscription = subscribe(
+        events.$NODE_SET(node.id),
+        (event, data) => {
+          node.inspector.handleData(data);
+          editor.trigger("nodecreated");
+          publish(events.INSPECTOR_SET, node.inspector.data());
+        }
+      );
 
       builder.call(component, node);
     };
@@ -20,24 +28,7 @@ function install(editor) {
 
   // handle publishing and subscribing to inspector
   editor.on("nodeselect", (node) => {
-    if (node === currentNode) return;
-    if (unsubscribe) unsubscribe();
-
-    currentNode = node;
-    // clear spaces first
-    publish(events.TEXT_EDITOR_SET, {});
-
-    // Set inspector
     publish(events.INSPECTOR_SET, node.inspector.data());
-
-    // we set up a subscribe to that nodes channel when it saves data.
-    unsubscribe = subscribe(events.$NODE_SET(node.id), (event, data) => {
-      node.inspector.handleData(data);
-      editor.trigger("nodecreated");
-      publish(events.INSPECTOR_SET, node.inspector.data());
-    });
-
-    // we need to unsibscribe when a new node is selected
   });
 }
 
