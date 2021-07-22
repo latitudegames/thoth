1991a18767af60a01d5e4637bc5a1b25733a72da
Michael Sharpe <m.sharpe@project89.org>
2021-07-22 15:58:46 -0700

    Add icons to all data controls

--- src/features/rete/dataControls/CodeControl.js
+++ src/features/rete/dataControls/CodeControl.js
@@ -1,10 +1,11 @@
 import { DataControl } from "../plugins/inspectorPlugin";
 export class CodeControl extends DataControl {
-  constructor({ dataKey, name }) {
+  constructor({ dataKey, name, icon = "feathers" }) {
     const options = {
       dataKey: dataKey,
       name: name,
       component: "code",
+      icon,
       options: {
         editor: true,
         language: "javascript",
