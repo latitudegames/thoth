fe61ed2ad7aeb746a830a2bbf60982cb6ece9297
Michael Sharpe <m.sharpe@project89.org>
2021-07-22 14:59:39 -0700

    Add debounce utils

---
+++ src/utils/debounce.js
@@ -0,0 +1,22 @@
+export function debounceLeading(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    if (!timer) {
+      func.apply(this, args);
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = undefined;
+    }, timeout);
+  };
+}
+
+export function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
