f1f3703d7a6cbd6d4b7ec5b802f8823fe64b3c36
Michael Sharpe <m.sharpe@project89.org>
2021-07-22 16:00:31 -0700

    Add information to code node

--- src/features/rete/components/Code.js
+++ src/features/rete/components/Code.js
@@ -18,6 +18,11 @@ function process(node, inputs, data) {
 }
 `;
 
+const info = `The code component is your swiss army knife when other components won't cut it.  You can define any number of inputs and outputs on it, and then write a custom worker function.  You have access to the any data plugged into the inputs you created on your component, and can send data out along your outputs.
+
+Please note that the return of your function must be an object whose keys are the same value as the names gives to your output sockets.  The incoming inputs argument is an object whose keys are the names you defined, and are an array.
+`;
+
 export class Code extends Rete.Component {
   constructor() {
     // Name of the component
@@ -28,7 +33,8 @@ export class Code extends Rete.Component {
         trigger: "option",
       },
     };
-    this.category = "Logic"
+    this.category = "Logic";
+    this.info = info;
   }
 
   builder(node) {
