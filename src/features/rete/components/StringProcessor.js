597ee879a606cef3deeb5d6152c7964f05fffa51
Michael Sharpe <m.sharpe@project89.org>
2021-07-23 15:59:55 -0700

    Add info to string processor.

--- src/features/rete/components/StringProcessor.js
+++ src/features/rete/components/StringProcessor.js
@@ -3,6 +3,10 @@ import { stringSocket, triggerSocket } from "../sockets";
 import { OutputGeneratorControl } from "../dataControls/OutputGenerator";
 import { CodeControl } from "../dataControls/CodeControl";
 
+const info = `The String Processor component take s astring as an input and allows you to write a function in the text editor to parse that string in whatever way you need.  You can define any number of outputs which you can pass the result of your parsing out through.
+
+Note that the return value of your function must be an objetc whose keys match the names of your generated output sockets.`;
+
 export class StringProcessor extends Rete.Component {
   constructor() {
     // Name of the component
@@ -12,14 +16,16 @@ export class StringProcessor extends Rete.Component {
       outputs: { trigger: "option" },
     };
     this.category = "Logic";
+    this.info = info;
   }
 
   node = {};
 
   builder(node) {
-
     // Add a default javascript template if the node is new and we don't have one.
-    if(!node.data.code) node.data.code =  "(inputStr) => {\n    return { \"outputKey\": \"outputValue\" }\n}";
+    if (!node.data.code)
+      node.data.code =
+        '(inputStr) => {\n    return { "outputKey": "outputValue" }\n}';
 
     // Rete controls
     const input = new Rete.Input("input", "Input", stringSocket);
@@ -47,10 +53,7 @@ export class StringProcessor extends Rete.Component {
     node.inspector.add(outputGenerator);
     node.inspector.add(codeControl);
 
-    return node
-        .addInput(input)
-        .addInput(triggerIn)
-        .addOutput(triggerOut);
+    return node.addInput(input).addInput(triggerIn).addOutput(triggerOut);
   }
 
   async worker(node, inputs, data) {
@@ -65,8 +68,9 @@ export class StringProcessor extends Rete.Component {
     // but end-users shouldn't be aware of this.  When they write
     // their javascript snippet, it should return a dict with the keys
     // they typed in, then we lower-case the keys for them.
-    const lowerCasedOutputs =
-        Object.keys(outputs).reduce((prev, key) => {return {...prev, [key.toLowerCase()]: outputs[key] }; }, {});
+    const lowerCasedOutputs = Object.keys(outputs).reduce((prev, key) => {
+      return { ...prev, [key.toLowerCase()]: outputs[key] };
+    }, {});
 
     return lowerCasedOutputs;
   }
