2039d901ba81e19fa7c3153a13af1948caba7ec4
Michael Sharpe <m.sharpe@project89.org>
2021-07-23 15:25:00 -0700

    Add info to generator

--- src/features/rete/components/Generator.js
+++ src/features/rete/components/Generator.js
@@ -6,6 +6,12 @@ import { InputControl } from "../dataControls/InputControl";
 import { FewshotControl } from "../dataControls/FewshotControl";
 import { completion } from "../../../utils/openaiHelper";
 
+const info = `The generator component is our general purpose completion component.  You can define any number of inputs, and utilise those inputs in a templating language known as Handlebars.  Any value which is wrapped like {{this}} in double braces will be replaced with the corresponding value coming in to the input with the same name.  This allows you to write almost any fewshot you might need, and input values from anywhere else in your chain.
+
+Controls have also been added which give you control of some of the fundamental settings of the OpenAI completion endpoint, including temperature, max tokens, and your stop sequence.
+
+The componet has two returns.  The composed will output your entire fewshot plus the completion, whereas the result output will only be the result of the completion. `;
+
 export class Generator extends Rete.Component {
   constructor() {
     super("Generator");
@@ -17,6 +23,7 @@ export class Generator extends Rete.Component {
       },
     };
     this.category = "AI/ML";
+    this.info = info;
   }
 
   builder(node) {
