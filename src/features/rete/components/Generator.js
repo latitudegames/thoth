20e62db3b6dfab7e8b2def3b8e91c57e7776fcb8
Michael Sharpe <m.sharpe@project89.org>
2021-07-22 23:19:13 -0700

    Rename input generator on generator component

--- src/features/rete/components/Generator.js
+++ src/features/rete/components/Generator.js
@@ -33,7 +33,7 @@ export class Generator extends Rete.Component {
 
     const inputGenerator = new SocketGeneratorControl({
       connectionType: "input",
-      name: "Input sockets (new)",
+      name: "Input Sockets",
       ignored: ["trigger"],
     });
 
