2f909a06d21ba1b0b2fc0e5b347e66273609e26d
Michael Sharpe <m.sharpe@project89.org>
2021-07-26 16:00:19 -0700

    Pass all props down to children in workspace provider composer

--- src/contexts/WorkspaceProvider.jsx
+++ src/contexts/WorkspaceProvider.jsx
@@ -12,20 +12,27 @@ const providers = [
   ReteProvider,
 ];
 
-function ComposeProviders({ providers, children }) {
+function ComposeProviders({ providers, children, ...parentProps }) {
   const _providers = [...providers].reverse();
   return _providers.reduce((acc, current) => {
     const [Provider, props] = Array.isArray(current)
       ? [current[0], current[1]]
       : [current, {}];
 
-    return <Provider {...props}>{acc}</Provider>;
+    const componentProps = {
+      ...props,
+      ...parentProps,
+    };
+
+    return <Provider {...componentProps}>{acc}</Provider>;
   }, children);
 }
 
 // Centralize all our providers to avoid nesting hell.
-const WorkspaceProviders = ({ children }) => (
-  <ComposeProviders providers={providers}>{children}</ComposeProviders>
+const WorkspaceProviders = ({ children, ...props }) => (
+  <ComposeProviders providers={providers} {...props}>
+    {children}
+  </ComposeProviders>
 );
 
 export default WorkspaceProviders;
