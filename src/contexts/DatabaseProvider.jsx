c97cb8ad477fb390435974f291c1c77fffe3c91e
Michael Sharpe <m.sharpe@project89.org>
2021-07-27 11:59:29 -0700

    Load models into database provider

--- src/contexts/DatabaseProvider.jsx
+++ src/contexts/DatabaseProvider.jsx
@@ -1,32 +1,37 @@
 import { useContext, createContext, useState, useEffect } from "react";
-import { initDB } from "../database";
+import { initDB, loadModels } from "../database";
 
 import LoadingScreen from "../features/common/LoadingScreen/LoadingScreen";
 
 const Context = createContext({
   db: {},
+  models: {},
 });
 
 export const useDB = () => useContext(Context);
 
 const DatabaseProvider = ({ children }) => {
   const [db, setDb] = useState(false);
+  const [models, setModels] = useState(false);
 
   useEffect(() => {
     (async () => {
       if (db) return;
 
       const database = await initDB();
+      const models = loadModels(db);
 
       setDb(database);
+      setModels(models);
     })();
   }, [db]);
 
   const publicInterface = {
     db,
+    models,
   };
 
-  if (!db) return <LoadingScreen />;
+  if (!db && !models) return <LoadingScreen />;
 
   return (
     <Context.Provider value={publicInterface}>{children}</Context.Provider>
