87239c2d37d8c73b3b0f2753ae56f760e09d8fcd
Michael Sharpe <m.sharpe@project89.org>
2021-07-27 14:25:48 -0700

    Populate all modules on module context load

--- src/contexts/ModuleProvider.tsx
+++ src/contexts/ModuleProvider.tsx
@@ -1,15 +1,30 @@
-import { useContext, createContext } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
+import { useDB } from './DatabaseProvider';
 
 const Context = createContext({
-
+  modules: [],
+  newModule: () => {}
 });
 
 export const usePubSub = () => useContext(Context);
 
 const ModuleProvider = ({ children }) => {
+  const [modules, setModules] = useState([])
+  const { models } = useDB();
+
+  // subscribe to all modules in the database
+  useEffect(() => {
+    if (!models) return;
+
+    models.modules.getModules((results => {
+      if (!results) return;
+      setModules(results.map(module => module.toSJON()))
+    }))
+  }, [models])
 
   const publicInterface = {
-
+    modules,
+    ...models.modules
   };
 
   return (
