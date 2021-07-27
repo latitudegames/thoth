9b48282b1dc1384d3f81bfa2f34df1d7a75100ef
Michael Sharpe <m.sharpe@project89.org>
2021-07-26 17:49:42 -0700

    Add rete provider functions

--- src/contexts/ReteProvider.jsx
+++ src/contexts/ReteProvider.jsx
@@ -2,6 +2,11 @@ import { useContext, createContext, useEffect } from "react";
 
 import { usePubSub } from "./PubSubProvider";
 
+/* 
+Some notes here.  The new rete provider, not to be foncused with the old rete provider renamed to the editor provider, is designed to serve as the single source of truth for interfacing with the rete internal system.  This unified interface will lso allow us to replicate the same API in the server, where rete expects certain function to exist but doesn't care what is behind these functions to long as they work.
+Not all functions will be needed on the server, anf functions which are not will be labeled as such.
+*/
+
 const Context = createContext({
   onInspector: () => {},
   onPlayTest: () => {},
@@ -15,18 +20,48 @@ const Context = createContext({
 
 export const useRete = () => useContext(Context);
 
-/* 
-Some notes here.  The new rete provider, not to be foncused with the old rete provider renamed to the editor provider, is designed to serve as the single source of truth for interfacing with the rete internal system.  This unified interface will lso allow us to replicate the same API in the server, where rete expects certain function to exist but doesn't care what is behind these functions to long as they work.
-Not all functions will be needed on the server, anf functions which are not will be labeled as such.
-*/
 const ReteProvider = ({ children, tab }) => {
-  const publicInterface = {};
-
   const { events, publish, subscribe } = usePubSub();
 
-  const onInspector = () => {};
+  const {
+    $PLAYTEST_INPUT,
+    $PLAYTEST_PRINT,
+    $INSPECTOR_SET,
+    $TEXT_EDITOR_CLEAR,
+    $NODE_SET,
+  } = events;
 
-  // We want
+  const onInspector = (node, callback) => {
+    return subscribe($NODE_SET(tab.id, node.id), (event, data) => {
+      callback(data);
+    });
+  };
+
+  const sendToInspector = (data) => {
+    publish($INSPECTOR_SET(tab.id), data);
+  };
+
+  const sendToPlaytest = (data) => {
+    publish($PLAYTEST_PRINT(tab.id), data);
+  };
+
+  const onPlaytest = (callback) => {
+    return subscribe($PLAYTEST_INPUT(tab.id), (event, data) => {
+      callback(data);
+    });
+  };
+
+  const clearTextEditor = () => {
+    publish($TEXT_EDITOR_CLEAR(tab.id));
+  };
+
+  const publicInterface = {
+    onInspector,
+    sendToInspector,
+    sendToPlaytest,
+    onPlaytest,
+    clearTextEditor,
+  };
 
   return (
     <Context.Provider value={publicInterface}>{children}</Context.Provider>
