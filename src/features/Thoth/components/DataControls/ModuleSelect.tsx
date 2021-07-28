c27e0465f3aac6ddc757c8be6ff82e7a0a4f23e2
Michael Sharpe <m.sharpe@project89.org>
2021-07-27 18:14:00 -0700

    Create module select component

--- src/features/Thoth/components/DataControls/ModuleSelect.tsx
+++ src/features/Thoth/components/DataControls/ModuleSelect.tsx
@@ -1,5 +1,47 @@
+import Select from "../../../common/Select/Select";
+import { useModule } from "../../../../contexts/ModuleProvider";
+
 const ModuleSelect = () => {
-  return <p>Hello world!</p>;
+  const { modules, newModule } = useModule();
+
+  const optionArray = () => {
+    return modules.map((module, index) => ({
+      value: module.name,
+      label: module.name,
+    }));
+  };
+
+  const onChange = (value) => {
+    console.log("on change", value);
+  };
+
+  const onCreateOption = (value) => {
+    console.log("on create option", value);
+  };
+
+  const noOptionsMessage = (inputValue) => {
+    return <span>Start typing to create a module</span>;
+  };
+
+  const isValidNewOption = (inputValue, selectValue, selectOptions) => {
+    return inputValue.length !== 0 && selectOptions.length === 0;
+  };
+
+  return (
+    <div style={{ flex: 1 }}>
+      <Select
+        searchable
+        creatable
+        createOptionPosition="top"
+        isValidNewOption={isValidNewOption}
+        noOptionsMessage={noOptionsMessage}
+        options={optionArray()}
+        onChange={onChange}
+        onCreateOption={onCreateOption}
+        placeholder="search modules"
+      />
+    </div>
+  );
 };
 
 export default ModuleSelect;
