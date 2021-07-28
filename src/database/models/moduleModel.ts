2577b213a6942d551262f0ec298c186ca46b4236
Michael Sharpe <m.sharpe@project89.org>
2021-07-27 21:43:03 -0700

    Add newModule to module model

--- src/database/models/moduleModel.ts
+++ src/database/models/moduleModel.ts
@@ -1,11 +1,23 @@
-const loadModuleModel = db => {
+import { v4 as uuidv4 } from "uuid";
+
+const loadModuleModel = (db) => {
   const getModules = async (callback) => {
-    const query = db.modules.find()
-    return callback ? query.$.subscribe(callback) : query.exec()
-  }
+    const query = db.modules.find();
+    return callback ? query.$.subscribe(callback) : query.exec();
+  };
+
+  const newModule = ({ name }) => {
+    const newModule = {
+      name,
+      id: uuidv4(),
+    };
+
+    return db.modules.insert(newModule);
+  };
 
   return {
-    getModules
-  }
-}
+    getModules,
+    newModule,
+  };
+};
 export default loadModuleModel;
