2d9f3aabed37c4f0398b93f47a80a0d6582f4838
Michael Sharpe <m.sharpe@project89.org>
2021-07-29 10:43:38 -0700

    Add silent to playtest area

--- src/features/rete/components/PlaytestInput.js
+++ src/features/rete/components/PlaytestInput.js
@@ -57,8 +57,8 @@ export class PlaytestInput extends Rete.Component {
 
   // the worker contains the main business logic of the node.  It will pass those results
   // to the outputs to be consumed by any connecte components
-  worker(node, inputs, outputs, { data }) {
-    node.display(data);
+  worker(node, inputs, outputs, { data, silent }) {
+    if (!silent) node.display(data);
 
     return {
       text: data,
