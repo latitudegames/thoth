d03adc04c3c8c31ae78f27d720f04c4dc0b08558
Michael Sharpe <m.sharpe@project89.org>
2021-07-29 11:33:10 -0700

    Aadd default write of false to module control

--- src/features/rete/dataControls/ModuleControl.ts
+++ src/features/rete/dataControls/ModuleControl.ts
@@ -1,11 +1,12 @@
 import { DataControl } from "../plugins/inspectorPlugin";
 
 export class ModuleControl extends DataControl {
-  constructor({ name, icon = "sieve" }) {
+  constructor({ name, icon = "sieve", write = false }) {
     const options = {
       dataKey: "module",
       name: name,
       component: "moduleSelect",
+      write,
       icon,
     };
 
