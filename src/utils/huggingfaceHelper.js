102768f8abffab92a3549e8670036531fe620df5
Michael Sharpe <m.sharpe@project89.org>
2021-08-12 23:30:58 -0700

    Add huggingface helper

---
+++ src/utils/huggingfaceHelper.js
@@ -0,0 +1,14 @@
+export const invokeInference = async (model, data) => {
+  const API_TOKEN = process.env.REACT_APP_HUGGINGFACE_API_TOKEN;
+
+  const response = await fetch(
+    `https://api-inference.huggingface.co/models/${model}`,
+    {
+      headers: { Authorization: `Bearer ${API_TOKEN}` },
+      method: "POST",
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  return result;
+};
