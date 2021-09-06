f084e6c2ea1176d6431803d713c16b594a38c707
Sean Gillespie <info@spg.business>
2021-09-06 17:40:12 -0400

    comment correction for editor.on

--- src/features/rete/editor.ts
+++ src/features/rete/editor.ts
@@ -200,7 +200,7 @@ const editor = async function ({ container, pubSub, thoth, tab, thothV2 }) {
     async () => {
       // Here we would swap out local processing for an endpoint that we send the serialised JSON too.
       // Then we run the fewshots, etc on the backend rather than on the client.
-      // Alterative for now is for the client to call our own /openai endpoint.
+      // Alternative for now is for the client to call our own /openai endpoint.
       // NOTE need to consider authentication against Latitude API from a web client
       await engine.abort();
       await engine.process(editor.toJSON(), null, { thoth: thothV2 });
