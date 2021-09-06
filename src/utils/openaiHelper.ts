a3f1141b383015348eb657783ce0e4aa1b7e6918
Sean Gillespie <info@spg.business>
2021-09-06 18:57:26 -0400

    Add ModelCompletionOpts from server

--- src/utils/openaiHelper.ts
+++ src/utils/openaiHelper.ts
@@ -1,4 +1,23 @@
-export const completion = async (body) => {
+
+export type ModelCompletionOpts = {
+  model: string
+  prompt?: string
+  maxTokens?: number
+  temperature?: number
+  topP?: number
+  n?: number
+  stream?: boolean
+  logprobs?: number
+  echo?: boolean
+  stop?: string | string[]
+  presencePenalty?: number
+  frequencyPenalty?: number
+  bestOf?: number
+  user?: string
+  logitBias?: { [token: string]: number }
+}
+
+export const completion = async (body: ModelCompletionOpts) => {
   const url = process.env.REACT_APP_API_URL;
 
   try {
@@ -9,7 +28,7 @@ export const completion = async (body) => {
         "Content-Type": "application/json",
         "x-api-key": process.env.REACT_APP_GAME_KEY || "",
       },
-      body: JSON.stringify({ ...body, prompt: body.prompt.trimEnd() }),
+      body: JSON.stringify({ ...body, prompt: body.prompt?.trimEnd() }),
     });
     const parsedResponse = await response.json();
     const { result } = parsedResponse;
