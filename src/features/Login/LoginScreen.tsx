fe47b55d066b2130f26cf089ef04ea48b37f8277
Michael Sharpe <m.sharpe@project89.org>
2021-09-07 14:14:35 -0700

    Add modal to login screen

--- src/features/Login/LoginScreen.tsx
+++ src/features/Login/LoginScreen.tsx
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
+
 import css from "./LoginScreen.module.css";
 
+import { useModal } from "../../contexts/ModalProvider";
+
 const LoginScreen = () => {
+  const { openModal } = useModal();
+
+  const onClose = () => {
+    alert("closed");
+  };
+
+  useEffect(() => {
+    openModal({
+      modal: "loginModal",
+      content: "TESTING",
+      title: "Login",
+      onClose,
+    });
+  }, []);
+
   return <div className={css["overlay"]}></div>;
 };
 
