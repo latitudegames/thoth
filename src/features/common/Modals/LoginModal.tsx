5636660b5d45408ef802160d95de281b4da32168
Michael Sharpe <m.sharpe@project89.org>
2021-09-07 15:16:21 -0700

    Pass down options into login modal

--- src/features/common/Modals/LoginModal.tsx
+++ src/features/common/Modals/LoginModal.tsx
@@ -1,8 +1,8 @@
 import Modal from "../Modal/Modal";
 
-const LoginModal = ({ title, content, onClose }) => {
+const LoginModal = ({ title, content, onClose, options }) => {
   return (
-    <Modal title={title} icon="info" onClose={onClose}>
+    <Modal title={title} icon="info" onClose={onClose} options={options}>
       <p style={{ whiteSpace: "pre-line" }}> {content} </p>
     </Modal>
   );
