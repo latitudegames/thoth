1aca955ccfb5038faf45d2312a3600b0fa0361a2
Michael Sharpe <m.sharpe@project89.org>
2021-09-07 16:57:52 -0700

    Default style prop

--- src/features/common/Input/Input.tsx
+++ src/features/common/Input/Input.tsx
@@ -1,4 +1,4 @@
-const Input = (value, onChange, style) => {
+const Input = ({ value, onChange, style = {} }) => {
   return <input style={style} value={value} type="text" onChange={onChange} />;
 };
 
