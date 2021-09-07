086df61db02d0351dd63220d562856220895164e
Michael Sharpe <m.sharpe@project89.org>
2021-09-07 14:13:45 -0700

    Add login modal to modals map

--- src/features/common/Modals/index.ts
+++ src/features/common/Modals/index.ts
@@ -1,7 +1,12 @@
 import ExampleModal from "./ExampleModal";
 import InfoModal from "./InfoModal";
+import LoginModal from "./LoginModal";
 
-const modals = { example: ExampleModal, infoModal: InfoModal };
+const modals = {
+  example: ExampleModal,
+  infoModal: InfoModal,
+  loginModal: LoginModal,
+};
 
 export const getModals = () => {
   return modals;
