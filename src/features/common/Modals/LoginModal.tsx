e8ff024ff6ab1dad59e5ec5a65650fe46ce111fe
Michael Sharpe <m.sharpe@project89.org>
2021-09-07 16:58:20 -0700

    Use input component in login screen

--- src/features/common/Modals/LoginModal.tsx
+++ src/features/common/Modals/LoginModal.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Modal from "../Modal/Modal";
+import InputComponent from "../Input/Input";
 
-const LoginModal = ({ title, content, onClose, options }) => {
+const LoginModal = ({ title, onClose, options }) => {
+  const [value, setValue] = useState("");
+
+  const inputStyle = {
+    flex: 6,
+    marginTop: 25,
+  };
+
   return (
     <Modal title={title} icon="info" onClose={onClose} options={options}>
-      <p style={{ whiteSpace: "pre-line" }}> {content} </p>
+      <InputComponent value={value} onChange={setValue} style={inputStyle} />
     </Modal>
   );
 };
