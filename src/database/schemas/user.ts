8b85b69cd7d4a625dd364cbad3075d5535f27fe8
Michael Sharpe <m.sharpe@project89.org>
2021-09-08 17:04:37 -0700

    Add user schema

---
+++ src/database/schemas/user.ts
@@ -0,0 +1,23 @@
+const userSchema = {
+  title: "user schema",
+  version: 0,
+  description: "The main user object",
+  type: "object",
+  properties: {
+    id: {
+      type: "string",
+      primary: true,
+    },
+    authData: {
+      type: "string",
+    },
+  },
+};
+
+const collection = {
+  settings: {
+    schema: userSchema,
+  },
+};
+
+export default collection;
