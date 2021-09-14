cea65d19cc1d33b60c0627ea1f518b2508110d3c
Sean Gillespie <info@spg.business>
2021-09-14 09:55:54 -0400

    Add engine related types to core

--- core/types.ts
+++ core/types.ts
@@ -1,3 +1,5 @@
+import { Component } from "rete";
+
 export type ModuleType = {
     id: String;
     name: string;
@@ -5,3 +7,80 @@ export type ModuleType = {
     createdAt: number;
     updatedAt: number;
   };
+
+export type ModelCompletionOpts = {
+    model: string
+    prompt?: string
+    maxTokens?: number
+    temperature?: number
+    topP?: number
+    n?: number
+    stream?: boolean
+    logprobs?: number
+    echo?: boolean
+    stop?: string | string[]
+    presencePenalty?: number
+    frequencyPenalty?: number
+    bestOf?: number
+    user?: string
+    logitBias?: { [token: string]: number }
+  }
+  
+  export type OpenAIResultChoice = {
+    text: string,
+    index: number,
+    logprobs: number[],
+    "top_logprobs": any[],
+    "text_offset": number[]
+  }
+
+  export type  OpenAIResponse =  {
+    id: string,
+    object: string,
+    created: number,
+    model: string,
+    choices: OpenAIResultChoice[]
+    "finish_reason": string
+  }
+
+  export type ModuleComponent = Component & {
+    run: Function
+  };
+  
+  export type NodeConnections = {
+    node: number,
+    input?: string,
+    output?: string,
+    data: Record<string, unknown>
+  }
+  
+  export type NodeOutputs = {
+    output?: {
+      connections: NodeConnections[]
+    },
+    trigger?: {
+      connections: NodeConnections[]
+    },
+    action?: {
+      connections: NodeConnections[]
+    }
+  }
+  
+  export type NodeData = {
+    socketKey?: string,
+    [DataKey: string]: unknown
+  }
+  
+  export type Node = {
+    id: number,
+    data: NodeData,
+    name: string,
+    inputs: NodeOutputs,
+    outputs?: NodeOutputs,
+    position: number[]
+  }
+  
+  export type Spell = {
+    id: string,
+    nodes: Record<number, Node>
+  }
