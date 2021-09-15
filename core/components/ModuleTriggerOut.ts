d847db046831dc28e6e52086328764a087dee116
Sean Gillespie <info@spg.business>
2021-09-15 17:19:08 -0400

    Update ModuleTriggerOuts.ts with custom classes + types

--- core/components/ModuleTriggerOut.ts
+++ core/components/ModuleTriggerOut.ts
@@ -3,7 +3,7 @@ import { InputControl } from "../dataControls/InputControl";
 import { triggerSocket } from "../sockets";
 import { v4 as uuidv4 } from "uuid";
 import { ThothComponent, ThothTask } from "../thoth-component"
-import { ThothNode,  } from "../types";
+import { ThothNode, } from "../types";
 const info = `The module trigger out component adds a trigger out socket to the parent module.  It can be given a name, which is displayed on the parent.`;
 
 export class ModuleTriggerOut extends ThothComponent {
@@ -38,7 +38,7 @@ export class ModuleTriggerOut extends ThothComponent {
   // the builder is used to "assemble" the node component.
   // when we have enki hooked up and have grabbed all few shots, we would use the builder
   // to generate the appropriate inputs and outputs for the fewshot at build time
-  builder(node:ThothNode) {
+  builder(node: ThothNode) {
     // create inputs here. First argument is the name, second is the type (matched to other components sockets), and third is the socket the i/o will use
     const input = new Rete.Input("trigger", "Trigger", triggerSocket);
 
