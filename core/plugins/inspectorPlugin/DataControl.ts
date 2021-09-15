565835d2629a2b4d3a4ba38bcf1cbd162ac6832a
Sean Gillespie <info@spg.business>
2021-09-15 15:06:05 -0400

    Update types for DataControl.ts

--- core/plugins/inspectorPlugin/DataControl.ts
+++ core/plugins/inspectorPlugin/DataControl.ts
@@ -1,6 +1,8 @@
 import { Inspector } from "./Inspector";
 import { Node, NodeEditor, Component } from "rete";
-
+export type RestProps = {
+  onData: () => void
+}
 export class DataControl {
   inspector: Inspector | null = null;
   editor: NodeEditor | null = null;
@@ -25,6 +27,15 @@ export class DataControl {
     write = true,
     icon = "ankh",
     ...rest
+  }: {
+    dataKey: string,
+    name: string,
+    component: string
+    data: Record<string, unknown>,
+    options: Record<string, unknown>,
+    write: boolean,
+    icon: string,
+    onData?: () => Promise<void>
   }) {
     if (!dataKey) throw new Error(`Data key is required`);
     if (!name) throw new Error(`Name is required`);
@@ -53,12 +64,12 @@ export class DataControl {
     };
   }
 
-  async onAdd(data) {
+  async onAdd() {
     return;
   }
 
   // stub function
-  async onData(data) {
+  async onData() {
     return;
   }
 }
