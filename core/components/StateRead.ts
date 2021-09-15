78450ff6b103d2fbafadcce149129a08e68f7db5
Sean Gillespie <info@spg.business>
2021-09-15 11:00:05 -0400

    Update StateRead.ts with custom classes + types

--- core/components/StateRead.ts
+++ core/components/StateRead.ts
@@ -1,8 +1,9 @@
-import { ThothReteComponent } from "./ThothReteComponent";
+import { ThothComponent } from "../thoth-component"
 import { SocketGeneratorControl } from "../dataControls/SocketGenerator";
-
+import { ThothNode, ThothWorkerInputs, ThothWorkerOutputs } from "../types";
+import { EngineContext } from "../engine";
 const info = `The State Read component allows you to read values from the state.  These can be found in and are managed by the State Manager window.  This window consists of a JSON object.  You can define any number ouf outputs where an outputs name corresponds to a key in the state manager.  Whatever value is assigned to that key will be read ans passed into your chain.`;
-export class StateRead extends ThothReteComponent {
+export class StateRead extends ThothComponent {
   constructor() {
     // Name of the component
     super("State Read");
@@ -15,7 +16,7 @@ export class StateRead extends ThothReteComponent {
     this.info = info;
   }
 
-  builder(node) {
+  builder(node: ThothNode) {
     const outputGenerator = new SocketGeneratorControl({
       connectionType: "output",
       name: "Output sockets",
@@ -28,16 +29,18 @@ export class StateRead extends ThothReteComponent {
 
   // the worker contains the main business logic of the node.  It will pass those results
   // to the outputs to be consumed by any connected components
-  async worker(node, inputs, outputs, { silent, thoth }) {
+  async worker(node: ThothNode, inputs: ThothWorkerInputs, outputs: ThothWorkerOutputs, { silent, thoth }: { silent: boolean, thoth: EngineContext }) {
     const { getCurrentGameState } = thoth
     const gameState = await getCurrentGameState();
 
     return Object.entries(gameState).reduce((acc, [key, value]) => {
-      if (node.data.outputs.some((out) => out.name === key)) {
+
+      const nodeOutputs = node.data.outputs as { name: string, [key: string]: unknown }[]
+      if (nodeOutputs.some((out) => out.name === key)) {
         acc[key] = value;
       }
 
       return acc;
-    }, {});
+    }, {} as { [key: string]: unknown });
   }
 }
