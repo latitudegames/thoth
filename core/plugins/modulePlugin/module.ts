a9b38bf6e8006534f1830e82e957e90ffdc1c3ac
Sean Gillespie <info@spg.business>
2021-09-15 16:07:27 -0400

    Update types for module.ts

--- core/plugins/modulePlugin/module.ts
+++ core/plugins/modulePlugin/module.ts
@@ -1,26 +1,26 @@
 export class Module {
-  inputs: object;
-  outputs: object;
+  inputs: Record<string, unknown>;
+  outputs: Record<string, unknown>;
   constructor() {
     this.inputs = {};
     this.outputs = {};
   }
 
-  read(inputs) {
+  read(inputs: Record<string, unknown>) {
     this.inputs = inputs;
   }
 
-  write(outputs) {
+  write(outputs: Record<string, unknown>) {
     Object.keys(this.outputs).forEach((key) => {
       outputs[key] = this.outputs[key];
     });
   }
 
-  getInput(key) {
+  getInput(key: string) {
     return this.inputs[key];
   }
 
-  setOutput(key, value) {
+  setOutput(key: string, value: unknown) {
     this.outputs[key] = value;
   }
 }
