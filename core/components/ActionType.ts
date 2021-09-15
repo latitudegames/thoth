7dbbda07df2b3938b70b8620506a1c7cc77bdebb
Sean Gillespie <info@spg.business>
2021-09-15 10:23:58 -0400

    Update ActionType.ts with custom classes + types

--- core/components/ActionType.ts
+++ core/components/ActionType.ts
@@ -1,7 +1,8 @@
 import Rete from "rete";
-import { ThothReteComponent } from "./ThothReteComponent";
+import {ThothComponent} from "../thoth-component"
 import { stringSocket, triggerSocket } from "../sockets";
 import { FewshotControl } from "../dataControls/FewshotControl";
+import { ThothNode, ThothWorkerInputs, ThothWorkerOutputs } from "../types";
 
 const fewshot = `Given an action classify the type of action it is
 
@@ -27,7 +28,7 @@ const info = `The Action type component will take in an action as text, and atte
 
 look, get, use, craft, dialog, movement, travel, combat, consume, other.`;
 
-export class ActionTypeComponent extends ThothReteComponent {
+export class ActionTypeComponent extends ThothComponent {
   constructor() {
     // Name of the component
     super("Action Type Classifier");
@@ -43,7 +44,7 @@ export class ActionTypeComponent extends ThothReteComponent {
   // the builder is used to "assemble" the node component.
   // when we have enki hooked up and have grabbed all few shots, we would use the builder
   // to generate the appropriate inputs and ouputs for the fewshot at build time
-  builder(node) {
+  builder(node: ThothNode) {
     node.data.fewshot = fewshot;
     // create inputs here. First argument is the name, second is the type (matched to other components sockets), and third is the socket the i/o will use
     const inp = new Rete.Input("action", "Action", stringSocket);
@@ -64,10 +65,11 @@ export class ActionTypeComponent extends ThothReteComponent {
 
   // the worker contains the main business logic of the node.  It will pass those results
   // to the outputs to be consumed by any connsected components
-  async worker(node, inputs, outputs, { silent, thoth }) {
+  async worker(node: ThothNode, inputs: ThothWorkerInputs, outputs: ThothWorkerOutputs, { silent, thoth }:{silent:boolean, thoth:any}) {
     const { completion } = thoth;
     const action = inputs["action"][0];
-    const prompt = node.data.fewshot + action + ",";
+    const fewshot = node.data.fewshot as string
+    const prompt = fewshot + action + ",";
 
     const body = {
       prompt,
