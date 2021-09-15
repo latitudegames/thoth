055566ec597fc870dc59f7c740a6caa841c576b2
Sean Gillespie <info@spg.business>
2021-09-15 16:06:57 -0400

    Update SwitchGate.ts with custom classes + types

--- core/components/SwitchGate.ts
+++ core/components/SwitchGate.ts
@@ -1,22 +1,22 @@
 import Rete from "rete";
-import { ThothReteComponent } from "./ThothReteComponent";
 import { anySocket, triggerSocket } from "../sockets";
 import { SocketGeneratorControl } from "../dataControls/SocketGenerator";
-
+import { ThothComponent } from "../thoth-component"
+import { ThothNode, ThothWorkerInputs } from "../types";
 // function capitalizeFirstLetter(string) {
 //   return string.charAt(0).toUpperCase() + string.slice(1);
 // }
 
 const info = `The Switch Gate component takes a single input, and allows you to define any number of outputs.  Its works the same as the javascript switch.  The component will try to match the value of the input to one of the output socketnames you have created.  It will route the trigger signal through that socket.`;
 
-export class SwitchGate extends ThothReteComponent {
+export class SwitchGate extends ThothComponent {
   constructor() {
     // Name of the component
     super("Switch");
 
     this.task = {
       outputs: { default: "option" },
-      init: (task) => {},
+      init: (task) => { },
     };
     this.category = "Logic";
     this.info = info;
@@ -24,7 +24,7 @@ export class SwitchGate extends ThothReteComponent {
 
   node = {};
 
-  builder(node) {
+  builder(node: ThothNode) {
     const outputGenerator = new SocketGeneratorControl({
       connectionType: "output",
       ignored: ["default"],
@@ -37,7 +37,7 @@ export class SwitchGate extends ThothReteComponent {
 
     const input = new Rete.Input("input", "Input", anySocket);
     const dataInput = new Rete.Input("trigger", "Trigger", triggerSocket);
-    const defaultOutput = new Rete.Input("default", "Default", triggerSocket);
+    const defaultOutput = new Rete.Output("default", "Default", triggerSocket);
 
     node.addInput(input).addInput(dataInput).addOutput(defaultOutput);
 
@@ -46,8 +46,8 @@ export class SwitchGate extends ThothReteComponent {
 
   // the worker contains the main business logic of the node.  It will pass those results
   // to the outputs to be consumed by any connected components
-  async worker(node, inputs, data) {
-    const input = inputs["input"][0];
+  async worker(node: ThothNode, inputs: ThothWorkerInputs) {
+    const input = inputs["input"][0] as string;
 
     // close all outputs
     // this._task.closed = node.data.outputs.map((out) => out.name);
