bfb02965bb9b4dcb782a32dcbd2c70979a85f5ae
Sean Gillespie <info@spg.business>
2021-09-15 13:12:09 -0400

    Update JoinList.ts with custom classes + types

--- core/components/JoinList.ts
+++ core/components/JoinList.ts
@@ -1,11 +1,11 @@
 import Rete from "rete";
-import { ThothReteComponent } from "./ThothReteComponent";
 import { TextInputControl } from "../controls/TextInputControl";
 import { stringSocket, arraySocket } from "../sockets";
-
+import { ThothComponent } from "../thoth-component"
+import { ThothNode, ThothWorkerInputs, ThothWorkerOutputs } from "../types";
 const info = `The Join List component takes in an array, and will join each item in the array together with a seperator, defined in the components input field.`;
 
-export class JoinListComponent extends ThothReteComponent {
+export class JoinListComponent extends ThothComponent {
   constructor() {
     // Name of the component
     super("Join List");
@@ -15,7 +15,7 @@ export class JoinListComponent extends ThothReteComponent {
         text: "output",
         trigger: "option",
       },
-      init: (task) => {},
+      init: (task) => { },
     };
 
     this.category = "Logic";
@@ -25,14 +25,14 @@ export class JoinListComponent extends ThothReteComponent {
   // the builder is used to "assemble" the node component.
   // when we have enki hooked up and have grabbed all few shots, we would use the builder
   // to generate the appropriate inputs and ouputs for the fewshot at build time
-  builder(node) {
+  builder(node: ThothNode) {
     // create inputs here. First argument is the name, second is the type (matched to other components sockets), and third is the socket the i/o will use
     const out = new Rete.Output("text", "String", stringSocket);
 
     const inputList = new Rete.Input("list", "List", arraySocket);
 
     // Handle default value if data is present
-    const separator = node.data.separator ? node.data.separator : " ";
+    const separator = node.data.separator ? node.data.separator as string : " ";
 
     // controls are the internals of the node itself
     // This default control sample has a text field.
@@ -47,9 +47,9 @@ export class JoinListComponent extends ThothReteComponent {
 
   // the worker contains the main business logic of the node.  It will pass those results
   // to the outputs to be consumed by any connected components
-  async worker(node, inputs, outputs) {
+  async worker(node: ThothNode, inputs: ThothWorkerInputs & { list: [string][] }, outputs: ThothWorkerOutputs) {
     return {
-      text: inputs.list[0].join(node.data.separator),
+      text: inputs.list[0].join(node.data.separator as string),
     };
   }
 }
