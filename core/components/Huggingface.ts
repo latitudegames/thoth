26eccd0e47c5485f89488a3a5456d75fae4d0487
Sean Gillespie <info@spg.business>
2021-09-15 13:02:02 -0400

    Cluster custom thoth imports in Huggingface.ts

--- core/components/Huggingface.ts
+++ core/components/Huggingface.ts
@@ -1,18 +1,19 @@
 import Rete from "rete";
-import { ThothReteComponent } from "./ThothReteComponent";
 import Handlebars from "handlebars";
 import { triggerSocket, stringSocket } from "../sockets";
 import { SocketGeneratorControl } from "../dataControls/SocketGenerator";
 import { InputControl } from "../dataControls/InputControl";
 import { FewshotControl } from "../dataControls/FewshotControl";
-
+import { ThothComponent } from "../thoth-component"
+import { ThothNode, ThothWorkerInputs, ThothWorkerOutputs } from "../types";
+import { EngineContext } from "../engine";
 const info = `The huggingface component is used to access models on huggingface.co.  For now it is very simple.  You define a number of inputs with the input generator, and you can use those in forming the request to your huggingface inference model.  You input the name of the mode from hugginface into the model name field, and you run it.  It will call the model, and return the result.
 
 NOTE:  Hugginface models are on deman, and sometimes require time to "boot up".  We have tried to trigger an initial request the cause the model to load in th ebackground while you and working, but this will not always be done in time. If it is not done, we will notify you via the "error" trigger out.
 
 Also note that you will likely need to parse the return from huggingfacwe yourself inside a code component, or similar.`;
 
-export class HuggingfaceComponent extends ThothReteComponent {
+export class HuggingfaceComponent extends ThothComponent {
   constructor() {
     super("Huggingface");
     this.task = {
@@ -26,7 +27,7 @@ export class HuggingfaceComponent extends ThothReteComponent {
     this.info = info;
   }
 
-  builder(node) {
+  builder(node: ThothNode) {
     const triggerIn = new Rete.Input("trigger", "Trigger", triggerSocket);
     const triggerOut = new Rete.Output("trigger", "Trigger", triggerSocket);
     const errorOut = new Rete.Output("error", "Error", triggerSocket);
@@ -69,25 +70,25 @@ export class HuggingfaceComponent extends ThothReteComponent {
     return node;
   }
 
-  async worker(node, rawInputs, outputs, { thoth }) {
+  async worker(node: ThothNode, rawInputs: ThothWorkerInputs, outputs: ThothWorkerOutputs, { thoth }: { silent: boolean, thoth: EngineContext }) {
     this._task.closed = ["error"];
 
     const stringInputs = rawInputs as { [key: string]: string[] };
     const inputs = Object.entries(stringInputs).reduce((acc, [key, value]) => {
       acc[key] = value[0];
       return acc;
-    }, {});
+    }, {} as Record<string, string>);
 
     const string = node.data.request || "";
     const template = Handlebars.compile(string);
     const request = template(inputs);
-    const model = node.data.model || "roberta-large-mnli";
+    const model = node.data.model as string || "roberta-large-mnli";
 
     try {
       const result = await thoth.huggingface(model, request);
 
       if (result.error) throw Error();
-      console.log("huggingfave result", result);
+      console.log("huggingface result", result);
 
       return {
         result,
