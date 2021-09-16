baf8a39d63373f0d1203920012241af27b21b5bc
Sean Gillespie <info@spg.business>
2021-09-16 14:39:48 -0400

    Update typing for utils.ts

--- core/plugins/modulePlugin/utils.ts
+++ core/plugins/modulePlugin/utils.ts
@@ -1,17 +1,21 @@
-import { Input, Output } from "rete";
+import { Input, NodeEditor, Output, Socket } from "rete";
+import { IRunContextEditor } from ".";
 
-import { socketNameMap } from "../../sockets";
+import { socketNameMap, SocketNameType, SocketType } from "../../sockets";
+import { ThothNode } from "../../types";
 import { ModuleSocketType } from "./module-manager";
 
 type DataSocketType = {
-  name: string;
+  name: SocketNameType;
   taskType: "output" | "option";
   socketKey: string;
   connectionType: "input" | "output";
-  socketType: "string";
+  socketType: SocketType;
 };
 
-export function extractNodes(nodes, map) {
+export type ThroughPutType = "outputs" | "inputs"
+
+export function extractNodes(nodes: Record<string, ThothNode>, map: Map<string, Socket>) {
   const names = Array.from(map.keys());
 
   return Object.keys(nodes)
@@ -31,10 +35,10 @@ const getRemovedSockets = (
 };
 
 const removeSockets = (
-  node,
+  node: ThothNode,
   sockets: DataSocketType[],
   type: "input" | "output",
-  editor
+  editor: NodeEditor
 ) => {
   sockets.forEach((socket) => {
     const connections = node.getConnections().filter((con) => {
@@ -54,26 +58,28 @@ const removeSockets = (
     // need to get the socket from the node first since this isnt the sockey object
 
     const removeMethod = type === "input" ? "removeInput" : "removeOutput";
-    const removedSocket = node[type + "s"].get(socket.socketKey);
-    if (removedSocket) node[removeMethod](removedSocket);
-    node.data[type + "s"] = node.data[type + "s"].filter(
+    const removedSocket = node[type + "s" as "inputs" | "outputs"].get(socket.socketKey);
+    if (removedSocket) node[removeMethod](removedSocket as Input & Output);
+    const nodeData = node.data as Record<string, DataSocketType[]>
+    nodeData[type + "s"] = nodeData[type + "s"].filter(
       (soc) => soc.socketKey !== socket.socketKey
     );
   });
 };
 
 const updateSockets = (
-  node,
+  node: ThothNode,
   sockets: ModuleSocketType[],
   taskType: "option" | "output" = "output"
 ) => {
   sockets.forEach(({ socketKey, name }) => {
     if (node.inputs.has(socketKey)) {
-      const input = node.inputs.get(socketKey);
+      const input = node.inputs.get(socketKey) as Input;
       input.name = name;
       node.inputs.set(socketKey, input);
       // Update the nodes data sockets as well
-      node.data.inputs = node.data.inputs.map((n) => {
+      const nodeInputs = node.data.inputs as DataSocketType[]
+      node.data.inputs = nodeInputs.map((n: DataSocketType) => {
         if (n.socketKey === socketKey) {
           n.name = name;
         }
@@ -82,11 +88,11 @@ const updateSockets = (
       });
     }
     if (node.outputs.has(socketKey)) {
-      const output = node.outputs.get(socketKey);
+      const output = node.outputs.get(socketKey) as Output;
       output.name = name;
       node.outputs.set(socketKey, output);
-
-      node.data.outputs = node.data.outputs.map((n) => {
+      const nodeOutputs = node.data.outputs as DataSocketType[]
+      node.data.outputs = nodeOutputs.map((n) => {
         if (n.socketKey === socketKey) {
           n.name = name;
         }
@@ -98,20 +104,20 @@ const updateSockets = (
 };
 
 const addSockets = (
-  node,
+  node: ThothNode,
   sockets: ModuleSocketType[],
   connectionType: "input" | "output",
   taskType: "option" | "output" = "output"
 ) => {
   const uniqueCount = new Set(sockets.map((i) => i.name)).size;
-  const existingSockets = node.data[connectionType + "s"].map(
+  const currentConnection = node.data[connectionType + "s" as ThroughPutType] as DataSocketType[]
+  const existingSockets = currentConnection.map(
     (soc: DataSocketType) => soc.socketKey
   );
 
   if (uniqueCount !== sockets.length)
     throw new Error(
-      `Module ${node.data.module} has duplicate ${
-        taskType === "option" ? "trigger" : ""
+      `Module ${node.data.module} has duplicate ${taskType === "option" ? "trigger" : ""
       } ${connectionType}s`
     );
 
@@ -127,23 +133,23 @@ const addSockets = (
 
       const Socket = connectionType === "output" ? Output : Input;
       const addMethod = connectionType === "output" ? "addOutput" : "addInput";
-
-      node.data[connectionType + "s"].push({
-        name: name,
+      const currentConnection = node.data[connectionType + "s" as ThroughPutType] as DataSocketType[]
+      currentConnection.push({
+        name: name as SocketNameType,
         taskType: taskType,
         socketKey: socketKey,
         connectionType: connectionType,
         socketType: socketNameMap[socket.name],
       });
 
-      node[addMethod](new Socket(socketKey, name, socket));
+      node[addMethod](new Socket(socketKey, name, socket) as Input & Output);
       if (connectionType === "output")
         node.inspector.component.task.outputs[socketKey] = taskType;
     });
 };
 
 export function addIO(
-  node,
+  node: ThothNode,
   inputs: ModuleSocketType[],
   outputs: ModuleSocketType[],
   triggerOuts: ModuleSocketType[],
@@ -159,13 +165,13 @@ export function addIO(
 // here we can only remove the inputs and outputs that are not supposed to be on the node.
 // This means we determine which IO are present on the node but not in the incoming IO
 export function removeIO(
-  node,
-  editor,
+  node: ThothNode,
+  editor: IRunContextEditor,
   inputs: ModuleSocketType[],
   outputs: ModuleSocketType[]
 ) {
-  const existingInputs = node.data.inputs;
-  const existingOutputs = node.data.outputs;
+  const existingInputs = node.data.inputs as DataSocketType[];
+  const existingOutputs = node.data.outputs as DataSocketType[];
   const inputRemovals = getRemovedSockets(existingInputs, inputs);
   const outputRemovals = getRemovedSockets(existingOutputs, outputs);
 
