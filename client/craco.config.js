a498f775ee46b9d26122748d1eeb49eca0819fee
Sean Gillespie <info@spg.business>
2021-09-17 13:06:12 -0400

    Attempt to use dual loader config with craco

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,26 +1,50 @@
 const path = require("path");
-const { getLoader, loaderByName } = require("@craco/craco");
+const { getLoaders, removeLoaders, addAfterLoader, loaderByName, throwUnexpectedConfigError } = require("@craco/craco");
 
 const packages = [];
 packages.push(path.join(__dirname, "../core"));
 
-module.exports = {
-  babel: {
-    presets: ["@babel/preset-typescript"]
-  },  
-  webpack: {
-    configure: (webpackConfig, arg) => {
-      const { isFound, match } = getLoader(
-        webpackConfig,
-        loaderByName("babel-loader")
-      );
-      if (isFound) {
-        const include = Array.isArray(match.loader.include)
-          ? match.loader.include
-          : [match.loader.include];
+const throwError = (message) =>
+  throwUnexpectedConfigError({
+    packageName: 'craco',
+    githubRepo: 'gsoft-inc/craco',
+    message,
+    githubIssueQuery: 'webpack',
+  });
 
-        match.loader.include = include.concat(packages);
-      }
+module.exports = {
+  webpack: {
+    configure: (webpackConfig, { paths }) => {
+      const { hasFoundAny, matches } = getLoaders(webpackConfig, loaderByName('babel-loader'));
+      if (!hasFoundAny) throwError('failed to find babel-loader');
+
+      console.log('removing babel-loader');
+      const { hasRemovedAny, removedCount } = removeLoaders(webpackConfig, loaderByName('babel-loader'));
+      if (!hasRemovedAny) throwError('no babel-loader to remove');
+      if (removedCount !== 2) throwError('had expected to remove 2 babel loader instances');
+
+      console.log('adding ts-loader');
+
+      const tsLoader = {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        include: paths.appSrc,
+        loader: require.resolve('ts-loader'),
+        options: { transpileOnly: true },
+      };
+
+      const { isAdded: tsLoaderIsAdded } = addAfterLoader(webpackConfig, loaderByName('url-loader'), tsLoader);
+      if (!tsLoaderIsAdded) throwError('failed to add ts-loader');
+      console.log('added ts-loader');
+
+      console.log('adding non-application JS babel-loader back');
+      const { isAdded: babelLoaderIsAdded } = addAfterLoader(
+        webpackConfig,
+        loaderByName('ts-loader'),
+        matches[1].loader // babel-loader
+      );
+      if (!babelLoaderIsAdded) throwError('failed to add back babel-loader for non-application JS');
+      console.log('added non-application JS babel-loader back');
+
       return webpackConfig;
     },
   },
