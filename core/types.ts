5498a7d5266b08a9692356a53c9c30d6d95aa39c
Sean Gillespie <info@spg.business>
2021-09-17 11:55:57 -0400

    Simplify EventsTypes

--- core/types.ts
+++ core/types.ts
@@ -1,5 +1,4 @@
-import { Component } from "rete";
-import { EventsTypes as DefaultEventsTypes } from "rete/types/events";
+import { Component, Connection, Input, Output } from "rete";
 import { Node } from "rete/types";
 //@seang todo: convert inspector plugin fully to typescript
 //@ts-ignore 
@@ -9,10 +8,19 @@ import { ModuleGraphData } from "./plugins/modulePlugin/module-manager";
 import { TaskOutputTypes } from "./plugins/taskPlugin/task";
 import { ThothTask } from "./thoth-component";
 
-export type EventsTypes = DefaultEventsTypes & {
+export type EventsTypes = {
   run: void;
   save: void;
   [key: string]: unknown;
+  //EventTypes from rete/types/events
+  connectionpath: {
+    points: number[];
+    connection: Connection;
+    d: string;
+  };
+  connectiondrop: Input | Output;
+  connectionpick: Input | Output;
+  resetconnection: void;
 }
 
 export type ThothNode = Node & {
@@ -106,9 +114,9 @@ export type Spell = {
   nodes: Record<number, Node>
 }
 
-export type ThothWorkerBaseInput = {type: TaskOutputTypes;outputData: unknown;task:ThothTask, key: string}
+export type ThothWorkerBaseInput = { type: TaskOutputTypes; outputData: unknown; task: ThothTask, key: string }
 export type ThothWorkerInput = ThothWorkerBaseInput | string | unknown[]
-export type ThothWorkerInputs = {[key:string]:ThothWorkerInput[]}
+export type ThothWorkerInputs = { [key: string]: ThothWorkerInput[] }
 export type ThothWorkerOutputs = WorkerOutputs & { [key: string]: string[] | string; }
 export type WorkerReturn = Node | ThothWorkerOutputs | void | Promise<void> | Promise<{ actionType: string }> | Promise<{ difficulty?: string, category?: string }> | Promise<{ [output: string]: string } | null> | Promise<never[] | { entities: { name: string; type: string; }[]; }> | Promise<{ element: unknown; } | undefined> | Promise<{ result: { error: unknown, [key: string]: unknown } } | { result?: undefined }> | Promise<{ text: unknown }> | Promise<{ boolean: boolean; }> | Promise<null | undefined> | WorkerOutputs[] | { trigger: boolean }
 export type ThothWorker = (node: ThothNode, inputs: WorkerInputs, outputs: WorkerOutputs, ...args: unknown[]) => WorkerReturn
@@ -120,40 +128,40 @@ export type ThothWorker = (node: ThothNode, inputs: WorkerInputs, outputs: Worke
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export interface PubSubBase
-        extends CountSubscriptions,
-            ClearAllSubscriptions,
-            GetSubscriptions,
-            Publish,
-            Subscribe,
-            Unsubscribe {
-        name: string;
-        version: string;
-    }
+  extends CountSubscriptions,
+  ClearAllSubscriptions,
+  GetSubscriptions,
+  Publish,
+  Subscribe,
+  Unsubscribe {
+  name: string;
+  version: string;
+}
 
-    interface CountSubscriptions {
-        countSubscriptions(token: any): number;
-    }
+interface CountSubscriptions {
+  countSubscriptions(token: any): number;
+}
 
-    interface ClearAllSubscriptions {
-        clearAllSubscriptions(token?: any): void;
-    }
+interface ClearAllSubscriptions {
+  clearAllSubscriptions(token?: any): void;
+}
 
-    interface GetSubscriptions {
-        getSubscriptions(token: any): any[];
-    }
+interface GetSubscriptions {
+  getSubscriptions(token: any): any[];
+}
 
-    interface Publish {
-        publish(message: string | Symbol, data?: any): boolean;
+interface Publish {
+  publish(message: string | Symbol, data?: any): boolean;
 
-        publishSync(message: string | Symbol, data?: any): boolean;
-    }
+  publishSync(message: string | Symbol, data?: any): boolean;
+}
 
-    interface Subscribe {
-        subscribe(message: string | Symbol, func: Function): string;
+interface Subscribe {
+  subscribe(message: string | Symbol, func: Function): string;
 
-        subscribeOnce(message: string | Symbol, func: Function): any;
-    }
+  subscribeOnce(message: string | Symbol, func: Function): any;
+}
 
-    interface Unsubscribe {
-        unsubscribe(tokenOrFunction: any): any;
-    }
+interface Unsubscribe {
+  unsubscribe(tokenOrFunction: any): any;
+}
