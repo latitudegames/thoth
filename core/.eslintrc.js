b6c6d7809ca58c350248e0e86ee384974316357b
Michael Sharpe <m.sharpe@project89.org>
2021-09-20 16:42:58 -0700

    Add eslint to core

---
+++ core/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: '../.eslintrc.js',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+}
