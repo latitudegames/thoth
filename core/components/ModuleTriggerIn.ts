71ee5c70d6c39780c13b6b88666f357be24c272d
Sean Gillespie <info@spg.business>
2021-09-21 11:19:24 -0400

    Correct missing task initialization in ModuleTrigger.ts that was overwritten in typescript / workspace refactor

--- core/components/ModuleTriggerIn.ts
+++ core/components/ModuleTriggerIn.ts
@@ -1,48 +1,57 @@
-import Rete from "rete";
+import Rete from 'rete'
 // @seang todo: convert data controls to typescript to remove this
 //@ts-ignore
-import { InputControl } from "../dataControls/InputControl";
-import { triggerSocket } from "../sockets";
-import { v4 as uuidv4 } from "uuid";
-import { ThothComponent, ThothTask } from "../thoth-component"
-import { NodeData, ThothNode, ThothWorkerInputs, ThothWorkerOutputs } from "../types";
-import { TaskOptions } from "../plugins/taskPlugin/task";
-const info = `The module trigger in adds a trigger input socket to the parent module.  It can be given a name, which is displayed on the parent.`;
+import { InputControl } from '../dataControls/InputControl'
+import { TaskOptions } from '../plugins/taskPlugin/task'
+import { triggerSocket } from '../sockets'
+import { v4 as uuidv4 } from 'uuid'
+import { ThothComponent, ThothTask } from '../thoth-component'
+import {
+  NodeData,
+  ThothNode,
+  ThothWorkerInputs,
+  ThothWorkerOutputs,
+} from '../types'
+const info = `The module trigger in adds a trigger input socket to the parent module.  It can be given a name, which is displayed on the parent.`
 
 export class ModuleTriggerIn extends ThothComponent {
-  task: TaskOptions;
-  module: object;
-  category: string;
-  info: string;
-  workspaceType: "module" | "spell";
-  contextMenuName: string;
-  nodeTaskMap: Record<number, ThothTask> = {};
+  task: TaskOptions
+  module: object
+  category: string
+  info: string
+  workspaceType: 'module' | 'spell'
+  contextMenuName: string
+  nodeTaskMap: Record<number, ThothTask> = {}
 
   constructor() {
     // Name of the component
     // If name of component changes please update module-manager workerModule code
-    super("Module Trigger In");
-    this.contextMenuName = "Trigger In";
+    super('Module Trigger In')
+    this.contextMenuName = 'Trigger In'
 
     this.task = {
       outputs: {
-        trigger: "option",
-      }
+        trigger: 'option',
+      },
+      init: (task: ThothTask, node: ThothNode) => {
+        // store the nodes task inside the component
+        this.nodeTaskMap[node.id] = task
+      },
     }
 
     this.module = {
-      nodeType: "triggerIn",
+      nodeType: 'triggerIn',
       socket: triggerSocket,
-    };
+    }
 
-    this.category = "Module";
-    this.info = info;
-    this.workspaceType = "module";
+    this.category = 'Module'
+    this.info = info
+    this.workspaceType = 'module'
   }
 
   async run(node: ThothNode, data: NodeData) {
-    const task = this.nodeTaskMap[node.id];
-    await task.run(data);
+    const task = this.nodeTaskMap[node.id]
+    await task.run(data)
   }
 
   // the builder is used to "assemble" the node component.
@@ -50,22 +59,26 @@ export class ModuleTriggerIn extends ThothComponent {
   // to generate the appropriate inputs and ouputs for the fewshot at build time
   builder(node: ThothNode) {
     // create inputs here. First argument is the name, second is the type (matched to other components sockets), and third is the socket the i/o will use
-    const out = new Rete.Output("trigger", "Trigger", triggerSocket);
-    node.data.socketKey = node?.data?.socketKey || uuidv4();
+    const out = new Rete.Output('trigger', 'Trigger', triggerSocket)
+    node.data.socketKey = node?.data?.socketKey || uuidv4()
 
     // Handle default value if data is present
     const nameInput = new InputControl({
-      dataKey: "name",
-      name: "Trigger name",
-    });
+      dataKey: 'name',
+      name: 'Trigger name',
+    })
 
-    node.inspector.add(nameInput);
+    node.inspector.add(nameInput)
 
-    return node.addOutput(out);
+    return node.addOutput(out)
   }
 
-  async worker(node: NodeData, inputs: ThothWorkerInputs, outputs: ThothWorkerOutputs) {
-    console.log("trigger worker outputs", outputs);
-    return {};
+  async worker(
+    node: NodeData,
+    inputs: ThothWorkerInputs,
+    outputs: ThothWorkerOutputs
+  ) {
+    console.log('trigger worker outputs', outputs)
+    return {}
   }
 }
