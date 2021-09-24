400a1e45908af93cba8d37a56736b060202b0ce8
Sean Gillespie <info@spg.business>
2021-09-24 17:33:13 -0400

    Change directory to client for conditional-canaray install, then return to root

--- netlify/plugins/conditional-canary/index.js
+++ netlify/plugins/conditional-canary/index.js
@@ -3,7 +3,8 @@ module.exports = {
     console.log('conditional-canary plugin')
     const coreChanges = git.fileMatch('core/**/*')
     const coreChanged = coreChanges.edited.length !== 0
-    const installCanary = 'yarn add @latitudegames/thoth-core@canary'
+    const installCanary =
+      'cd client && yarn add @latitudegames/thoth-core@canary && cd ..'
     netlifyConfig.build.command = coreChanged
       ? `${installCanary} && ${netlifyConfig.build.command}`
       : netlifyConfig.build.command
