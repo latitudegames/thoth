0c402ac6c9e07f7632203c915f8ecd3ad7e850b0
Sean Gillespie <info@spg.business>
2021-09-24 15:21:20 -0400

    Complete logic for conditional-canary netlify plugin

--- .netlify/plugins/conditional-canary/index.js
+++ .netlify/plugins/conditional-canary/index.js
@@ -1,5 +1,11 @@
 module.exports = {
-  onPreBuild: () => {
+  onPreBuild: ({ utils: { netlifyConfig, git } }) => {
     console.log('conditional-canary plugin')
+    const coreChanges = git.fileMatch('core/**/*')
+    const coreChanged = coreChanges.edited.length !== 0
+    const installCanary = 'yarn add @latitudegames/thoth-core@canary'
+    netlifyConfig.build.command = coreChanged
+      ? `${installCanary} && ${netlifyConfig.build.command}`
+      : netlifyConfig.build.command
   },
 }
