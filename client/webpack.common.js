819725610fb6d3989e6a40296609cd9015bf987a
Michael Sharpe <m.sharpe@project89.org>
2021-09-29 10:12:42 -0700

    Create common webpack config for client

---
+++ client/webpack.common.js
@@ -0,0 +1,116 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const CopyPlugin = require('copy-webpack-plugin')
+const dotenv = require('dotenv')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+const prod = process.env.NODE_ENV === 'production'
+
+const babelOptions = {
+  sourceType: 'unambiguous',
+  presets: [
+    '@babel/env',
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: ['@babel/plugin-proposal-class-properties'],
+}
+
+module.exports = () => {
+  const env = dotenv.config().parsed
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next])
+    return prev
+  }, {})
+
+  return {
+    entry: ['regenerator-runtime/runtime.js', './src/index.js'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js',
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.css'],
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+      },
+    },
+    ignoreWarnings: [/Failed to parse source map/],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          enforce: 'pre',
+          // include: [path.resolve(__dirname, 'node_modules/@latitudegames')],
+          use: ['source-map-loader'],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-modules-typescript-loader' },
+            { loader: 'css-loader' },
+          ],
+        },
+        {
+          test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.js(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: babelOptions,
+            },
+          ],
+        },
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: babelOptions,
+            },
+            {
+              loader: 'ts-loader',
+              options: {
+                compilerOptions: {
+                  noEmit: false,
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+        inject: true,
+        favicon: './public/favicon.ico',
+      }),
+      new webpack.DefinePlugin({
+        ...envKeys,
+        'process.env.NODE_DEBUG': JSON.stringify(process.env.NODE_DEBUG),
+      }),
+
+      new CopyPlugin({
+        patterns: [{ from: 'public', to: '' }],
+      }),
+    ],
+  }
+}
