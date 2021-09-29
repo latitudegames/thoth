074051b99cf6fc0f5deec8c5debc6480330f2f7d
Michael Sharpe <m.sharpe@project89.org>
2021-09-29 11:34:59 -0700

    Add dotenv to prod config

--- client/webpack/webpack.prod.js
+++ client/webpack/webpack.prod.js
@@ -1,7 +1,8 @@
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const Dotenv = require('dotenv-webpack')
 const CompressionPlugin = require('compression-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
@@ -13,16 +14,10 @@ module.exports = () => {
     mode: 'production',
     plugins: [
       new CompressionPlugin(),
-      // {
-      //   apply: compiler => {
-      //     compiler.hooks.done.tap('DonePlugin', stats => {
-      //       console.log('Compile is done !')
-      //       setTimeout(() => {
-      //         process.exit(0)
-      //       })
-      //     })
-      //   },
-      // },
+      new Dotenv({
+        path: './.env',
+        systemvars: true,
+      }),
     ],
     optimization: {
       minimize: true,
