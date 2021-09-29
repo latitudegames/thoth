5e30e121d48cc6a793037725f86a4f8bd673f0c7
Michael Sharpe <m.sharpe@project89.org>
2021-09-29 11:09:23 -0700

    Only use bundle analyzer in core on env flag

--- core/webpack.config.js
+++ core/webpack.config.js
@@ -3,61 +3,71 @@
 const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = {
-  entry: './index.ts',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'index.js',
-    library: {
-      name: 'thothCore',
-      type: 'umd',
-    },
-  },
-  externals: {
-    react: 'react',
-  },
-  mode: 'production',
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    alias: {
-      handlebars: 'handlebars/dist/handlebars.js',
-    },
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    port: process.env.PORT || 3001,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
+module.exports = () => {
+  const config = {
+    entry: './index.ts',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'index.js',
+      library: {
+        name: 'thothCore',
+        type: 'umd',
       },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader',
-        options: {
-          compilerOptions: {
-            outDir: './dist',
+    },
+    externals: {
+      react: 'react',
+    },
+    mode: 'production',
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      alias: {
+        handlebars: 'handlebars/dist/handlebars.js',
+      },
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+    },
+    devServer: {
+      static: path.join(__dirname, 'dist'),
+      port: process.env.PORT || 3001,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              outDir: './dist',
+            },
           },
         },
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        use: ['file-loader'],
-      },
-    ],
-  },
+        {
+          test: /\.(css|scss)$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+  }
+
+  const isAnalyze = typeof process.env.BUNDLE_ANALYZE !== 'undefined'
+
+  if (isAnalyze) {
+    config.plugins.push(new BundleAnalyzerPlugin())
+  }
+
+  return config
 }
