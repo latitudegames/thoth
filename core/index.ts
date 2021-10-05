56b0c80da9eca3de5314ca41a1bb810969ed6070
Michael Sharpe <m.sharpe@project89.org>
2021-10-05 14:28:13 -0700

    Fix TS compilations errors on build

--- core/index.ts
+++ core/index.ts
@@ -1,14 +1,14 @@
-import { components } from './src/components/components'
+import { getComponents } from './src/components/components'
 import { initEditor } from './src/editor'
 import { Task } from './src/plugins/taskPlugin/task'
 
-export { components } from './src/components/components'
+export { getComponents } from './src/components/components'
 export { initEditor } from './src/editor'
 export type { EngineContext } from './src/engine'
 export { Task } from './src/plugins/taskPlugin/task'
 
 export default {
-  components,
+  getComponents,
   initEditor,
   Task,
 }
--- core/src/index.ts
+++ core/src/index.ts
@@ -1,14 +1,14 @@
-import { components } from './components/components'
+import { getComponents } from './components/components'
 import { initEditor } from './editor'
 import { Task } from './plugins/taskPlugin/task'
 
-export { components } from './components/components'
+export { getComponents } from './components/components'
 export { initEditor } from './editor'
 export type { EngineContext } from './engine'
 export { Task } from './plugins/taskPlugin/task'
 
 export default {
-  components,
+  getComponents,
   initEditor,
   Task,
 }
