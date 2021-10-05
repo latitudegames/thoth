0cc9643625a56bdbc1df3b0730d31a4c3a1fa8c9
Sean Gillespie <info@spg.business>
2021-10-05 10:53:09 -0400

    Comment correction in PlaytestInput

--- core/src/components/PlaytestInput.ts
+++ core/src/components/PlaytestInput.ts
@@ -63,7 +63,7 @@ export class PlaytestInput extends ThothComponent {
   }
 
   // the builder is used to "assemble" the node component.
-  // when we have enki hooked up and have garbbed all few shots, we would use the builder
+  // when we have enki hooked up and have grabbed all few shots, we would use the builder
   // to generate the appropriate inputs and ouputs for the fewshot at build time
   builder(node: ThothNode) {
     if (this.subscriptionMap[node.id]) this.subscriptionMap[node.id]()
