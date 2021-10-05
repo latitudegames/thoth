76256377a59ecd67be28db368ad5f9214da61f3c
Michael Sharpe <m.sharpe@project89.org>
2021-10-05 16:48:43 -0700

    Add tab reducer to store

--- client/src/state/store.ts
+++ client/src/state/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-// import counterReducer from '../features/counter/counterSlice';
+import tabReducer from './tabs'
 
 export const store = configureStore({
   reducer: {
-    // counter: counterReducer,
+    tabs: tabReducer,
   },
 })
 
