674ee88c283d7e7a8725aeb4fbc953e477b4c929
Sean Gillespie <info@spg.business>
2021-10-05 23:46:36 -0400

    Add initSharedEngine and ThothComponent to exports

--- core/index.ts
+++ core/index.ts
@@ -1,6 +1,8 @@
 import { getComponents, components } from './src/components/components'
 import { initEditor } from './src/editor'
+import { initSharedEngine } from './src/engine'
 import { Task } from './src/plugins/taskPlugin/task'
+import { ThothComponent } from './src/thoth-component'
 
 export { getComponents } from './src/components/components'
 export { initEditor } from './src/editor'
@@ -10,6 +12,8 @@ export { Task } from './src/plugins/taskPlugin/task'
 export default {
   components,
   getComponents,
+  initSharedEngine,
   initEditor,
   Task,
+  ThothComponent,
 }
