85a98a6bdc121b8fb871274a84ce3acbd391fccc
Michael Sharpe <m.sharpe@project89.org>
2021-10-07 14:17:50 -0700

    Add tabOpened reducer and support functions

--- client/src/state/tabs.ts
+++ client/src/state/tabs.ts
@@ -1,32 +1,69 @@
+import { v4 as uuidv4 } from 'uuid'
 import {
   createSlice,
   // PayloadAction,
+  createDraftSafeSelector,
   createEntityAdapter,
 } from '@reduxjs/toolkit'
 
-// import { RootState } from './store'
+import { RootState } from './store'
+import defaultJson from '../contexts/layouts/defaultLayout.json'
 
+// Used to set workspaces to tabs
+const workspaceMap = {
+  default: defaultJson,
+}
 export interface Tab {
   id: string
   name: string
   active: boolean
   layoutJson: Record<string, unknown>
-  type: 'spell' | 'module'
+  type?: 'spell' | 'module'
   // probably going to need to insert a proper spell type in here
-  spell: string
+  spell?: string
   // this will also be a ref to a property somewhere else
   module: string
 }
 
+// Entity adapter
 const tabAdapater = createEntityAdapter<Tab>()
+const tabSelectors = tabAdapater.getSelectors()
 
+// Initial State
 const initialState = tabAdapater.getInitialState()
 
+// Selectors
+const _activeTabSelector = createDraftSafeSelector(
+  tabSelectors.selectAll,
+  tabs => {
+    return Object.values(tabs).find(tab => tab?.active)
+  }
+)
+
+// Used to build a tab with various defaults set, as well as workspace json and UUID
+const buildTab = (tab, properties = {}) => ({
+  ...tab,
+  name: tab.name || 'Untitled',
+  id: uuidv4(),
+  layoutJson: workspaceMap[tab.workspace || 'default'],
+  spell: tab?.spell || null,
+  type: tab?.type || 'module',
+  module: tab?.moduleName || null,
+  ...properties,
+})
+
+// This is the primary composed of our "duck", and returns a number of helper functions and properties.
 export const tabSlice = createSlice({
   name: 'tabs',
   initialState,
   reducers: {
-    tabOpened: tabAdapater.addOne,
+    tabOpened: (state, action) => {
+      const activeTab = _activeTabSelector(state) as Tab
+      if (activeTab) activeTab.active = false
+
+      const tab = buildTab(action.payload, { active: true })
+      tabAdapater.addOne(state, tab)
+    },
     tabClosed: tabAdapater.removeOne,
     tabSwitched: tabAdapater.updateOne,
     tabsCleared: tabAdapater.removeAll,
@@ -34,6 +71,7 @@ export const tabSlice = createSlice({
   },
 })
 
+// actions
 export const {
   tabOpened,
   tabClosed,
@@ -42,6 +80,8 @@ export const {
   tabLayoutSaved,
 } = tabSlice.actions
 
-// export const selectActiveTab = (state: RootState) => state.tabs.active
+// selectors
+export const activeTabSelector = (state: RootState) =>
+  _activeTabSelector(state.tabs)
 
 export default tabSlice.reducer
