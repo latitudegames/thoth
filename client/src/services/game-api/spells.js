84b8d446d1547c4738593c36125574c23c01f292
Michael Sharpe <m.sharpe@project89.org>
2021-10-07 15:04:15 -0700

    Add placeholder spell service

---
+++ client/src/services/game-api/spells.js
@@ -0,0 +1,20 @@
+export const saveSpell = spell =>
+  new Promise(resolve => {
+    resolve(spell)
+  })
+
+export const loadSpell = spell =>
+  new Promise(resolve => {
+    resolve(spell)
+  })
+
+export const getSpell = spell =>
+  new Promise(resolve => {
+    resolve(spell)
+  })
+
+export const getSpells = spell =>
+  new Promise(resolve => {
+    // need some stubbed spell data
+    resolve(spell)
+  })
