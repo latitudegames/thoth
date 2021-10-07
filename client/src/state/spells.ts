313609ca8a1bbb7799911d7a3761d332a3ce43f8
Michael Sharpe <m.sharpe@project89.org>
2021-10-07 16:42:27 -0700

    Switch spell slice to to createApi

--- client/src/state/spells.ts
+++ client/src/state/spells.ts
@@ -1,37 +1,41 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Spell as SpellType } from '@latitudegames/thoth-core/types'
-import {
-  createSlice,
-  // PayloadAction,
-  // createDraftSafeSelector,
-  createEntityAdapter,
-} from '@reduxjs/toolkit'
 
-// import { RootState } from './store'
+import { initDB } from '../database'
 
+const _spells = async () => {
+  const db = await initDB()
+  const { spells } = db.modules
+  return spells
+}
 export interface Spell {
-  user: Record<string, unknown> | null | undefined
+  user?: Record<string, unknown> | null | undefined
   name: string
   graph: SpellType
   gameState: Record<string, unknown>
-  createdAt: number
-  updatedAt: number
+  createdAt?: number
+  updatedAt?: number
 }
 
-const spellAdapater = createEntityAdapter<Spell>()
-const initialState = spellAdapater.getInitialState()
-// const spellSelectors = spellAsdapater.getSelectors()
-
-const spellSlice = createSlice({
-  name: 'spells',
-  initialState,
-  reducers: {
-    spellSaved: spellAdapater.updateOne,
-    spellCreated: spellAdapater.addOne,
-    spellLoaded: () => {},
-    spellDeleted: () => {},
-  },
+export const spellApi = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_API_URL || 'localhost:8000/',
+  }),
+  endpoints: builder => ({
+    // getSpells: builder.query<Spell, true>({
+    //   queryFn: getSpells,
+    // }),
+    getSpell: builder.query<Spell, number>({
+      async queryFn(args) {
+        return { data: {} as Spell }
+      },
+    }),
+    // saveSpell: builder.query<Spell, Spell>({
+    //   queryFn: saveSpell,
+    // }),
+  }),
 })
 
-export const {} = spellSlice.actions
+console.log('Spell api', spellApi)
 
-export default spellSlice.reducer
+export const { useGetSpellQuery } = spellApi
