b0db4cc6bc50744556efa1fcbb00c4507e8dc6a7
Michael Sharpe <m.sharpe@project89.org>
2021-10-08 10:56:26 -0700

    Switch variable from spell to spellModell.  Add reducer for saveSpell

--- client/src/state/spells.ts
+++ client/src/state/spells.ts
@@ -3,7 +3,7 @@ import { Spell as SpellType } from '@latitudegames/thoth-core/types'
 
 import { initDB } from '../database'
 
-const _spells = async () => {
+const _spellModel = async () => {
   const db = await initDB()
   const { spells } = db.models
   return spells
@@ -22,9 +22,9 @@ export const spellApi = createApi({
     baseUrl: process.env.REACT_APP_API_URL || 'localhost:8000/',
   }),
   endpoints: builder => ({
-    getSpells: builder.query<Spell, true>({
+    getSpells: builder.query<Spell[], true>({
       async queryFn() {
-        const spellModel = await _spells()
+        const spellModel = await _spellModel()
         const spells = await spellModel.getSpells()
 
         return { data: spells.map(spell => spell.toJSON()) }
@@ -32,34 +32,40 @@ export const spellApi = createApi({
     }),
     getSpell: builder.query<Spell, string>({
       async queryFn(spellId) {
-        const spellModel = await _spells()
+        const spellModel = await _spellModel()
         const spell = await spellModel.getSpell(spellId)
 
-        return { data: spell.toJSON() as Spell }
+        return { data: spell.toJSON() }
       },
     }),
     saveSpell: builder.mutation<Spell, Spell>({
       async queryFn(spell) {
-        return { data: {} as Spell }
+        const spellModel = await _spellModel()
+        const updatedSpell = await spellModel.saveSpell(spell)
+        return { data: updatedSpell.toJSON() }
       },
     }),
     newSpell: builder.mutation<Spell, Partial<Spell>>({
       async queryFn(spellData) {
         const newSpell = { gameState: {}, ...spellData }
-        const spellModel = await _spells()
+        const spellModel = await _spellModel()
 
         const spell = await spellModel.newSpell(newSpell)
 
-        return { data: spell.toJSON() as Spell }
+        return { data: spell.toJSON() }
       },
     }),
   }),
 })
 
-const selectSpellResults = spellApi.endpoints.getSpells
+// const selectSpellResults = spellApi.endpoints.getSpells()
+
+export const {
+  useGetSpellQuery,
+  useLazyGetSpellQuery,
+  useNewSpellMutation,
+  useSaveSpellMutation,
+} = spellApi
 
 export const useGetSpellSubscription =
   spellApi.endpoints.getSpell.useQuerySubscription
-
-export const { useGetSpellQuery, useLazyGetSpellQuery, useNewSpellMutation } =
-  spellApi
