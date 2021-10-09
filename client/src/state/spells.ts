3be58857e074c52fa9c83d755003e5d913923461
Michael Sharpe <m.sharpe@project89.org>
2021-10-08 18:31:48 -0700

    Export new hooks

--- client/src/state/spells.ts
+++ client/src/state/spells.ts
@@ -123,6 +123,8 @@ export const {
   useLazyGetSpellQuery,
   useNewSpellMutation,
   useSaveSpellMutation,
+  useDeployMutation,
+  useGetVersionsQuery,
 } = spellApi
 
 export const useGetSpellSubscription =
