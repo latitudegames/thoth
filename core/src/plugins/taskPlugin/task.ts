fd098d8bfc94583db2821da64e6d1f6f12513990
Michael Sharpe <m.sharpe@project89.org>
2021-10-22 13:24:40 -0700

    Add comments to task plugin

--- core/src/plugins/taskPlugin/task.ts
+++ core/src/plugins/taskPlugin/task.ts
@@ -95,10 +95,19 @@ export class Task {
 
     if (!this.outputData) {
       const inputs = {} as Record<string, unknown[]>
-      // here we run through all INPUTS connected to other OUTPUTS for the node.
-      // We run eachinput back to whatever node it is connected to.
-      // We run that nodes task run, and then return its output data and
-      // associate it with This nodes input key
+
+      /*
+        This is where we are populating all the input values to be passed into the worker.
+        We are getting all the input connections that are connected as outputs (ie have values)
+        We filter out all connections which did not come from the previou node.  This is to hgelp support multiple
+        inputs properly, otherwise we actually back propagate along every input and run it, whichI think is unwanted behaviour.
+        After we have filtered these out, we need to run the task, which triggers that nodes worker.  After the worker runs,
+        the task has populated output data, which we take and we associate with the tasks input values, which are subsequently
+        passed to the nodes worker for processing.
+
+        We assume here that his nodes worker does not need to access ALL values simultaneously, but is only interested in one.
+        There is a task option which enables this functionality just in case we have use cases that don't want this behaviour.
+      */
       await Promise.all(
         this.getInputs('output').map(async key => {
           const thothWorkerinputs = await Promise.all(
