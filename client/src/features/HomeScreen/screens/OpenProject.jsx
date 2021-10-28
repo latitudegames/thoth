56fe60c06e7fa19fc3f586490979eb329748d6ca
Michael Sharpe <m.sharpe@project89.org>
2021-10-28 16:56:52 -0700

    Move open project to screens

--- client/src/features/HomeScreen/screens/OpenProject.jsx
+++ client/src/features/HomeScreen/screens/OpenProject.jsx
@@ -6,8 +6,8 @@ import Icon from '../../common/Icon/Icon'
 import Panel from '../../common/Panel/Panel'
 import css from '../homeScreen.module.css'
 import thothBanner from '../version-banner-0.0.0beta.jpg'
-import FileInput from './FileInput'
-import ProjectRow from './ProjectRow'
+import FileInput from '../components/FileInput'
+import ProjectRow from '../components/ProjectRow'
 
 const OpenProject = ({
   spells,
