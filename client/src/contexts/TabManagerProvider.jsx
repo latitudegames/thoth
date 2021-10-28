ef91ff40447036afb32ca7fc8c7fd8a6a28de33c
Michael Sharpe <m.sharpe@project89.org>
2021-10-28 14:42:03 -0700

    Remove wouter form tab manager

--- client/src/contexts/TabManagerProvider.jsx
+++ client/src/contexts/TabManagerProvider.jsx
@@ -1,7 +1,6 @@
 import { useContext, createContext, useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { useHistory } from 'react-router-dom'
-import { useLocation } from 'wouter'
 
 import LoadingScreen from '../features/common/LoadingScreen/LoadingScreen'
 import { useDB } from './DatabaseProvider'
