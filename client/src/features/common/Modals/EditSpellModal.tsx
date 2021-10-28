250094f2c89cc5e35abb3a87804b517fe5cad280
Michael Sharpe <m.sharpe@project89.org>
2021-10-27 22:44:33 -0700

    Hook up form to patch mutation

--- client/src/features/common/Modals/EditSpellModal.tsx
+++ client/src/features/common/Modals/EditSpellModal.tsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
-import { useModal } from '../../../contexts/ModalProvider'
+// import { useModal } from '../../../contexts/ModalProvider'
 import { usePatchSpellMutation } from '../../../state/spells'
 import { useForm } from 'react-hook-form'
 import Modal from '../Modal/Modal'
+import css from './modalForms.module.css'
 
 const EditSpellModal = ({ content, spellId, name }) => {
   const [error, setError] = useState('')
   const [patchSpell] = usePatchSpellMutation()
 
-  const { closeModal } = useModal()
+  // const { closeModal } = useModal()
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    // formState: { errors },
   } = useForm()
 
   const onSubmit = handleSubmit(async data => {
@@ -24,11 +25,34 @@ const EditSpellModal = ({ content, spellId, name }) => {
   })
 
   const options = [
-    { label: 'Oki doki', className: 'primary', onClick: closeModal },
+    {
+      className: `${css['loginButton']} primary`,
+      label: 'save',
+      onClick: onSubmit,
+    },
   ]
+
   return (
     <Modal title="Edit Spell" options={options} icon="info">
-      <p> {content} </p>
+      <div className={css['login-container']}>
+        {error && <span className={css['error-message']}>{error}</span>}
+        <form>
+          {/* register your input into the hook by invoking the "register" function */}
+          <div className={css['input-container']}>
+            <label className={css['label']} htmlFor="">
+              Spell name
+            </label>
+            <input
+              type="text"
+              className={css['input']}
+              defaultValue={name}
+              {...register('name')}
+            />
+          </div>
+          {/* errors will return when field validation fails  */}
+          {/* {errors.password && <span>This field is required</span>} */}
+        </form>
+      </div>
     </Modal>
   )
 }
