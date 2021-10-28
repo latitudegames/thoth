a5d41f624a573bb4fdc34b56e9e7d5c913cddfc8
Michael Sharpe <m.sharpe@project89.org>
2021-10-28 15:30:59 -0700

    Use better pattern in route guard

--- client/src/features/common/GuardedRoute/GuardedRoute.tsx
+++ client/src/features/common/GuardedRoute/GuardedRoute.tsx
@@ -2,18 +2,12 @@ import { Route, Redirect } from 'react-router-dom'
 
 import { useAuth } from '../../../contexts/AuthProvider'
 
-const GuardedRoute = ({ component: Component, ...rest }) => {
+const GuardedRoute = props => {
   const { user } = useAuth()
 
   const auth = user && user.accessToken
-  return (
-    <Route
-      {...rest}
-      component={props =>
-        auth ? <Component {...props} /> : <Redirect to="/login" />
-      }
-    />
-  )
+
+  return auth ? <Route {...props} /> : <Redirect to="/login" />
 }
 
 export default GuardedRoute
