8715a86cf896019729ca0155673afb262a63c288
Michael Sharpe <m.sharpe@project89.org>
2021-10-28 11:53:23 -0700

    Update tab in edit modal and add warning

--- client/src/features/common/Modals/EditSpellModal.tsx
+++ client/src/features/common/Modals/EditSpellModal.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 // import { useModal } from '../../../contexts/ModalProvider'
 import { usePatchSpellMutation } from '../../../state/spells'
+import { useTabManager } from '../../../contexts/TabManagerProvider'
 import { useForm } from 'react-hook-form'
 import Modal from '../Modal/Modal'
 import css from './modalForms.module.css'
 
-const EditSpellModal = ({ content, spellId, name }) => {
+const EditSpellModal = ({ content, spellId, name, tab }) => {
   const [error, setError] = useState('')
   const [patchSpell] = usePatchSpellMutation()
+  const { updateTab } = useTabManager()
 
   // const { closeModal } = useModal()
   const {
@@ -21,8 +23,14 @@ const EditSpellModal = ({ content, spellId, name }) => {
 
     if (response.error) {
       setError(response.error.message)
+      return
     }
+
+    if (data.name) updateTab(tab.id, { name: data.name, spell: data.name })
   })
+
+  // notes
+  // after you save the spell, you need to refetch it?a
 
   const options = [
     {
@@ -36,6 +44,11 @@ const EditSpellModal = ({ content, spellId, name }) => {
     <Modal title="Edit Spell" options={options} icon="info">
       <div className={css['login-container']}>
         {error && <span className={css['error-message']}>{error}</span>}
+        <p>
+          Warning: Changing the name of your spell currently will break the url
+          of your spell deployment. If you rename your spell, please change the
+          url for any urls you are using in production.
+        </p>
         <form>
           {/* register your input into the hook by invoking the "register" function */}
           <div className={css['input-container']}>
