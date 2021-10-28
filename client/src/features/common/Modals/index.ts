0a71ae1f194e2a7579f0ab6dc133d598e00328df
Michael Sharpe <m.sharpe@project89.org>
2021-10-27 20:36:03 -0700

    Add modal to modal map

--- client/src/features/common/Modals/index.ts
+++ client/src/features/common/Modals/index.ts
@@ -2,12 +2,14 @@ import ExampleModal from './ExampleModal'
 import InfoModal from './InfoModal'
 import LoginModal from './LoginModal'
 import DeployModal from './DeployModal'
+import EditSpellModal from './EditSpellModal'
 
 const modals = {
   example: ExampleModal,
   infoModal: InfoModal,
   loginModal: LoginModal,
   deployModal: DeployModal,
+  editSpellModal: EditSpellModal,
 }
 
 export const getModals = () => {
