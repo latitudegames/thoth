d99094c9c0eec9de9030407dd42cee39a670ee7c
Michael Sharpe <m.sharpe@project89.org>
2021-10-28 13:56:09 -0700

    Redirect route guard to login

--- client/src/features/common/GuardedRoute/GuardedRoute.tsx
+++ client/src/features/common/GuardedRoute/GuardedRoute.tsx
@@ -3,7 +3,9 @@ import { Route, Redirect } from 'wouter'
 const GuardedRoute = ({ component: Component, auth, ...rest }) => (
   <Route
     {...rest}
-    component={props => (auth ? <Component {...props} /> : <Redirect to="/" />)}
+    component={props =>
+      auth ? <Component {...props} /> : <Redirect to="/login" />
+    }
   />
 )
 
