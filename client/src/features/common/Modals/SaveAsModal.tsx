181626fed9e198942bc0f4624d61d2b5ca81530a
Michael Sharpe <m.sharpe@project89.org>
2021-10-29 15:01:57 -0700

    Add save as modal

---
+++ client/src/features/common/Modals/SaveAsModal.tsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+// import { useModal } from '../../../contexts/ModalProvider'
+import {
+  useGetSpellQuery,
+  useSaveSpellMutation,
+} from '../../../state/api/spells'
+import { useTabManager } from '../../../contexts/TabManagerProvider'
+import { useForm } from 'react-hook-form'
+import Modal from '../Modal/Modal'
+import css from './modalForms.module.css'
+
+const EditSpellModal = ({ tab }) => {
+  const [error, setError] = useState('')
+  const [saveSpell] = useSaveSpellMutation()
+  const { data: spell } = useGetSpellQuery(tab.spell, { skip: !tab.spell })
+  const { openTab } = useTabManager()
+
+  // const { closeModal } = useModal()
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm()
+
+  const onSubmit = handleSubmit(async data => {
+    const saveResponse: any = await saveSpell({
+      ...spell,
+      name: data.name,
+    })
+
+    if (saveResponse.error) {
+      // show snackbar
+      setError(saveResponse.error.message)
+      return
+    }
+
+    // show snackbar
+    // open a new tab to the new spell
+    await openTab({
+      name: data.name,
+      spellId: data.name,
+      type: 'spell',
+    })
+
+    // if (data.name) updateTab(tab.id, { name: data.name, spell: data.name })
+  })
+
+  // notes
+  // after you save the spell, you need to refetch it?a
+
+  const options = [
+    {
+      className: `${css['loginButton']} primary`,
+      label: 'Save spell as',
+      onClick: onSubmit,
+    },
+  ]
+
+  return (
+    <Modal title="Edit Spell" options={options} icon="info">
+      <div className={css['login-container']}>
+        {error && <span className={css['error-message']}>{error}</span>}
+        <form>
+          {/* register your input into the hook by invoking the "register" function */}
+          <div className={css['input-container']}>
+            <label className={css['label']} htmlFor="">
+              Spell name
+            </label>
+            <input
+              type="text"
+              className={css['input']}
+              defaultValue={tab.spell}
+              {...register('name')}
+            />
+          </div>
+          {/* errors will return when field validation fails  */}
+          {/* {errors.password && <span>This field is required</span>} */}
+        </form>
+      </div>
+    </Modal>
+  )
+}
+
+export default EditSpellModal
