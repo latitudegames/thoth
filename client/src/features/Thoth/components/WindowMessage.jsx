7debff0a2dae8e4775959695a54959660d2a9096
Michael Sharpe <m.sharpe@project89.org>
2021-10-31 12:00:16 -0700

    Update path to css in window message

--- client/src/features/Thoth/components/WindowMessage.jsx
+++ client/src/features/Thoth/components/WindowMessage.jsx
@@ -1,4 +1,4 @@
-import css from './DataControls/datacontrols.module.css'
+import css from '../windows/InspectorWindow/DataControls/datacontrols.module.css'
 
 const WindowMessage = ({ content = 'No component selected' }) => {
   return <p className={css['message']}>{content}</p>
