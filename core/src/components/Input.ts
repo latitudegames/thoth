bef4e4d7d9b9e1ddb276aa29c17448f83547ba66
Michael Sharpe <m.sharpe@project89.org>
2021-11-05 15:50:54 -0700

    Add info to input component

--- core/src/components/Input.ts
+++ core/src/components/Input.ts
@@ -15,7 +15,7 @@ import { Task } from '../plugins/taskPlugin/task'
 
 import { anySocket } from '../sockets'
 import { ThothComponent, ThothTask } from '../thoth-component'
-const info = ``
+const info = `The input component allows you to pass a single value to your chain.  You can set a default value to fall back to if no value is provided at runtim.  You can also turn the input on to receive data from the playtest input.`
 
 type InputReturn = {
   output: unknown
