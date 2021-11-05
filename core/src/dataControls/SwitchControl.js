2ae89597bcde8551df0fcf1a806a138d696a7e24
Michael Sharpe <m.sharpe@project89.org>
2021-11-04 17:14:43 -0700

    Add checked option to switch control class

--- core/src/dataControls/SwitchControl.js
+++ core/src/dataControls/SwitchControl.js
@@ -1,12 +1,13 @@
 import { DataControl } from '../plugins/inspectorPlugin'
 
 export class SwitchControl extends DataControl {
-  constructor({ dataKey, name, icon = 'hand' }) {
+  constructor({ dataKey, name, icon = 'hand', checked = false }) {
     const options = {
       dataKey: dataKey,
       name,
       component: 'switch',
       icon,
+      checked,
     }
 
     super(options)
