7f7d48213b84f54cca481306e7e1928da8fe7261
Michael Sharpe <m.sharpe@project89.org>
2021-11-07 10:20:09 -0800

    Remove comments

--- core/src/components/Output.ts
+++ core/src/components/Output.ts
@@ -14,7 +14,6 @@ const info = `The Playtest Print component will print whatever value is attached
 
 export class Output extends ThothComponent<void> {
   constructor() {
-    // Name of the component
     super('Output')
 
     this.task = {
@@ -34,11 +33,7 @@ export class Output extends ThothComponent<void> {
     this.info = info
   }
 
-  // the builder is used to "assemble" the node component.
-  // when we have enki hooked up and have grabbed all few shots, we would use the builder
-  // to generate the appropriate inputs and ouputs for the fewshot at build time
   builder(node: ThothNode) {
-    // create inputs here. First argument is the name, second is the type (matched to other components sockets), and third is the socket the i/o will use
     const triggerInput = new Rete.Input(
       'trigger',
       'Trigger',
@@ -63,8 +58,6 @@ export class Output extends ThothComponent<void> {
       .addOutput(triggerOutput)
   }
 
-  // the worker contains the main business logic of the node.  It will pass those results
-  // to the outputs to be consumed by any connected components
   worker(
     node: NodeData,
     inputs: ThothWorkerInputs,
