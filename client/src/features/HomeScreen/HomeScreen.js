3ee9d9e03fd6bc8b0678d51ae096f3a696fbec2f
Michael Sharpe <m.sharpe@project89.org>
2021-11-08 15:49:31 -0800

    Remove use of indexdb spell in file reader

--- client/src/features/HomeScreen/HomeScreen.js
+++ client/src/features/HomeScreen/HomeScreen.js
@@ -27,13 +27,9 @@ const StartScreen = ({ createNew, allProjects }) => {
 
   const onReaderLoad = async event => {
     const spellData = JSON.parse(event.target.result)
+    const spell = await saveSpell(spellData)
     // TODO check for proper values here and throw errors
-    let spell
-    try {
-      spell = await models.spells.getSpell(spellData.name)
-    } catch (error) {
-      spell = await saveSpell(spellData)
-    }
+
     // Load modules from the spell
     if (spellData?.modules && spellData.modules.length > 0)
       await Promise.all(
