ba643341210715a77f93163f4cb06cd11389d269
shawticus <shawmakesmagic@gmail.com>
2022-02-04 00:18:20 -0800

    More changes to get things working

--- core/src/components/components.ts
+++ core/src/components/components.ts
@@ -2,26 +2,24 @@ import { ActionTypeComponent } from './ActionType'
 import { Alert } from './AlertMessage'
 import { BooleanGate } from './BooleanGate'
 import { Code } from './Code'
-import { DifficultyDetectorComponent } from './DifficultyDetector'
-import { EnkiTask } from './EnkiTask'
-import { EntityDetector } from './EntityDetector'
-import { ForEach } from './ForEach'
-import { Generator } from './Generator'
-import { HuggingfaceComponent } from './Huggingface'
-import { InputComponent } from './Input'
+// import { EnkiTask } from './EnkiTask'
+// import { HuggingfaceComponent } from './Huggingface'
 import { InputFieldComponent } from './deprecated/InputField'
-import { ItemTypeComponent } from './ItemDetector'
-import { JoinListComponent } from './JoinList'
-import { ModuleComponent } from './Module'
 import { ModuleInput } from './deprecated/ModuleInput'
 import { ModuleOutput } from './deprecated/ModuleOutput'
-import { TriggerIn } from './TriggerIn'
-import { TriggerOut } from './TriggerOut'
-import { Output } from './Output'
 import { PlaytestInput } from './deprecated/PlaytestInput'
 import { PlaytestPrint } from './deprecated/PlaytestPrint'
 import { ProseToScript } from './ProseToScript'
 import { RunInputComponent } from './deprecated/RunInput'
+import { DifficultyDetectorComponent } from './DifficultyDetector'
+import { EntityDetector } from './EntityDetector'
+import { ForEach } from './ForEach'
+import { Generator } from './Generator'
+import { InputComponent } from './Input'
+import { ItemTypeComponent } from './ItemDetector'
+import { JoinListComponent } from './JoinList'
+import { ModuleComponent } from './Module'
+import { Output } from './Output'
 import { SafetyVerifier } from './SafetyVerifier'
 import { StateRead } from './StateRead'
 import { StateWrite } from './StateWrite'
@@ -29,6 +27,8 @@ import { StringProcessor } from './StringProcessor'
 import { SwitchGate } from './SwitchGate'
 import { TenseTransformer } from './TenseTransformer'
 import { TimeDetectorComponent } from './TimeDetector'
+import { TriggerIn } from './TriggerIn'
+import { TriggerOut } from './TriggerOut'
 
 // Here we load up all components of the builder into our editor for usage.
 // We might be able to programatically generate components from enki
@@ -39,11 +39,11 @@ export const components = {
   booleanGate: () => new BooleanGate(),
   code: () => new Code(),
   difficultyDetectorComponent: () => new DifficultyDetectorComponent(),
-  enkiTask: () => new EnkiTask(),
+  // enkiTask: () => new EnkiTask(),
   entityDetector: () => new EntityDetector(),
   forEach: () => new ForEach(),
   generator: () => new Generator(),
-  huggingfaceComponent: () => new HuggingfaceComponent(),
+  // huggingfaceComponent: () => new HuggingfaceComponent(),
   inputComponent: () => new InputComponent(),
   inputFieldComponent: () => new InputFieldComponent(),
   itemTypeComponent: () => new ItemTypeComponent(),
@@ -63,7 +63,7 @@ export const components = {
   switchGate: () => new SwitchGate(),
   tenseTransformer: () => new TenseTransformer(),
   timeDetectorComponent: () => new TimeDetectorComponent(),
-  TriggerIn: () => new TriggerIn(),
+  triggerIn: () => new TriggerIn(),
   triggerOut: () => new TriggerOut(),
 }
 
