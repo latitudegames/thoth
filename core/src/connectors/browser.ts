5747cb2e40bf492db1e21ca3c6afac9710a09134
shawticus <shawmakesmagic@gmail.com>
2022-02-04 12:24:13 -0800

    Convert connectors to ts, very roughly

--- core/src/connectors/browser.ts
+++ core/src/connectors/browser.ts
@@ -1,9 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import fs from 'fs'
 import puppeteer from 'puppeteer'
 
 //The default browser class for puppeteer, it can be used to build other clients with puppeteer
-export async function browserWindow(options) {
-
+export async function browserWindow(options: any) {
   if (fs.existsSync('/.dockerenv')) {
     options.headless = true
     options.args = (options.args || []).concat([
@@ -16,30 +18,31 @@ export async function browserWindow(options) {
 }
 
 export class PageUtils {
-  page
+  page: any
   autoLog
-  constructor({ page, autoLog = true }) {
+  constructor({ page = null, autoLog = true }) {
     this.page = page
     this.autoLog = autoLog
   }
 
-  async clickButton(buttonName) {
-    await this.page.evaluate(selector => {
+  async clickButton(buttonName: any) {
+    await this.page.evaluate((selector: any) => {
       const v = document.querySelector(selector)
       if (v != undefined && v != null) v.click()
     }, buttonName)
   }
 
   //clicks a selector using the class name's regex
-  async clickSelectorClassRegex(selector, classRegex) {
-    if (this.autoLog) console.log(`Clicking for a ${selector} matching ${classRegex}`)
+  async clickSelectorClassRegex(selector: any, classRegex: any) {
+    if (this.autoLog)
+      console.log(`Clicking for a ${selector} matching ${classRegex}`)
 
     await this.page.evaluate(
-      (selector, _classRegex) => {
+      (selector: any, _classRegex: any) => {
         const classRegex = new RegExp(_classRegex)
         const buttons = Array.from(document.querySelectorAll(selector))
         const enterButton = buttons.find(button =>
-          Array.from(button.classList).some(c => classRegex.test(c))
+          Array.from(button.classList).some((c: string) => classRegex.test(c))
         )
         if (enterButton) enterButton.click()
       },
@@ -48,11 +51,11 @@ export class PageUtils {
     )
   }
   //clicks a selector based on their id
-  async clickSelectorId(selector, id) {
+  async clickSelectorId(selector: any, id: any) {
     if (this.autoLog) console.log(`Clicking for a ${selector} matching ${id}`)
 
     await this.page.evaluate(
-      (selector, id) => {
+      (selector: any, id: any) => {
         const matches = Array.from(document.querySelectorAll(selector))
         const singleMatch = matches.find(button => button.id === id)
         if (singleMatch && singleMatch.click) {
@@ -76,11 +79,12 @@ export class PageUtils {
     )
   }
   //clicks a selector based on their alt value
-  async clickSelectorByAlt(selector, title) {
-    if (this.autoLog) console.log(`Clicking for a ${selector} matching ${title}`)
+  async clickSelectorByAlt(selector: any, title: any) {
+    if (this.autoLog)
+      console.log(`Clicking for a ${selector} matching ${title}`)
 
     await this.page.evaluate(
-      (selector, title) => {
+      (selector: any, title: any) => {
         const matches = Array.from(document.querySelectorAll(selector))
         const singleMatch = matches.find(btn => btn.alt === title)
         if (singleMatch && singleMatch.click) {
@@ -104,10 +108,10 @@ export class PageUtils {
     )
   }
   //clicks the first selector it finds of the given type
-  async clickSelectorFirstMatch(selector) {
+  async clickSelectorFirstMatch(selector: any) {
     if (this.autoLog) console.log(`Clicking for first ${selector}`)
 
-    await this.page.evaluate(selector => {
+    await this.page.evaluate((selector: any) => {
       const matches = Array.from(document.querySelectorAll(selector))
       const singleMatch = matches[0]
       if (singleMatch) singleMatch.click()
