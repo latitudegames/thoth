c850940b2edf7969db90f8b70ca224258acba78c
alextoti <alexandrostitonis@gmail.com>
2022-02-10 16:15:27 +0200

    implemented fact nodes (client only), implemented the string combiner node, implemented the string evaluator node, added the google speech functions from superbeing

---
+++ core/src/axiosUtils.ts
@@ -0,0 +1,69 @@
+/* eslint-disable no-console */
+/* eslint-disable require-await */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Rete from 'rete'
+
+import {
+  NodeData,
+  ThothNode,
+  ThothWorkerInputs,
+  ThothWorkerOutputs,
+} from '../types'
+import { EngineContext } from './engine'
+import { triggerSocket, stringSocket, anySocket } from './sockets'
+import { ThothComponent } from './thoth-component'
+
+const info = 'Facts Recall is used to get facts for an agent and user'
+
+type InputReturn = {
+  output: unknown
+  facts: unknown
+}
+
+export class FactsRecall extends ThothComponent<Promise<InputReturn>> {
+  constructor() {
+    super('Facts Recall')
+
+    this.task = {
+      outputs: {
+        output: 'output',
+        facts: 'output',
+        trigger: 'option',
+      },
+    }
+
+    this.category = 'AI/ML'
+    this.display = true
+    this.info = info
+  }
+
+  builder(node: ThothNode) {
+    const out = new Rete.Output('output', 'Output', anySocket)
+    const factsOut = new Rete.Output('facts', 'Output', stringSocket)
+    const dataInput = new Rete.Input('trigger', 'Trigger', triggerSocket, true)
+    const dataOutput = new Rete.Output('trigger', 'Trigger', triggerSocket)
+
+    return node
+      .addInput(dataInput)
+      .addOutput(dataOutput)
+      .addOutput(out)
+      .addOutput(factsOut)
+  }
+
+  async worker(
+    node: NodeData,
+    inputs: ThothWorkerInputs,
+    outputs: ThothWorkerOutputs,
+    { silent, thoth }: { silent: boolean; thoth: EngineContext }
+  ) {
+    const action = inputs['string'][0]
+
+    console.log('post facts store', action)
+    const facts = ''
+
+    return {
+      output: action as string,
+      facts: facts,
+    }
+  }
+}
