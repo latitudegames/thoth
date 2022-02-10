57799ab4e3cb8bd3a2fe0f2154bc58aedebb33ff
alextoti <alexandrostitonis@gmail.com>
2022-02-10 16:15:27 +0200

    implemented fact nodes (client only), implemented the string combiner node, implemented the string evaluator node, added the google speech functions from superbeing

--- core/src/components/components.ts
+++ core/src/components/components.ts
@@ -1,5 +1,6 @@
 import { ActionTypeComponent } from './ActionType'
 import { Alert } from './AlertMessage'
+import { ArchiveConversation } from './ArchiveConversation'
 import { BooleanGate } from './BooleanGate'
 import { Code } from './Code'
 // import { EnkiTask } from './EnkiTask'
@@ -11,6 +12,9 @@ import { PlaytestPrint } from './deprecated/PlaytestPrint'
 import { RunInputComponent } from './deprecated/RunInput'
 import { DifficultyDetectorComponent } from './DifficultyDetector'
 import { EntityDetector } from './EntityDetector'
+import { FactsCount } from './FactsCount'
+import { FactsRecall } from './FactsRecall'
+import { FactsStore } from './FactsStore'
 import { FastGreetingDetector } from './FastGreetingDetector'
 import { FastProfanityDetector } from './FastProfanityDetector'
 import { FastQuestionDetector } from './FastQuestionDetector'
@@ -28,6 +32,8 @@ import { ProseToScript } from './ProseToScript'
 import { SafetyVerifier } from './SafetyVerifier'
 import { StateRead } from './StateRead'
 import { StateWrite } from './StateWrite'
+import { StringCombiner } from './StringCombiner'
+import { StringEvaluator } from './StringEvaluator'
 import { StringProcessor } from './StringProcessor'
 import { SwitchGate } from './SwitchGate'
 import { TenseTransformer } from './TenseTransformer'
@@ -51,7 +57,13 @@ export const components = {
   fastProfanityDetector: () => new FastProfanityDetector(),
   mlGreetingDetector: () => new MLGreetingDetector(),
   mlProfanityDetector: () => new MLProfanityDetector(),
+  factsStore: () => new FactsStore(),
+  factsRecall: () => new FactsRecall(),
+  factsCount: () => new FactsCount(),
   forEach: () => new ForEach(),
+  stringEvaluator: () => new StringEvaluator(),
+  stringCombiner: () => new StringCombiner(),
+  archiveConversation: () => new ArchiveConversation(),
   generator: () => new Generator(),
   huggingfaceComponent: () => new HuggingfaceComponent(),
   inputComponent: () => new InputComponent(),
