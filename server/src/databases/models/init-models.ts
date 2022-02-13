64c957aad2ae048262b6384b363bca9fbe2ebd59
shawticus <shawmakesmagic@gmail.com>
2022-02-12 17:17:39 -0800

    Add completion models and stuff so enki etc doesn't fail?

--- server/src/databases/models/init-models.ts
+++ server/src/databases/models/init-models.ts
@@ -1,68 +1,72 @@
 import type { Sequelize } from "sequelize";
-import { xr_world_understanding_prompt as _xr_world_understanding_prompt } from "./xr_world_understanding_prompt"
-import type { xr_world_understanding_promptAttributes, xr_world_understanding_promptCreationAttributes } from "./xr_world_understanding_prompt"
-import { agent_config as _agent_config } from "./agent_config";
-import type { agent_configAttributes, agent_configCreationAttributes } from "./agent_config";
-import { agent_facts as _agent_facts } from "./agent_facts";
-import type { agent_factsAttributes, agent_factsCreationAttributes } from "./agent_facts";
-import { agent_facts_archive as _agent_facts_archive } from "./agent_facts_archive";
-import type { agent_facts_archiveAttributes, agent_facts_archiveCreationAttributes } from "./agent_facts_archive";
-import { agent_instance as _agent_instance } from "./agent_instance";
-import type { agent_instanceAttributes, agent_instanceCreationAttributes } from "./agent_instance";
-import { agents as _agents } from "./agents";
 import type { agentsAttributes, agentsCreationAttributes } from "./agents";
-import { default_bad_words as _bad_words } from "./default_bad_words";
-import type { bad_wordsAttributes, bad_wordsCreationAttributes } from "./default_bad_words";
-import { chains as _chains } from "./chains";
+import { agents as _agents } from "./agents";
+import type { agent_configAttributes, agent_configCreationAttributes } from "./agent_config";
+import { agent_config as _agent_config } from "./agent_config";
+import type { agent_factsAttributes, agent_factsCreationAttributes } from "./agent_facts";
+import { agent_facts as _agent_facts } from "./agent_facts";
+import type { agent_facts_archiveAttributes, agent_facts_archiveCreationAttributes } from "./agent_facts_archive";
+import { agent_facts_archive as _agent_facts_archive } from "./agent_facts_archive";
+import type { agent_instanceAttributes, agent_instanceCreationAttributes } from "./agent_instance";
+import { agent_instance as _agent_instance } from "./agent_instance";
 import type { chainsAttributes, chainsCreationAttributes } from "./chains";
-import { chat_history as _chat_history } from "./chat_history";
+import { chains as _chains } from "./chains";
 import type { chat_historyAttributes, chat_historyCreationAttributes } from "./chat_history";
-import { client_settings as _client_settings } from "./client_settings";
+import { chat_history as _chat_history } from "./chat_history";
 import type { client_settingsAttributes, client_settingsCreationAttributes } from "./client_settings";
-import { config as _config } from "./config";
+import { client_settings as _client_settings } from "./client_settings";
 import type { configAttributes, configCreationAttributes } from "./config";
-import { context as _context } from "./context";
+import { config as _config } from "./config";
 import type { contextAttributes, contextCreationAttributes } from "./context";
-import { conversation as _conversation } from "./conversation";
+import { context as _context } from "./context";
 import type { conversationAttributes, conversationCreationAttributes } from "./conversation";
-import { deployedSpells as _deployedSpells } from "./deployedSpells";
+import { conversation as _conversation } from "./conversation";
+import type { bad_wordsAttributes, bad_wordsCreationAttributes } from "./default_bad_words";
+import { default_bad_words as _bad_words } from "./default_bad_words";
 import type { deployedSpellsAttributes, deployedSpellsCreationAttributes } from "./deployedSpells";
-import { dialog as _dialogue } from "./dialog";
+import { deployedSpells as _deployedSpells } from "./deployedSpells";
 import type { dialogueAttributes, dialogueCreationAttributes } from "./dialog";
-import { facts as _facts } from "./facts";
+import { dialog as _dialogue } from "./dialog";
 import type { factsAttributes, factsCreationAttributes } from "./facts";
-import { ignored_keywords as _ignored_keywords } from "./ignored_keywords";
+import { facts as _facts } from "./facts";
+import type { fewshotDataAttributes, fewshotDataCreationAttributes } from "./fewshotData";
+import { fewshotData as _fewshotData } from "./fewshotData";
+import type { fewshotSerializationAttributes, fewshotSerializationCreationAttributes } from "./fewshotSerialization";
+import { fewshotSerialization as _fewshotSerialization } from "./fewshotSerialization";
+import type { fewshotTaskAttributes, fewshotTaskCreationAttributes } from "./fewshotTask";
+import { fewshotTask as _fewshotTask } from "./fewshotTask";
 import type { ignored_keywordsAttributes, ignored_keywordsCreationAttributes } from "./ignored_keywords";
-import { leading_statements as _leading_statements } from "./leading_statements";
+import { ignored_keywords as _ignored_keywords } from "./ignored_keywords";
 import type { leading_statementsAttributes, leading_statementsCreationAttributes } from "./leading_statements";
-import { meta as _meta } from "./meta";
+import { leading_statements as _leading_statements } from "./leading_statements";
 import type { metaAttributes, metaCreationAttributes } from "./meta";
-import { monologue as _monologue } from "./monologue";
+import { meta as _meta } from "./meta";
 import type { monologueAttributes, monologueCreationAttributes } from "./monologue";
-import { morals as _morals } from "./morals";
+import { monologue as _monologue } from "./monologue";
 import type { moralsAttributes, moralsCreationAttributes } from "./morals";
-import { needs_motivations as _needs_motivations } from "./needs_motivations";
-import type { needs_motivationsAttributes, needs_motivationsCreationAttributes } from "./needs_motivations";
-import { opinion_form_prompt as _opinion_form_prompt } from "./opinion_form_prompt";
+import { morals as _morals } from "./morals";
 import type { opinion_form_promptAttributes, opinion_form_promptCreationAttributes } from "./opinion_form_prompt";
-import { personality as _personality } from "./personality";
+import { opinion_form_prompt as _opinion_form_prompt } from "./opinion_form_prompt";
 import type { personalityAttributes, personalityCreationAttributes } from "./personality";
-import { personality_questions as _personality_questions } from "./personality_questions";
+import { personality as _personality } from "./personality";
 import type { personality_questionsAttributes, personality_questionsCreationAttributes } from "./personality_questions";
-import { speaker_fact_summarization as _speaker_fact_summarization } from "./speaker_fact_summarization";
-import type { speaker_fact_summarizationAttributes, speaker_fact_summarizationCreationAttributes } from "./speaker_fact_summarization";
-import { speaker_profane_responses as _speaker_profane_responses } from "./speaker_profane_responses";
-import type { speaker_profane_responsesAttributes, speaker_profane_responsesCreationAttributes } from "./speaker_profane_responses";
-import { speakers_facts as _speakers_facts } from "./speakers_facts";
+import { personality_questions as _personality_questions } from "./personality_questions";
 import type { speakers_factsAttributes, speakers_factsCreationAttributes } from "./speakers_facts";
-import { speakers_facts_archive as _speakers_facts_archive } from "./speakers_facts_archive";
+import { speakers_facts as _speakers_facts } from "./speakers_facts";
 import type { speakers_facts_archiveAttributes, speakers_facts_archiveCreationAttributes } from "./speakers_facts_archive";
-import { speakers_model as _speakers_model } from "./speakers_model";
+import { speakers_facts_archive as _speakers_facts_archive } from "./speakers_facts_archive";
 import type { speakers_modelAttributes, speakers_modelCreationAttributes } from "./speakers_model";
-import { wikipedia as _wikipedia } from "./wikipedia";
+import { speakers_model as _speakers_model } from "./speakers_model";
+import type { speaker_fact_summarizationAttributes, speaker_fact_summarizationCreationAttributes } from "./speaker_fact_summarization";
+import { speaker_fact_summarization as _speaker_fact_summarization } from "./speaker_fact_summarization";
+import type { speaker_profane_responsesAttributes, speaker_profane_responsesCreationAttributes } from "./speaker_profane_responses";
+import { speaker_profane_responses as _speaker_profane_responses } from "./speaker_profane_responses";
 import type { wikipediaAttributes, wikipediaCreationAttributes } from "./wikipedia";
-import { xr_engine_room_prompt as _xr_engine_room_prompt } from "./xr_engine_room_prompt";
+import { wikipedia as _wikipedia } from "./wikipedia";
 import type { xr_engine_room_promptAttributes, xr_engine_room_promptCreationAttributes } from "./xr_engine_room_prompt";
+import { xr_engine_room_prompt as _xr_engine_room_prompt } from "./xr_engine_room_prompt";
+import type { xr_world_understanding_promptAttributes, xr_world_understanding_promptCreationAttributes } from "./xr_world_understanding_prompt";
+import { xr_world_understanding_prompt as _xr_world_understanding_prompt } from "./xr_world_understanding_prompt";
 
 export {
   _xr_world_understanding_prompt as xr_world_understanding_prompt,
@@ -81,12 +85,14 @@ export {
   _deployedSpells as deployedSpells,
   _dialogue as dialog,
   _facts as facts,
+  _fewshotData as fewshotData,
+  _fewshotSerialization as fewshotSerialization,
+  _fewshotTask as fewshotTask,
   _ignored_keywords as ignored_keywords,
   _leading_statements as leading_statements,
   _meta as meta,
   _monologue as monologue,
   _morals as morals,
-  _needs_motivations as needs_motivations,
   _opinion_form_prompt as opinion_form_prompt,
   _personality as personality,
   _personality_questions as personality_questions,
@@ -98,7 +104,6 @@ export {
   _wikipedia as wikipedia,
   _xr_engine_room_prompt as xr_engine_room_prompt,
 };
-
 export type {
   xr_world_understanding_promptAttributes,
   xr_world_understanding_promptCreationAttributes,
@@ -132,6 +137,12 @@ export type {
   dialogueCreationAttributes,
   factsAttributes,
   factsCreationAttributes,
+  fewshotDataAttributes,
+  fewshotDataCreationAttributes,
+  fewshotSerializationAttributes,
+  fewshotSerializationCreationAttributes,
+  fewshotTaskAttributes,
+  fewshotTaskCreationAttributes,
   ignored_keywordsAttributes,
   ignored_keywordsCreationAttributes,
   leading_statementsAttributes,
@@ -142,8 +153,6 @@ export type {
   monologueCreationAttributes,
   moralsAttributes,
   moralsCreationAttributes,
-  needs_motivationsAttributes,
-  needs_motivationsCreationAttributes,
   opinion_form_promptAttributes,
   opinion_form_promptCreationAttributes,
   personalityAttributes,
@@ -166,6 +175,7 @@ export type {
   xr_engine_room_promptCreationAttributes,
 };
 
+
 export function initModels(sequelize: Sequelize) {
   const xr_world_understanding_prompt = _xr_world_understanding_prompt.initModel(sequelize);
   const agent_config = _agent_config.initModel(sequelize);
@@ -183,12 +193,14 @@ export function initModels(sequelize: Sequelize) {
   const deployedSpells = _deployedSpells.initModel(sequelize);
   const dialog = _dialogue.initModel(sequelize);
   const facts = _facts.initModel(sequelize);
+  const fewshotData = _fewshotData.initModel(sequelize);
+  const fewshotSerialization = _fewshotSerialization.initModel(sequelize);
+  const fewshotTask = _fewshotTask.initModel(sequelize);
   const ignored_keywords = _ignored_keywords.initModel(sequelize);
   const leading_statements = _leading_statements.initModel(sequelize);
   const meta = _meta.initModel(sequelize);
   const monologue = _monologue.initModel(sequelize);
   const morals = _morals.initModel(sequelize);
-  const needs_motivations = _needs_motivations.initModel(sequelize);
   const opinion_form_prompt = _opinion_form_prompt.initModel(sequelize);
   const personality = _personality.initModel(sequelize);
   const personality_questions = _personality_questions.initModel(sequelize);
@@ -217,12 +229,14 @@ export function initModels(sequelize: Sequelize) {
     deployedSpells: deployedSpells,
     dialog: dialog,
     facts: facts,
+    fewshotData: fewshotData,
+    fewshotSerialization: fewshotSerialization,
+    fewshotTask: fewshotTask,
     ignored_keywords: ignored_keywords,
     leading_statements: leading_statements,
     meta: meta,
     monologue: monologue,
     morals: morals,
-    needs_motivations: needs_motivations,
     opinion_form_prompt: opinion_form_prompt,
     personality: personality,
     personality_questions: personality_questions,
