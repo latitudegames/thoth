941b3614cec351a387772e84dadc638fcc3ba9c0
shawticus <shawmakesmagic@gmail.com>
2022-02-18 06:11:47 -0800

    Fix weird things

--- core/src/components/makeModelRequest.ts
+++ core/src/components/makeModelRequest.ts
@@ -2,8 +2,8 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-nocheck
 
+import axios from 'axios'
 import { config } from 'dotenv'
-import fetch from 'node-fetch'
 config()
 
 //Model Request using the Hugging Face API (models can be found at -> https://huggingface.co/models)
@@ -14,7 +14,7 @@ export async function makeModelRequest(
   options = { use_cache: false, wait_for_model: true }
 ) {
   try {
-    const response = await fetch(
+    const response = await axios(
       `https://api-inference.huggingface.co/models/${model}`,
       {
         headers: {
