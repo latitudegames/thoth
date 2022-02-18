e10b64bd912b09c1e879be1c5be2e667a93d23fa
alextoti <alexandrostitonis@gmail.com>
2022-02-18 15:41:50 +0200

    added voice support through google cloud (server)

--- client/src/features/Thoth/windows/PlaytestWindow.jsx
+++ client/src/features/Thoth/windows/PlaytestWindow.jsx
@@ -5,6 +5,7 @@ import { useHotkeys } from 'react-hotkeys-hook'
 import { usePubSub } from '../../../contexts/PubSubProvider'
 import Window from '../../common/Window/Window'
 import css from '../thoth.module.css'
+import singleton from '../../../speechUtils'
 
 const Input = props => {
   const ref = useRef()
@@ -18,6 +19,18 @@ const Input = props => {
     [props, ref]
   )
 
+  useEffect(() => {
+    window.onbeforeunload = () => {
+      if (
+        props &&
+        props.recording &&
+        !singleton.getInstance().streamStreaming
+      ) {
+        singleton.getInstance().socket?.emit('endGoogleCloudStream', '')
+      }
+    }
+  }, [])
+
   return (
     <div className={css['playtest-input']}>
       <input
@@ -29,11 +42,69 @@ const Input = props => {
       <button className="small" onClick={props.onSend}>
         Send
       </button>
+      <button
+        className="small"
+        onClick={props.recording ? props.stopRecord : props.record}
+      >
+        {props.recording ? 'Stop Recording' : 'Record'}
+      </button>
     </div>
   )
 }
 
 const Playtest = ({ tab }) => {
+  const recordingRef = useRef(false)
+  const [update, setUpdate] = useState(false)
+
+  const startRecording = updateState => {
+    if (recordingRef.current === true) return
+    recordingRef.current = true
+    singleton.getInstance().initRecording(text => {
+      send(text)
+    })
+    if (updateState) {
+      setUpdate(!update)
+    }
+  }
+  const stopRecording = updateState => {
+    if (recordingRef.current === false) return
+    singleton.getInstance().stopRecording()
+    recordingRef.current = false
+    if (updateState) {
+      setUpdate(!update)
+    }
+  }
+
+  useHotkeys(
+    'shift+r',
+    () => {
+      startRecording(false)
+    },
+    { enableOnTags: 'INPUT' },
+    [recordingRef]
+  )
+
+  document.addEventListener(
+    'keyup',
+    function (event) {
+      document.addEventListener('keyup', handleEventListener(event))
+
+      return () =>
+        document.removeEventListener('keyup', handleEventListener(event))
+    },
+    []
+  )
+
+  function handleEventListener(event) {
+    if (
+      (event.key.toLowerCase() === 'r' ||
+        event.key.toLowerCase() === 'shift') &&
+      recordingRef.current === true
+    ) {
+      stopRecording(false)
+    }
+  }
+
   const [history, setHistory] = useState([])
   const [value, setValue] = useState('')
 
@@ -67,6 +138,12 @@ const Playtest = ({ tab }) => {
     publish($PLAYTEST_INPUT(tab.id), value || ' ')
     setValue('')
   }
+  const send = text => {
+    console.log('send:', text)
+    const newHistory = [...history, `You: ${text}`]
+    setHistory(newHistory)
+    publish($PLAYTEST_INPUT(tab.id), text)
+  }
 
   const onChange = e => {
     setValue(e.target.value)
@@ -92,7 +169,14 @@ const Playtest = ({ tab }) => {
             <ul>{history.map(printItem)}</ul>
           </Scrollbars>
         </div>
-        <Input onChange={onChange} value={value} onSend={onSend} />
+        <Input
+          onChange={onChange}
+          value={value}
+          onSend={onSend}
+          recording={recordingRef.current}
+          record={() => startRecording(true)}
+          stopRecord={() => stopRecording(true)}
+        />
       </div>
     </Window>
   )
---
+++ server/src/utils/googleSpeechToText.ts
@@ -0,0 +1,76 @@
+import { SpeechClient } from '@google-cloud/speech'
+import { Server } from 'socket.io'
+
+let speechClient: SpeechClient
+const encoding = 'LINEAR16'
+const sampleRateHertz = 16000
+const languageCode = 'en-US'
+
+const request = {
+  config: {
+    encoding: encoding as any,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+    profanityFilter: false,
+    enableWordTimeOffsets: true,
+    // speechContexts: [{
+    //     phrases: ["hi","hello"]
+    //    }]
+  },
+  interimResults: false,
+}
+
+export async function initSpeechServer() {
+  const io = new Server(parseInt(process.env.SPEECH_SERVER_PORT as string), {
+    cors: { origin: '*', methods: ['GET', 'POST'] },
+  })
+  console.log('speech server started on port', process.env.SPEECH_SERVER_PORT)
+
+  speechClient = new SpeechClient()
+
+  io.on('connection', function (client) {
+    let recognizeStream: any = null
+
+    client.on('join', function (data) {
+      client.emit('messages', 'Client connected')
+    })
+    client.on('messages', function (data) {
+      client.emit('broad', data)
+    })
+
+    client.on('startGoogleCloudStream', function (data) {
+      startRecognitionStream(client)
+    })
+    client.on('endGoogleCloudStream', function (data) {
+      stopRecognitionStream()
+    })
+
+    client.on('binaryData', function (data) {
+      if (recognizeStream !== null) {
+        recognizeStream.write(data)
+      }
+    })
+
+    function startRecognitionStream(client: any) {
+      recognizeStream = speechClient
+        .streamingRecognize(request)
+        .on('error', err => {
+          console.log(err)
+        })
+        .on('data', data => {
+          client.emit('speechData', data)
+          if (data.results[0] && data.results[0].isFinal) {
+            stopRecognitionStream()
+            startRecognitionStream(client)
+          }
+        })
+    }
+
+    function stopRecognitionStream() {
+      if (recognizeStream) {
+        recognizeStream.end()
+      }
+      recognizeStream = null
+    }
+  })
+}
