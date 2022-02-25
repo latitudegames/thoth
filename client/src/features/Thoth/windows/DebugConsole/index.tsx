49aa20f8a300d51e171ff92751ea17f6dfa9cd6d
Jakob Grant <haruhunab1320@gmail.com>
2022-02-24 18:07:36 -0800

    add comments to commands

--- client/src/features/Thoth/windows/DebugConsole/index.tsx
+++ client/src/features/Thoth/windows/DebugConsole/index.tsx
@@ -27,15 +27,19 @@ const DebugConsole = ({ tab }) => {
     [messages]
   )
 
-const commands = {
-  echo: {
-    description: 'Echo a passed string.',
-    usage: 'echo <string>',
-    fn: function () {
-      return `${Array.from(arguments).join(' ')}`
+
+  /**
+   * Terminal commands
+   */
+  const commands = {
+    echo: {
+      description: 'Echo a passed string.',
+      usage: 'echo <string>',
+      fn: function () {
+        return `${Array.from(arguments).join(' ')}`
+      },
     },
-  },
-}
+  }
   // https://github.com/linuswillner/react-console-emulator/tree/e2b602f631e8b7c57c4a7407491cbfb84f357519
   return (
     <Terminal
