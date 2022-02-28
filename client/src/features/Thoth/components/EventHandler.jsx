fec73eaf1c0420f0e27a8af789a82e17fae0319b
alextoti <alexandrostitonis@gmail.com>
2022-02-28 12:07:36 +0200

    implemented the search corpus

--- client/src/features/Thoth/components/EventHandler.jsx
+++ client/src/features/Thoth/components/EventHandler.jsx
@@ -37,6 +37,7 @@ const EventHandler = ({ pubSub, tab }) => {
     $SAVE_SPELL_AS,
     $CREATE_STATE_MANAGER,
     $CREATE_AGENT_MANAGER,
+    $CREATE_SEARCH_CORPUS,
     $CREATE_ENT_MANAGER,
     $CREATE_CONFIG_MANAGER,
     $CREATE_PLAYTEST,
@@ -64,6 +65,10 @@ const EventHandler = ({ pubSub, tab }) => {
 
   const createAgentManager = () => {
     createOrFocus(windowTypes.AGENT_MANAGER, 'Agent Manager')
+  }
+
+  const createSearchCorpus = () => {
+    createOrFocus(windowTypes.SEARCH_CORPUS, 'Search Corpus')
   }
 
   const createConfigManager = () => {
@@ -136,6 +141,7 @@ const EventHandler = ({ pubSub, tab }) => {
     [$SAVE_SPELL(tab.id)]: saveSpell,
     [$CREATE_STATE_MANAGER(tab.id)]: createStateManager,
     [$CREATE_AGENT_MANAGER(tab.id)]: createAgentManager,
+    [$CREATE_SEARCH_CORPUS(tab.id)]: createSearchCorpus,
     [$CREATE_ENT_MANAGER(tab.id)]: createEntManager,
     [$CREATE_CONFIG_MANAGER(tab.id)]: createConfigManager,
     [$CREATE_PLAYTEST(tab.id)]: createPlaytest,
