685dd59b1e9a815628100046cdc7410f9b73f0e1
Michael Sharpe <m.sharpe@project89.org>
2022-02-28 09:41:11 -0800

    Start work on component error highlighting

--- core/src/plugins/debuggerPlugin/index.ts
+++ core/src/plugins/debuggerPlugin/index.ts
@@ -1,21 +1,45 @@
-import { Component, NodeEditor } from 'rete/types'
+import { NodeEditor } from 'rete/types'
+import { Node } from 'rete'
 import { EngineContext } from '../../engine'
-
+import { ThothComponent } from '../../thoth-component'
 interface IRunContextEditor extends NodeEditor {
   thoth: EngineContext
+  abort: Function
 }
 
 function install(editor: IRunContextEditor) {
-  editor.on('componentregister', (component: Component) => {
+  editor.on('componentregister', (component: ThothComponent<unknown>) => {
     const worker = component.worker
+    const builder = component.builder
+
+    component.builder = node => {
+      node.data.error = false
+
+      return builder.call(component, node)
+    }
 
     component.worker = (node, inputs, outputs, data, ...args) => {
+      node.data.error = false
       try {
-        return worker.apply(component, [node, inputs, outputs, data, ...args])
+        const result = worker.apply(component, [
+          node,
+          inputs,
+          outputs,
+          data,
+          ...args,
+        ])
+
+        return result
       } catch (error: any) {
         if (!editor.thoth.sendToDebug) return
+
         editor.thoth.sendToDebug({ message: error })
-        console.error(error)
+        node.data.error = true
+
+        const fullNode = Node.fromJSON(node)
+        console.log('node object', fullNode)
+        editor.selectNode(fullNode)
+        throw error
       }
     }
   })
