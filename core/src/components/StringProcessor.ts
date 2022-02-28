663dc7b38c4ddf4676b6dfa1334d3e4fe873b39f
Jakob Grant <haruhunab1320@gmail.com>
2022-02-28 14:44:11 -0800

    fix typos in stringprocessor

--- core/src/components/StringProcessor.ts
+++ core/src/components/StringProcessor.ts
@@ -5,9 +5,9 @@ import { CodeControl } from '../dataControls/CodeControl'
 import { SocketGeneratorControl } from '../dataControls/SocketGenerator'
 import { stringSocket, triggerSocket } from '../sockets'
 import { ThothComponent } from '../thoth-component'
-const info = `The String Processor component take s astring as an input and allows you to write a function in the text editor to parse that string in whatever way you need.  You can define any number of outputs which you can pass the result of your parsing out through.
+const info = `The String Processor component takes a string as an input and allows you to write a function in the text editor to parse that string in whatever way you need.  You can define any number of outputs which you can pass the result of your parsing out through.
 
-Note that the return value of your function must be an objetc whose keys match the names of your generated output sockets.`
+Note that the return value of your function must be an object whose keys match the names of your generated output sockets.`
 
 export class StringProcessor extends ThothComponent<Record<string, string>> {
   constructor() {
