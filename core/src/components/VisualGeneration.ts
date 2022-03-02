e68d7a58e3f936871abd3168acf6af0b56550729
Jakob Grant <haruhunab1320@gmail.com>
2022-03-02 10:55:41 -0800

    strip out reference to cacheTag in visualgeneration component

--- core/src/components/VisualGeneration.ts
+++ core/src/components/VisualGeneration.ts
@@ -10,11 +10,9 @@ import { EngineContext } from '../engine'
 import { triggerSocket, stringSocket, arraySocket } from '../sockets'
 import { ThothComponent } from '../thoth-component'
 
-const info = `The VisualGeneration component is used to access the image cache. You pass it a caption, a cacheTag and an optional topK value and it returns an array of images that are related to your caption. 
+const info = `The VisualGeneration component is used to access the image cache. You pass it a caption and an optional topK value and it returns an array of images that are related to your caption. 
 
 caption- is a string related to what type of image you want to search for.
-
-cacheTag- {insert cacheTag description here}. 
 
 topK- number of (k) matches for a particular description. IE: if you submit as a caption: "castle" and k was 5, it would return a fortress, a keep, a battlement, a gatehouse, and a tower. The k=5 images most similar to the word "castle`
 
