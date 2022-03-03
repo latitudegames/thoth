9b3a2ea084e9f5f3e3569ea86fc8a200f957f382
Michael Sharpe <m.sharpe@project89.org>
2022-03-03 13:54:46 -0800

    Remove unused code

--- client/src/features/common/RequireAuth/RequireAuth.tsx
+++ client/src/features/common/RequireAuth/RequireAuth.tsx
@@ -1,11 +1,10 @@
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { useAuthContext } from '../../../contexts/NewAuthProvider'
 
 const defaultGroups = ['internal', 'thoth']
 
 const RequireAuth = (props: Record<string, any>) => {
   const { user, loginRedirect } = useAuthContext()
-  const navigate = useNavigate()
   const groups = props?.access
     ? [...props?.access, ...defaultGroups]
     : defaultGroups
