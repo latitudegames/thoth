421aaed8904a1492c659b98d6222ec4f38f67881
Jakob Grant <haruhunab1320@gmail.com>
2022-03-04 11:54:12 -0800

    change database provider back to js

--- client/src/contexts/DatabaseProvider.jsx
+++ client/src/contexts/DatabaseProvider.jsx
@@ -1,19 +1,8 @@
 import { useContext, createContext, useState, useEffect } from 'react'
 import { initDB } from '../database'
 import LoadingScreen from '../features/common/LoadingScreen/LoadingScreen'
-import { ModuleModal } from '@/database/models/moduleModel'
 
-interface ContextType {
-  db: {}
-  models: {}
-  modules?: ModuleModal[] | ModuleModal
-}
-
-const Context = createContext<ContextType>({
-  db: {},
-  models: {},
-  modules: [] as ModuleModal[],
-})
+const Context = createContext(!undefined)
 
 export const useDB = () => useContext(Context)
 
@@ -35,7 +24,6 @@ const DatabaseProvider = ({ children }) => {
   const publicInterface = {
     db,
     models,
-    // modules
   }
 
   if (!db || !models) return <LoadingScreen />
