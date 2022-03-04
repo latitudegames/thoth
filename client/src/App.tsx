fc37119eda1af66b923937d085346447cc7afca1
Jakob Grant <haruhunab1320@gmail.com>
2022-03-03 21:52:04 -0800

    remove deleted routes from app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 
-import { useAuth } from './contexts/AuthProvider'
 import { useTabManager } from './contexts/TabManagerProvider'
 import RequireAuth from './features/common/RequireAuth/RequireAuth'
 import LoadingScreen from './features/common/LoadingScreen/LoadingScreen'
 import ThothPageWrapper from './features/common/ThothPage/ThothPageWrapper'
-import LoginScreen from './features/Login/LoginScreen'
 import HomeScreen from './features/HomeScreen/HomeScreen'
 import Thoth from './features/Thoth/Thoth'
+import { useAuthContext } from './contexts/NewAuthProvider'
 
 import 'flexlayout-react/style/dark.css'
 import './design-globals/design-globals.css'
@@ -17,7 +16,7 @@ import './App.css'
 function App() {
   // Use our routes
   const { tabs, activeTab } = useTabManager()
-  const { user } = useAuth()
+  const { user } = useAuthContext()
 
   const redirect = () => {
     if (user && tabs.length > 0) {
@@ -33,7 +32,6 @@ function App() {
     <ThothPageWrapper tabs={tabs} activeTab={activeTab}>
       <Routes>
         <Route element={<RequireAuth />}>
-          <Route path="/login" element={<LoginScreen />} />
           <Route path="/thoth" element={<Thoth />} />
           <Route path="/thoth/:spellName" element={<Thoth />} />
           <Route path="/home/*" element={<HomeScreen />} />
