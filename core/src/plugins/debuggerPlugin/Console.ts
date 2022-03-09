89e14a12c17753ed98036ddb5105d60a0fe148b0
Michael Sharpe <m.sharpe@project89.org>
2022-03-09 10:10:13 -0800

    Attache node view to console class

--- core/src/plugins/debuggerPlugin/Console.ts
+++ core/src/plugins/debuggerPlugin/Console.ts
@@ -1,3 +1,4 @@
+import { NodeView } from 'rete/types/view/node'
 import { IRunContextEditor, ThothNode } from '../../../types'
 import { ThothComponent } from '../../thoth-component'
 
@@ -11,10 +12,23 @@ export class Console {
   node: ThothNode
   editor: IRunContextEditor
   component: ThothComponent<unknown>
+  nodeView: NodeView
 
   constructor({ component, editor, node }: ConsoleConstructor) {
     this.component = component
     this.editor = editor
     this.node = node
+
+    const nodeValues = Array.from(editor.view.nodes)
+    const foundNode = nodeValues.find(([, n]) => n.node.id === node.id)
+
+    if (!foundNode) return
+
+    this.nodeView = foundNode[1]
+  }
+
+  updateNodeView() {
+    this.nodeView.onStart()
+    this.nodeView.node.update()
   }
 }
