0eb0bc781bc97dc44c9ba217cc865db8956f6a8a
Michael Sharpe <m.sharpe@project89.org>
2022-03-09 17:08:34 -0800

    Comment out console override

--- core/src/plugins/debuggerPlugin/index.ts
+++ core/src/plugins/debuggerPlugin/index.ts
@@ -6,12 +6,13 @@ function install(
   editor: IRunContextEditor,
   { server = false, throwError }: { server?: boolean; throwError?: Function }
 ) {
-  const consoleLog = console.log
+  // const _log = console.log
 
-  console.log = function (message) {
-    // if (editor.thoth.sendToDebug) editor.thoth.sendToDebug(message)
-    consoleLog.apply(console, arguments)
-  }
+  // console.log = function (message) {
+  //   // if (editor.thoth.sendToDebug) editor.thoth.sendToDebug(message)
+  //   console.warn('testing')
+  //   return Function.prototype.bind.call(_log, arguments)
+  // }
 
   editor.on('componentregister', (component: ThothComponent<unknown>) => {
     const worker = component.worker
