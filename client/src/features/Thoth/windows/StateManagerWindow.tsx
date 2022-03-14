939c95941bb6aad5e365b08281842a64095fbef7
Jakob Grant <haruhunab1320@gmail.com>
2022-03-14 11:57:03 -0700

    remove unused function

--- client/src/features/Thoth/windows/StateManagerWindow.tsx
+++ client/src/features/Thoth/windows/StateManagerWindow.tsx
@@ -1,10 +1,10 @@
 // @ts-nocheck
 import Editor from '@monaco-editor/react'
 import jsonFormat from 'json-format'
+// import debounce from 'lodash.debounce'
 import { useSnackbar } from 'notistack'
 import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-
 import {
   selectSpellById,
   useSaveSpellMutation,
