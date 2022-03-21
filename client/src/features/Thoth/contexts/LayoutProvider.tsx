5bad5c2be3e46722fef7c9630c50c7fad811f0f5
Michael Sharpe <m.sharpe@project89.org>
2022-03-21 16:52:17 -0700

    Convert layout provider to typescript and strip out tab provider

--- client/src/features/Thoth/contexts/LayoutProvider.tsx
+++ client/src/features/Thoth/contexts/LayoutProvider.tsx
@@ -10,10 +10,12 @@ import { useContext, createContext, useEffect, useState, useRef } from 'react'
 
 import LoadingScreen from '@common/LoadingScreen/LoadingScreen'
 import { usePubSub } from '@/contexts/PubSubProvider'
-import { useTabManager } from '@/contexts/TabManagerProvider'
 import { useGetSpellQuery, useSaveSpellMutation } from '@/state/api/spells'
+import { InspectorData } from '@latitudegames/thoth-core/types'
+import { saveTabLayout } from '@/state/tabs'
+import { useDispatch } from 'react-redux'
 // Component types are listed here which are used to load components from the data sent by rete
-const windowTypes = {
+const windowTypes: WindowTypes = {
   TEXT_EDITOR: 'textEditor',
   INSPECTOR: 'inspector',
   STATE_MANAGER: 'stateManager',
@@ -21,25 +23,39 @@ const windowTypes = {
   PLAYTEST: 'playtest',
 }
 
+type WindowType =
+  | 'textEditor'
+  | 'inspector'
+  | 'stateManager'
+  | 'editor'
+  | 'playtest'
+
+type WindowTypes = Record<string, WindowType>
+
 // helpful resources
 // https://github.com/edemaine/comingle/blob/726d42e975307beb5281fddbf576591c36c1022d/client/Room.coffee#L365-L384
 // https://github.com/caplin/FlexLayout/blob/master/examples/demo/App.tsx
 
-const Context = createContext({
-  inspectorData: {},
-  textEditorData: {},
-  createModel: () => {},
-  currentModel: {},
-  currentRef: {},
-  setCurrentRef: () => {},
-  saveInspector: () => {},
-  saveTextEditor: () => {},
-  createOrFocus: () => {},
-  addWindow: () => {},
-  windowTypes: {},
-  workspaceMap: {},
-  getWorkspace: () => {},
-})
+declare global {
+  interface Window {
+    getLayout: any
+  }
+}
+
+type LayoutContext = {
+  inspectorData: InspectorData | null
+  textEditorData: Partial<InspectorData> | null
+  saveTextEditor: Function
+  saveInspector: Function
+  currentModel: Model | null
+  createModel: Function
+  createOrFocus: Function
+  windowTypes: WindowTypes
+  currentRef
+  setCurrentRef
+}
+
+const Context = createContext<LayoutContext>(undefined!)
 
 export const useLayout = () => useContext(Context)
 
@@ -47,50 +63,54 @@ const LayoutProvider = ({ children, tab }) => {
   const { subscribe, publish, events } = usePubSub()
 
   const [saveSpell] = useSaveSpellMutation()
-  const { data: spell } = useGetSpellQuery(tab.spell, {
-    skip: !tab.spell,
+  const { data: spell } = useGetSpellQuery(tab.spellId, {
+    skip: !tab.spellId,
   })
-  const currentModelRef = useRef({})
+  const currentModelRef = useRef<Model | null>(null)
 
-  const [currentModel, setCurrentModel] = useState(null)
+  const [currentModel, setCurrentModel] = useState<Model | null>(null)
   const [currentRef, setCurrentRef] = useState(null)
-  const [inspectorData, setInspectorData] = useState(null)
+  const [inspectorData, setInspectorData] = useState<InspectorData | null>(null)
   const [textEditorData, setTextEditorData] = useState({})
 
-  const updateCurrentModel = model => {
+  const updateCurrentModel = (model: Model) => {
     currentModelRef.current = model
     setCurrentModel(model)
   }
 
   useEffect(() => {
-    window.getLayout = () => currentModelRef.current.toJson()
+    window.getLayout = () =>
+      currentModelRef?.current && currentModelRef?.current?.toJson()
   }, [currentModel])
 
   // inspector subscription
   useEffect(() => {
-    return subscribe(events.$INSPECTOR_SET(tab.id), (event, data) => {
-      if (data?.nodeId !== inspectorData?.nodeId) setInspectorData({})
-      setInspectorData(data)
+    return subscribe(
+      events.$INSPECTOR_SET(tab.id),
+      (event, data: InspectorData) => {
+        if (data?.nodeId !== inspectorData?.nodeId) setInspectorData(null)
+        setInspectorData(data)
 
-      if (!data.dataControls) return
+        if (!data.dataControls) return
 
-      // Handle components in a special way here.  Could probaby abstract this better
+        // Handle components in a special way here.  Could probaby abstract this better
 
-      Object.entries(data.dataControls).forEach(([, control]) => {
-        if (control?.options?.editor) {
-          // we relay data to the text editor component for display here as well.
-          const textData = {
-            data: data.data[control.dataKey],
-            nodeId: data.nodeId,
-            name: data.name,
-            control: control,
-            options: control.options,
+        Object.entries(data.dataControls).forEach(([, control]) => {
+          if (control?.options?.editor) {
+            // we relay data to the text editor component for display here as well.
+            const textData = {
+              data: data.data[control.dataKey],
+              nodeId: data.nodeId,
+              name: data.name,
+              control: control,
+              options: control.options,
+            }
+
+            setTextEditorData(textData)
           }
-
-          setTextEditorData(textData)
-        }
-      })
-    })
+        })
+      }
+    )
   }, [events, subscribe, publish])
 
   // text editor subscription
@@ -112,6 +132,8 @@ const LayoutProvider = ({ children, tab }) => {
       [textData.control.dataKey]: textData.data,
     }
 
+    if (!inspectorData) return
+
     const update = {
       ...inspectorData,
       data: {
@@ -124,13 +146,13 @@ const LayoutProvider = ({ children, tab }) => {
     if (inspectorData) {
       setInspectorData(update)
     }
-    saveSpell(spell)
+    spell && saveSpell(spell)
   }
 
   const saveInspector = inspectorData => {
     setInspectorData(inspectorData)
     publish(events.$NODE_SET(tab.id, inspectorData.nodeId), inspectorData)
-    saveSpell(spell)
+    spell && saveSpell(spell)
   }
 
   const createModel = json => {
@@ -152,13 +174,20 @@ const LayoutProvider = ({ children, tab }) => {
     }
     const currentModel = currentModelRef.current
 
+    if (!currentModel) return
+
+    // TODO the types provided by react flex layout are wrong for these constructors. Fix with a PR or a fork of the library?
     const rootNode = currentModel.getRoot()
+    // @ts-expect-error
     const tabNode = new TabNode(currentModel, tabJson)
+    // @ts-expect-error
     const tabSetNode = new TabSetNode(currentModel, {
       type: 'tabset',
       weight: 12,
     })
 
+    // We are here using a provate variable, so TS isnt picking it up
+    // @ts-expect-error
     rootNode._addChild(tabSetNode)
 
     currentModel.doAction(
@@ -172,8 +201,14 @@ const LayoutProvider = ({ children, tab }) => {
   }
 
   const createOrFocus = (componentName, title) => {
+    if (!currentModelRef.current || !currentModel) return
+
+    // We are here using a provate variable, so TS isnt picking it up
+    // @ts-expect-error
     const component = Object.entries(currentModelRef.current._idMap).find(
       ([, value]) => {
+        // Since there is not type for _idMap, we don't know the type value is.
+        // @ts-expect-error
         return value._attributes?.component === componentName
       }
     )
@@ -200,8 +235,8 @@ const LayoutProvider = ({ children, tab }) => {
 }
 
 export const Layout = ({ json, factory, tab }) => {
+  const dispatch = useDispatch()
   const { currentModel, createModel, setCurrentRef } = useLayout()
-  const { saveTabLayout } = useTabManager()
   const layoutRef = useRef(null)
 
   useEffect(() => {
@@ -214,7 +249,10 @@ export const Layout = ({ json, factory, tab }) => {
   }, [layoutRef, setCurrentRef])
 
   const onModelChange = () => {
-    saveTabLayout(tab.id, currentModel.toJson())
+    if (!currentModel) return
+    dispatch(
+      saveTabLayout({ tabId: tab.id, layoutJson: currentModel.toJson() })
+    )
   }
 
   if (!currentModel) return <LoadingScreen />
@@ -225,7 +263,7 @@ export const Layout = ({ json, factory, tab }) => {
       ref={layoutRef}
       model={currentModel}
       factory={factory}
-      font={{ size: '12px', fontFamily: 'IBM Plex Sans' }}
+      font={{ size: '12px' }}
     />
   )
 }
