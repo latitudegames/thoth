ce866f7c3aea432cdf8f08a4a7b1cd0630cc2b4a
Michael Sharpe <m.sharpe@project89.org>
2022-03-21 11:27:07 -0700

    Show snackbar error when save fails

--- client/src/features/Thoth/windows/StateManagerWindow.tsx
+++ client/src/features/Thoth/windows/StateManagerWindow.tsx
@@ -95,7 +95,13 @@ const StateManager = ({ tab, ...props }) => {
         gameState: parsedState,
       }
       const res = await saveSpell(spellUpdate)
-      if ('error' in res) return
+      if ('error' in res) {
+        enqueueSnackbar('Error saving state', {
+          preventDuplicate: true,
+          variant: 'error',
+        })
+        throw new Error('Error saving spell')
+      }
       res.data.gameState && setCode(JSON.stringify(res.data.gameState?.state))
 
       enqueueSnackbar('State saved', {
