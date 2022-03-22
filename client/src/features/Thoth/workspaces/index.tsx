fe676a6ed598308638fbb9f73e21d568d8088ae5
Michael Sharpe <m.sharpe@project89.org>
2022-03-21 17:05:52 -0700

    Add some additional comments and todos

--- client/src/features/Thoth/workspaces/index.tsx
+++ client/src/features/Thoth/workspaces/index.tsx
@@ -1,6 +1,9 @@
 import WorkspaceProvider from '../contexts/WorkspaceProvider'
 import Composer from './composer'
 
+// TODO create a proper workspace component that can take in everything we need it to
+// for a standalone workspace environment.  Factor, events, etc.
+// Workspace should register events with the events provider, etc.
 const workspaceMap = {
   spell: Composer,
   module: Composer,
@@ -10,6 +13,8 @@ const Workspaces = ({ tabs, pubSub, activeTab }) => {
   return (
     <>
       {tabs.map(tab => {
+        //TODO use a tab.fileType instead of tab.type
+        // this will allow us to begin to expand towards a file base approach to the application.
         const Workspace = workspaceMap[tab.type]
 
         const props = {
