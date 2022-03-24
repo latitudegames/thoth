7e1bd8b43c2a544b470cbf021c08d27cebea135e
Michael Sharpe <m.sharpe@project89.org>
2022-03-24 13:54:38 -0700

    Hook up subscription to inspector set event in provider

--- client/src/workspaces/contexts/InspectorProvider.tsx
+++ client/src/workspaces/contexts/InspectorProvider.tsx
@@ -1,5 +1,6 @@
 import { usePubSub } from '@/contexts/PubSubProvider'
-import { createContext, useContext } from 'react'
+import { InspectorData } from '@latitudegames/thoth-core/types'
+import { createContext, useContext, useEffect, useState } from 'react'
 
 type InspectorContext = {}
 
@@ -7,8 +8,39 @@ const Context = createContext<InspectorContext>(undefined!)
 
 export const useInspector = () => useContext(Context)
 
-const InspectorProvider = ({ children }) => {
+const InspectorProvider = ({ children, tab }) => {
   const { subscribe, publish, events } = usePubSub()
+
+  const [inspectorData, setInspectorData] = useState<InspectorData | null>(null)
+
+  // inspector subscription
+  useEffect(() => {
+    return subscribe(
+      events.$INSPECTOR_SET(tab.id),
+      (event, data: InspectorData) => {
+        if (data?.nodeId !== inspectorData?.nodeId) setInspectorData(null)
+        setInspectorData(data)
+
+        if (!data.dataControls) return
+
+        // Handle components in a special way here.  Could probaby abstract this better
+
+        Object.entries(data.dataControls).forEach(([, control]) => {
+          if (control?.options?.editor) {
+            // we relay data to the text editor component for display here as well.
+            // const textData = {
+            //   data: data.data[control.dataKey],
+            //   nodeId: data.nodeId,
+            //   name: data.name,
+            //   control: control,
+            //   options: control.options,
+            // }
+            // setTextEditorData(textData)
+          }
+        })
+      }
+    )
+  }, [events, subscribe, publish])
 
   const publicInterface: InspectorContext = {}
 
