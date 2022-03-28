fdb5daf614a8ff7a58d1e328ee1e3c442b14e2f8
Michael Sharpe <m.sharpe@project89.org>
2022-03-27 20:28:40 -0700

    Use a set timeout of 0 to let pubsub to finish processing

--- client/src/workspaces/contexts/ThothInterfaceProvider.tsx
+++ client/src/workspaces/contexts/ThothInterfaceProvider.tsx
@@ -119,7 +119,9 @@ const ThothInterfaceProvider = ({ children, tab }) => {
   const onPlaytest = callback => {
     return subscribe($PLAYTEST_INPUT(tab.id), (event, data) => {
       publish($PROCESS(tab.id))
-      callback(data)
+      // weird hack.  This staggers the process slightly to allow the published event to finish before the callback runs.
+      // No super elegant, but we need a better more centralised way to run the engine than these callbacks.
+      setTimeout(() => callback(data), 0)
     })
   }
 
