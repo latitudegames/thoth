560695212505d165080cf9ded4b579f2c415e153
Michael Sharpe <m.sharpe@project89.org>
2022-03-28 11:43:34 -0700

    Add save diff to spell API state

--- client/src/state/api/spells.ts
+++ client/src/state/api/spells.ts
@@ -22,6 +22,11 @@ export interface Spell {
   updatedAt?: number
 }
 
+export interface Diff {
+  name: string
+  diff: Record<string, unknown>
+}
+
 export interface DeployedSpellVersion {
   spellId: string
   version: string
@@ -59,6 +64,14 @@ export const spellApi = rootApi.injectEndpoints({
           url: `game/spells/${spellId}`,
         }
       },
+    }),
+    saveDiff: builder.mutation<void, Diff>({
+      invalidatesTags: ['Spells'],
+      query: diffData => ({
+        url: 'game/spells/saveDiff',
+        method: 'POST',
+        body: diffData,
+      }),
     }),
     saveSpell: builder.mutation<Partial<Spell>, Partial<Spell> | Spell>({
       invalidatesTags: ['Spell'],
@@ -144,6 +157,7 @@ export const {
   useNewSpellMutation,
   useDeleteSpellMutation,
   useSaveSpellMutation,
+  useSaveDiffMutation,
   useDeploySpellMutation,
   usePatchSpellMutation,
   useGetDeploymentsQuery,
