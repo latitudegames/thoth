5ba1b34944b9dc54d96479252489bae83708b9d7
Michael Sharpe <m.sharpe@project89.org>
2022-03-29 12:01:57 -0700

    Add write false back into spell select control

--- core/src/dataControls/SpellControl.ts
+++ core/src/dataControls/SpellControl.ts
@@ -1,11 +1,20 @@
 import { DataControl } from '../plugins/inspectorPlugin'
 
 export class SpellControl extends DataControl {
-  constructor({ name, icon = 'sieve' }: { name: string; icon?: string }) {
+  constructor({
+    name,
+    icon = 'sieve',
+    write = false,
+  }: {
+    name: string
+    icon?: string
+    write: boolean
+  }) {
     const options = {
       dataKey: 'spell',
       name: name,
       component: 'spellSelect',
+      write,
       icon,
     }
 
