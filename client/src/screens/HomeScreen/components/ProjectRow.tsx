94bfc2fb35ecf19e53185d2bb86020d5fd760c61
Michael Sharpe <m.sharpe@project89.org>
2022-03-29 16:21:53 -0700

    Fix project row spell type import

--- client/src/screens/HomeScreen/components/ProjectRow.tsx
+++ client/src/screens/HomeScreen/components/ProjectRow.tsx
@@ -1,4 +1,4 @@
-import { Spell } from '@/state/api/spells'
+import { Spell } from '@latitudegames/thoth-core/types'
 import { CSSProperties } from 'react'
 import Icon from '../../../components/Icon/Icon'
 import css from '../homeScreen.module.css'
