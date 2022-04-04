3cc5236dea3fb78c68b952222d5b565ef6f14d35
Michael Sharpe <m.sharpe@project89.org>
2022-04-04 00:04:51 -0700

    Add get spell docs to sharedb provider

--- client/src/contexts/SharedbProvider.tsx
+++ client/src/contexts/SharedbProvider.tsx
@@ -1,15 +1,26 @@
 import { sharedb, websocketUrl } from '@/config'
 import { useContext, createContext, useEffect, useState } from 'react'
+import { Doc } from 'sharedb'
 
 import ReconnectingWebSocket from 'reconnecting-websocket'
 import client from 'sharedb/lib/client'
 import { Socket } from 'sharedb/lib/sharedb'
+import { Spell } from '@latitudegames/thoth-core/types'
+import LoadingScreen from '@/components/LoadingScreen/LoadingScreen'
 
 const Connection = client.Connection
 
-const Context = createContext({})
+interface SharedbContext {
+  connection: client.Connection | null
+  socket: Socket | null
+  getSpellDoc: (spell: Spell) => Doc
+}
+
+const Context = createContext<SharedbContext>(undefined!)
 
 export const useSharedb = () => useContext(Context)
+
+export const docMap = new Map()
 
 // Might want to namespace these
 const SharedbProvider = ({ children }) => {
@@ -17,17 +28,41 @@ const SharedbProvider = ({ children }) => {
   const [connection, setConnection] = useState<client.Connection | null>(null)
 
   useEffect(() => {
-    console.log('CONNECTING TO SAHAREDB')
     const _socket = new ReconnectingWebSocket(websocketUrl)
     const _connection = new Connection(_socket as Socket)
     setConnection(_connection)
     setSocket(_socket as Socket)
   }, [])
 
-  const publicInterface = {
+  const getSpellDoc = (spell: Spell) => {
+    if (!connection) return
+    const key = spell.name
+    if (docMap.has(key)) return docMap.get(key)
+
+    const doc = connection.get('spell', key)
+    docMap.set(key, doc)
+
+    doc.subscribe(error => {
+      if (error) return console.error(error)
+
+      // If doc.type is undefined, the document has not been created, so let's create it
+      if (!doc.type) {
+        doc.create(spell, error => {
+          if (error) console.error(error)
+        })
+      }
+    })
+
+    return doc
+  }
+
+  const publicInterface: SharedbContext = {
     socket,
     connection,
+    getSpellDoc,
   }
+
+  if (!connection) return <LoadingScreen />
 
   return <Context.Provider value={publicInterface}>{children}</Context.Provider>
 }
