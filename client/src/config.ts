1a38e709d7e4a4defaf3cb5051c10e428203222d
Michael Sharpe <m.sharpe@project89.org>
2022-04-03 20:43:25 -0700

    Add sharedb env flag to config

--- client/src/config.ts
+++ client/src/config.ts
@@ -9,3 +9,5 @@ export const appRootUrl =
   process.env.NODE_ENV === 'production'
     ? process.env.REACT_APP_SITE_ROOT_URL_PROD
     : process.env.REACT_APP_SITE_ROOT_URL
+
+export const sharedb = process.env.REACT_APP_SHAREDB
