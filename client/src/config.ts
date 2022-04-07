c3f00683d169d5b99cd4eddc764939926247b070
Michael Sharpe <m.sharpe@project89.org>
2022-04-06 21:20:35 -0700

    Add websockets to config

--- client/src/config.ts
+++ client/src/config.ts
@@ -12,4 +12,5 @@ export const appRootUrl =
 
 // coercing this into a boolean
 export const sharedb = process.env.REACT_APP_SHAREDB === 'true'
+export const websockets = process.env.REACT_APP_WEBSOCKETS === 'true'
 export const websocketUrl = 'ws://localhost:8080'
