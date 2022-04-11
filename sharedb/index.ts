ac7564e904a6495c4a750f76f639461b3890cd40
Michael Sharpe <m.sharpe@project89.org>
2022-04-11 15:17:29 -0700

    Small update to sharedb file

--- sharedb/index.ts
+++ sharedb/index.ts
@@ -1,19 +1,30 @@
+import 'regenerator-runtime'
+import { SpellRunner } from '@latitudegames/thoth-core/dist/server'
 import express from 'express'
 import http from 'http'
 import WebSocket from 'ws'
 import ShareDB from 'sharedb'
 import WebSocketJSONStream from '@teamwork/websocket-json-stream'
+import { buildThothInterface } from './src/thothInterface'
 
 const app = express()
 const server = http.createServer(app)
-const webSocketServer = new WebSocket.Server({ server: server })
+const wss = new WebSocket.Server({ server: server })
 
 const backend = new ShareDB()
 
-webSocketServer.on('connection', webSocket => {
-  console.log('SHARE DB RUNNING!')
+let spellRunner
+
+wss.on('connection', webSocket => {
+  const thothInterface = buildThothInterface()
+  spellRunner = new SpellRunner({ thothInterface })
+
   const stream = new WebSocketJSONStream(webSocket)
   backend.listen(stream)
+
+  wss.on('message', data => {
+    console.log('message received', data)
+  })
 })
 
 server.listen(8080, () => {
