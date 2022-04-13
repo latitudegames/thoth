cfdf26eb861f5b6fde0d2a22a4066455f0da1ff4
parixitas <sakhiya.parixita@gmail.com>
2022-04-13 12:42:32 +0530

    Playtest Window scroll issue fix

--- client/src/workspaces/spells/windows/PlaytestWindow.jsx
+++ client/src/workspaces/spells/windows/PlaytestWindow.jsx
@@ -34,6 +34,7 @@ const Input = props => {
 }
 
 const Playtest = ({ tab }) => {
+  const scrollbars = useRef()
   const [history, setHistory] = useState([])
   const [value, setValue] = useState('')
 
@@ -49,7 +50,9 @@ const Playtest = ({ tab }) => {
     },
     [history]
   )
-
+  useEffect(()=>{
+    scrollbars.current.scrollToBottom();
+  }, [history])
   useEffect(() => {
     const unsubscribe = subscribe($PLAYTEST_PRINT(tab.id), printToConsole)
 
@@ -86,7 +89,7 @@ const Playtest = ({ tab }) => {
     <Window toolbar={toolbar}>
       <div style={{ display: 'flex', height: '100%', flexDirection: 'column' }}>
         <div className={css['playtest-output']}>
-          <Scrollbars>
+          <Scrollbars ref={ref => (scrollbars.current = ref)} >
             <ul>{history.map(printItem)}</ul>
           </Scrollbars>
         </div>
