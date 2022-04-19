0e7a3c7da5fb943259441ebf9efb8e790a853241
Michael Sharpe <m.sharpe@project89.org>
2022-04-19 09:56:19 -0700

    Update docusaurus github link in config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,38 +1,38 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Thoth",
-  tagline: "Spell composer",
-  url: "https://thoth.latitude.io",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "Latitude Inc.", // Usually your GitHub org/user name.
-  projectName: "Thoth", // Usually your repo name.
+  title: 'Thoth',
+  tagline: 'Spell composer',
+  url: 'https://thoth.latitude.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Latitude Inc.', // Usually your GitHub org/user name.
+  projectName: 'Thoth', // Usually your repo name.
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: "https://github.com/latitudegames/Thoth-Docs",
+          editUrl: 'https://github.com/latitudegames/thoth/tree/main/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl: "https://github.com/latitudegames/Thoth-Docs",
+          editUrl: 'https://github.com/latitudegames/thoth/tree/main/docs',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -42,57 +42,57 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "Home",
+        title: 'Home',
         logo: {
-          alt: "Thoth",
-          src: "img/thoth-logo.png",
+          alt: 'Thoth',
+          src: 'img/thoth-logo.png',
         },
         items: [
           {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Intro",
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Intro',
           },
           // { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/latitudegames/thoth",
-            label: "GitHub",
-            position: "right",
+            href: 'https://github.com/latitudegames/thoth',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "Tutorial",
-                to: "/docs/intro",
+                label: 'Tutorial',
+                to: '/docs/intro',
               },
             ],
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "Thoth Discord",
-                href: "https://discord.gg/MEqNmmzrtx",
+                label: 'Thoth Discord',
+                href: 'https://discord.gg/MEqNmmzrtx',
               },
             ],
           },
           {
-            title: "More",
+            title: 'More',
             items: [
               // {
               //   label: "Blog",
               //   to: "/blog",
               // },
               {
-                label: "GitHub",
-                href: "https://github.com/latitudegames/thoth",
+                label: 'GitHub',
+                href: 'https://github.com/latitudegames/thoth',
               },
             ],
           },
@@ -104,6 +104,6 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-};
+}
 
-module.exports = config;
+module.exports = config
