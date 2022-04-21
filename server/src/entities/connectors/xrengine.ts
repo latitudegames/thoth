b605d2b224eb823277772914737b3378b3a86f75
alextoti <alexandrostitonis@gmail.com>
2022-04-21 16:07:32 +0300

    fixed issues with voice and avatar

--- server/src/entities/connectors/xrengine.ts
+++ server/src/entities/connectors/xrengine.ts
@@ -15,6 +15,7 @@
 // We need to break some of this code out so that we have more control of it in the node graph
 // i.e. text classification and such
 
+import { tts } from '../../systems/googleTextToSpeech'
 import Xvfb from 'xvfb'
 import { database } from '../../database'
 import { browserWindow, PageUtils } from './browser'
@@ -403,6 +404,9 @@ export class xrengine_client {
   }
 
   async handleXREngineResponse(response, addPing, sender, isVoice) {
+    if (response === undefined || !response) {
+      return
+    }
     console.log('response: ' + response)
 
     if (
@@ -410,6 +414,24 @@ export class xrengine_client {
       (response as string).trim().toLowerCase().endsWith('.wav')
     ) {
       isVoice = true
+    }
+
+    if (!isVoice) {
+      await this.xrengineBot.sendMessage(response)
+      if (!(response as string).startsWith('/')) {
+        isVoice = true
+        const fileId = await tts(response as string)
+        const url =
+          (process.env.FILE_SERVER_URL?.endsWith('/')
+            ? process.env.FILE_SERVER_URL
+            : process.env.FILE_SERVER_URL + '/') + fileId
+
+        console.log('url:', url)
+        response = url
+        await this.xrengineBot.delay(1000)
+        await this.xrengineBot.sendMessage('!voiceUrl|' + response)
+      }
+      return
     }
 
     if (isVoice === false) {
@@ -484,17 +506,16 @@ export class xrengine_client {
     //   xr = undefined
     // }
     if (xr) {
-      console.log("***********agent", agent)
+      console.log('***********agent', agent)
       this.xrengineBot = new XREngineBot({
         headless: true,
         agent: agent,
         settings: settings,
         xrengineclient: this,
-        old: { bool: true, browser: xr.browser, page: xr.page, pu: xr.pu }
+        old: { bool: true, browser: xr.browser, page: xr.page, pu: xr.pu },
       })
       xr = undefined
-    }
-    else {
+    } else {
       this.xrengineBot = new XREngineBot({
         headless: true,
         agent: agent,
@@ -545,14 +566,6 @@ class XREngineBot {
   pu
   userId
   chatHistory = []
-  avatars = [
-    'Alissa',
-    'Cornelius',
-    'James_ReadyPlayerMe',
-    'Jamie',
-    'Mogrid',
-    'Warrior',
-  ]
   username_regex
   agent
   settings
@@ -566,7 +579,7 @@ class XREngineBot {
     agent,
     settings,
     xrengineclient,
-    old = { bool: false }
+    old = { bool: false },
   } = {}) {
     if (old.bool) {
       this.browser = old.browser
@@ -603,20 +616,24 @@ class XREngineBot {
       }
     }, 1000)
   }
-  async sendMessage(message: string, clean = false) {
+  async sendMessage(message, clean = false) {
     log('sending message: ' + message)
-    if (message === null || message === undefined) return
+    if (!message || message === undefined) return
+    // TODO:
+    // Send message to google cloud speech
+    // Get response URL from google cloud speech
 
-    if (this.queue.length > 0) {
-      this.queue.push({
-        delay: message.endsWith('.mp3') || message.endsWith('.wav') ? 5 : 1,
-        message,
-        clean,
-      })
-      return
-    } else {
-      await this.handleMessage(message, clean)
-    }
+    // await this.sendAudio(5)
+
+    this.evaluate(msg => {
+      try {
+        globalThis.sendMessage(msg)
+      } catch (e) {}
+    }, message)
+    /*console.log('typing message')
+    await this.typeMessage('newMessage', message, false)
+    console.log('sending message!')
+    await this.pressKey('Enter')*/
   }
   async handleMessage(message, clean = false) {
     await this.typeMessage('newMessage', message, clean)
@@ -889,7 +906,7 @@ class XREngineBot {
     await this.waitForTimeout(timeout)
   }
 
-  async interactObject() { }
+  async interactObject() {}
 
   /** Return screenshot
    * @param {Function} fn Function to execut _in the node context._
@@ -943,6 +960,7 @@ class XREngineBot {
     log('Launching browser')
     const options = {
       headless: true,
+      slowMo: 0,
       ignoreHTTPSErrors: true,
       args: [
         '--disable-gpu',
@@ -1185,8 +1203,10 @@ class XREngineBot {
 
       await this.updateUsername(name)
       await this.delay(10000)
-      const index = this.getRandomNumber(0, this.avatars.length - 1)
-      await this.updateAvatar(this.avatars[index])
+      await this.updateAvatar(
+        'CyberbotGreen',
+        'https://try.n3xus.city:8642/avatars/public/CyberbotGreen.glb'
+      )
       await this.requestPlayers()
       await this.getUser()
       await setInterval(() => this.getUser(), 1000)
@@ -1247,7 +1267,6 @@ class XREngineBot {
 
         return res
       } else {
-        console.log("Couldn't get chat state")
         return undefined
       }
     })
@@ -1262,7 +1281,22 @@ class XREngineBot {
     await this.clickElementById('SPAN', 'Profile_0')
   }
 
-  async updateAvatar(avatar) {
+  async updateAvatar(avatar: string, avatarUrl: string) {
+    setTimeout(
+      (avatar, avatarUrl) => {
+        console.log('updating avatar to:', avatar, avatarUrl)
+        this.evaluate(
+          (avatar, avatarUrl) => {
+            globalThis.setAvatar(avatar, avatarUrl)
+          },
+          avatar,
+          avatarUrl
+        )
+      },
+      10000,
+      avatar,
+      avatarUrl
+    )
     /*
     log(`updating avatar to: ${avatar}`)
     await this.clickElementById('SPAN', 'Profile_0')
@@ -1305,7 +1339,7 @@ class XREngineBot {
   async typeMessage(input, message, clean) {
     if (clean)
       await this.page.click(`input[name="${input}"]`, { clickCount: 3 })
-    await this.page.type(`input[name="${input}"]`, message)
+    await this.page.type(`input[name="${input}"]`, message, { delay: 50 })
   }
 
   async setFocus(selector) {
