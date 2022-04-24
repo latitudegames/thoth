d8a3cb35c6e6a8d72243c2caaf86eb001077436e
Jakob Grant <haruhunab1320@gmail.com>
2022-04-23 22:58:21 -0700

    import preferencesReducer

--- client/src/state/reducers.ts
+++ client/src/state/reducers.ts
@@ -2,6 +2,7 @@ import { combineReducers } from '@reduxjs/toolkit'
 
 import { spellApi } from './api/spells'
 import tabReducer from './tabs'
+import preferencesReducer from './preferences'
 
 export default combineReducers({
   tabs: tabReducer,
