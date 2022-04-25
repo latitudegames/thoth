94b52b1a4f9d115ad3026fc13b37cb6ffd1fb7ee
Jakob Grant <haruhunab1320@gmail.com>
2022-04-25 10:34:20 -0700

    remove duplicated modals folder

--- client/src/services/Modals.ts
+++
@@ -1,8 +0,0 @@
-import ExampleModal from '../components/Modals/ExampleModal'
-import InfoModal from '../components/Modals/InfoModal'
-
-const modals = { example: ExampleModal, infoModal: InfoModal }
-
-export const getModals = () => {
-  return modals
-}
