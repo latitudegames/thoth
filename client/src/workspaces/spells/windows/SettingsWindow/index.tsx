d2b475e5efd9405be15ebd2bc21a615b5d68c037
Jakob Grant <haruhunab1320@gmail.com>
2022-04-26 14:04:57 -0700

    update import paths in settingsWindow

--- client/src/workspaces/spells/windows/SettingsWindow/index.tsx
+++ client/src/workspaces/spells/windows/SettingsWindow/index.tsx
@@ -1,11 +1,11 @@
 import { RootState } from '@/state/store'
 import { useSelector, useDispatch } from 'react-redux'
-import Window from '../../../../components/Window/Window'
+import Window from '@/components/Window/Window'
 import {
   toggleDoNotShowUnlockWarning,
   toggleAutoSave,
 } from '@/state/preferences'
-import SwitchComponent from '../../../../components/Switch/Switch'
+import SwitchComponent from '@/components/Switch/Switch'
 
 const SettingsWindow = ({ tab }) => {
   const preferences = useSelector((state: RootState) => state.preferences)
