e73c4ec17de94880826d78ce5c77829f4c02298d
Jakob Grant <haruhunab1320@gmail.com>
2022-04-26 17:26:52 -0700

    open install function

--- core/src/plugins/errorPlugin/index.ts
+++ core/src/plugins/errorPlugin/index.ts
@@ -2,3 +2,8 @@ import { ThothComponent } from '@latitudegames/thoth-core/src/thoth-component'
 import { IRunContextEditor, NodeData } from '../../../types'
 import { ThothConsole } from '../debuggerPlugin/ThothConsole'
 
+function install(
+  engine: IRunContextEditor,
+  { server = false, throwError }: { server?: boolean; throwError?: Function }
+) {
+}
