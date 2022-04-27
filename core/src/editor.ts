bf3e8275e0813122a2c8c682b2901e7d5651b6ad
Jakob Grant <haruhunab1320@gmail.com>
2022-04-26 17:21:22 -0700

    add a ThothEngineClient interface

--- core/src/editor.ts
+++ core/src/editor.ts
@@ -24,6 +24,10 @@ import KeyCodePlugin from './plugins/keyCodePlugin'
 import ModulePlugin from './plugins/modulePlugin'
 import SelectionPlugin from './plugins/selectionPlugin'
 import errorPlugin from './plugins/errorPlugin'
+
+interface ThothEngineClient extends ThothEngine {
+  thoth: EditorContext
+}
 export class ThothEditor extends NodeEditor<EventsTypes> {
   tasks: Task[]
   pubSub: PubSubContext
