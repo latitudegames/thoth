968960cd3e2349c1f207ffa3eb9b968aa4401b27
Jakob Grant <haruhunab1320@gmail.com>
2022-04-29 13:23:58 -0700

    remove comment and add redux imports

--- client/src/workspaces/contexts/ThothInterfaceProvider.tsx
+++ client/src/workspaces/contexts/ThothInterfaceProvider.tsx
@@ -11,11 +11,8 @@ import { invokeInference } from '../../utils/huggingfaceHelper'
 import { usePubSub } from '../../contexts/PubSubProvider'
 import { useFetchFromImageCacheMutation } from '@/state/api/visualGenerationsApi'
 import { useGetSpellQuery, useRunSpellMutation } from '@/state/api/spells'
-
-/*
-Some notes here.  The new rete provider, not to be confused with the old rete provider renamed to the editor provider, is designed to serve as the single source of truth for interfacing with the rete internal system.  This unified interface will also allow us to replicate the same API in the server, where rete expects certain functions to exist but doesn't care what is behind these functions so long as they work.
-Not all functions will be needed on the server, and functions which are not will be labeled as such.
-*/
+import { RootState } from '@/state/store'
+import { useSelector } from 'react-redux'
 
 const Context = createContext<EditorContext>(undefined!)
 
