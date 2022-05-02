fbac0ce7b613e5e215564a8512222516234f8cf5
Michael Sharpe <m.sharpe@project89.org>
2022-05-02 14:00:25 -0700

    Init feathers app in runner package

---
+++ packages/runner/src/services/users/users.class.ts
@@ -0,0 +1,52 @@
+import { Id, NullableId, Paginated, Params, ServiceMethods } from '@feathersjs/feathers';
+import { Application } from '../../declarations';
+
+interface Data {}
+
+interface ServiceOptions {}
+
+export class Users implements ServiceMethods<Data> {
+  app: Application;
+  options: ServiceOptions;
+
+  constructor (options: ServiceOptions = {}, app: Application) {
+    this.options = options;
+    this.app = app;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async find (params?: Params): Promise<Data[] | Paginated<Data>> {
+    return [];
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async get (id: Id, params?: Params): Promise<Data> {
+    return {
+      id, text: `A new message with ID: ${id}!`
+    };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async create (data: Data, params?: Params): Promise<Data> {
+    if (Array.isArray(data)) {
+      return Promise.all(data.map(current => this.create(current, params)));
+    }
+
+    return data;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async update (id: NullableId, data: Data, params?: Params): Promise<Data> {
+    return data;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async patch (id: NullableId, data: Data, params?: Params): Promise<Data> {
+    return data;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async remove (id: NullableId, params?: Params): Promise<Data> {
+    return { id };
+  }
+}
