c9e57281605989270fde04d1991200751c015f19
Jakob Grant <haruhunab1320@gmail.com>
2022-05-03 19:56:48 -0700

    add ThothDemo to home page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -47,7 +47,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
@@ -55,8 +55,9 @@ export default function Home() {
     >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        {/* <HomepageFeatures /> */}
+        <ThothDemo />
       </main>
     </Layout>
-  );
+  )
 }
