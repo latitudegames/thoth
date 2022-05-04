de3ccc337ac92bb26ca530d5e2352b198d643bb0
Michael Sharpe <m.sharpe@project89.org>
2022-05-04 16:40:40 -0700

    Log headers in socket handler

--- packages/runner/src/sockets.ts
+++ packages/runner/src/sockets.ts
@@ -2,7 +2,8 @@ import io from 'socket.io'
 
 const handleSockets = (io: io.Server) => {
   io.on('connection', function (socket: io.Socket) {
-    console.log('connection established with socket', socket)
+    console.log('handshake headers', socket.handshake.headers)
+    // Authenticate with the auth headers here
   })
 }
 
