1a9b3ced6268c5bf662439a18768d215138ed8fe
Jakob Grant <haruhunab1320@gmail.com>
2022-05-03 19:53:05 -0700

    add import to docs index

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,10 +1,11 @@
-import React from "react";
-import clsx from "clsx";
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import styles from "./index.module.css";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import React from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import styles from './index.module.css'
+import HomepageFeatures from '@site/src/components/HomepageFeatures'
+import ThothDemo from '../components/HomepageFeatures/ThothDemo'
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
