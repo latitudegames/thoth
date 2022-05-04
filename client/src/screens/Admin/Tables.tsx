b53fb3b74323dad83cafa674377fcaee4120167a
kimenyi kevin <kimenyikevin@gmail.com>
2022-05-04 15:53:19 +0200

    add config data

--- client/src/screens/Admin/Tables.tsx
+++ client/src/screens/Admin/Tables.tsx
@@ -1,23 +1,27 @@
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
+import { styled } from '@mui/material/styles'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import Checkbox from '@mui/material/Checkbox'
+import data from '../../data/config/clientSettings'
+import Pagination from '@mui/material/Pagination'
+import MoreHoriz from '@mui/icons-material/MoreHoriz'
+import IconButton from '@mui/material/IconButton'
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "rgba(70, 70, 70, 0.95)",
+    backgroundColor: 'rgba(70, 70, 70, 0.95)',
     color: theme.palette.common.white,
     fontSize: 16,
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
   },
-}));
+}))
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
@@ -27,54 +31,72 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   'td, th': {
     border: 0,
   },
-}));
+}))
 
-function createData(
-  client: string,
-  name: string,
-  type: string,
-  defaultValue: string,
-) {
-  return { client, name, type, defaultValue };
-}
+// function createData(
+//   client: string,
+//   name: string,
+//   type: string,
+//   defaultValue: string
+// ) {
+//   return { client, name, type, defaultValue }
+// }
 
-const rows = [
-  createData( "discord", "discord_api_token", "string", ""),
-  createData( "twitter", "twitterConsumerKey", "string", ""),
-];
+const rows = data.slice(0, 10)
 
 export default function CustomizedTables() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="right">Select</StyledTableCell>
-            <StyledTableCell align='right'>Client</StyledTableCell>
-            <StyledTableCell align="right">Name</StyledTableCell>
-            <StyledTableCell align="right">Type</StyledTableCell>
-            <StyledTableCell align="right">Default Value</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="right">Select</StyledTableCell>
+              <StyledTableCell align="right">Client</StyledTableCell>
+              <StyledTableCell align="right">Name</StyledTableCell>
+              <StyledTableCell align="right">Type</StyledTableCell>
+              <StyledTableCell align="right">Default Value</StyledTableCell>
+              <StyledTableCell align="right">Action</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <StyledTableRow key={row.name}>
                 <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            inputProps={{
-              'aria-label': 'select all desserts',
-            }}
-          />
-        </TableCell>
-              <StyledTableCell align="right">{row.client}</StyledTableCell>
-              <StyledTableCell align="right">{row.name}</StyledTableCell>
-              <StyledTableCell align="right">{row.type}</StyledTableCell>
-              <StyledTableCell align="right">{row.defaultValue}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+                  <Checkbox
+                    color="primary"
+                    inputProps={{
+                      'aria-label': 'select all desserts',
+                    }}
+                  />
+                </TableCell>
+                <StyledTableCell align="right">{row.client}</StyledTableCell>
+                <StyledTableCell align="right">{row.name}</StyledTableCell>
+                <StyledTableCell align="right">{row.type}</StyledTableCell>
+                <StyledTableCell align="right">
+                  {row.defaultValue}
+                </StyledTableCell>
+                <TableCell padding="checkbox" align="center">
+                  <IconButton>
+                    <MoreHoriz />
+                  </IconButton>
+                </TableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: '1.5rem',
+        }}
+      >
+        <Pagination count={2} variant="outlined" shape="rounded" />
+      </div>
+    </div>
+  )
 }
--- client/src/screens/Admin/index.tsx
+++ client/src/screens/Admin/index.tsx
@@ -1,52 +1,53 @@
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import Tables from "../Admin/Tables";
-import FormControl from '@mui/material/FormControl';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import AppBar from '@mui/material/AppBar'
+import CssBaseline from '@mui/material/CssBaseline'
+import Toolbar from '@mui/material/Toolbar'
+import List from '@mui/material/List'
+import Typography from '@mui/material/Typography'
+import ListItem from '@mui/material/ListItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import InboxIcon from '@mui/icons-material/MoveToInbox'
+import MailIcon from '@mui/icons-material/Mail'
+import Tables from '../Admin/Tables'
+import FormControl from '@mui/material/FormControl'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import { styled } from '@mui/material/styles'
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
 const Container = styled(Grid)({
-     marginBottom: "1.5rem"
+  marginBottom: '1.5rem',
 })
 
 const ButtonCustom = styled(Button)({
-  background: "#424242",
-  color: "#fff",
-  border: "1px solid #636363",
+  background: '#424242',
+  color: '#fff',
+  border: '1px solid #636363',
   '&:hover': {
-    background: "#424242",
-  }
+    background: '#424242',
+  },
 })
 
 const OutlineButton = styled(Button)({
-  color: "#fff",
-  border: "2px solid #636363",
+  color: '#fff',
+  border: '2px solid #636363',
   '&:hover': {
-    border: "1px solid #636363",
-  }
+    border: '1px solid #636363',
+  },
 })
 
-
-
-const  AdminDashboard = () => {
+const AdminDashboard = () => {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: theme => theme.zIndex.drawer + 1 }}
+      >
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
             Thoth
@@ -58,7 +59,11 @@ const  AdminDashboard = () => {
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box', background: "rgba(70, 70, 70, 0.95)" },
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+            background: 'rgba(70, 70, 70, 0.95)',
+          },
         }}
       >
         <Toolbar />
@@ -78,41 +83,38 @@ const  AdminDashboard = () => {
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
         <Typography variant="h3" gutterBottom component="div">
-       Client Settings
-      </Typography>
+          Client Settings
+        </Typography>
         <Container container spacing={2}>
-  <Grid item xs={10}>
+          <Grid item xs={10}>
+            <Typography variant="h6" gutterBottom component="div">
+              These are all the client settings you have created
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <ButtonCustom variant="contained" size="medium" fullWidth>
+              Add Setting
+            </ButtonCustom>
+          </Grid>
+        </Container>
 
-      <Typography variant="h6" gutterBottom component="div">
-        These are all the client settings you have created 
-      </Typography>
-  </Grid>
-  <Grid item xs={2}>
-  <ButtonCustom variant="contained" size="medium" fullWidth>
-          Add Setting
-        </ButtonCustom>
-  </Grid>
-</Container>
+        <Container container spacing={2}>
+          <Grid item xs={11}>
+            <FormControl sx={{ width: '100%' }}>
+              <OutlinedInput placeholder="Search" />
+            </FormControl>
+          </Grid>
+          <Grid item xs={1}>
+            <OutlineButton variant="outlined" size="medium" fullWidth>
+              Filter
+            </OutlineButton>
+          </Grid>
+        </Container>
 
-
-<Container container spacing={2}>
-  <Grid item xs={11}>
-
-  <FormControl sx={{ width: '100%' }}>
-  <OutlinedInput placeholder="Search" />
-</FormControl>
-  </Grid>
-  <Grid item xs={1}>
-  <OutlineButton variant="outlined" size="medium" fullWidth>Filter</OutlineButton>
-  </Grid>
-</Container>
-
-
-
-          <Tables/>
+        <Tables />
       </Box>
     </Box>
-  );
+  )
 }
 
-export default AdminDashboard;
+export default AdminDashboard
