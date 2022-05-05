4600b85445257028b3839795be3400982e953efa
Michael Sharpe <m.sharpe@project89.org>
2022-05-05 14:36:50 -0700

    Fix exports from server ts in core

--- packages/core/server.ts
+++ packages/core/server.ts
@@ -2,14 +2,12 @@ import { getComponents, components } from './src/components/components'
 import { initSharedEngine } from './src/engine'
 import { Task } from './src/plugins/taskPlugin/task'
 import { ThothComponent } from './src/thoth-component'
-import SpellRunner from './src/spellManager/SpellRunner'
-import SpellManager from './src/spellManager/SpellManager'
 
 export { getComponents } from './src/components/components'
 export { Task } from './src/plugins/taskPlugin/task'
 export { initSharedEngine }
-export { SpellRunner }
-export { SpellManager }
+
+export * from './src/spellManager'
 export * from './src/utils/chainHelpers'
 
 export default {
@@ -18,5 +16,4 @@ export default {
   initSharedEngine,
   Task,
   ThothComponent,
-  SpellRunner,
 }
