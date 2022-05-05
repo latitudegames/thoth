926ef8b9d521f87a24937604890746a7028cc110
Michael Sharpe <m.sharpe@project89.org>
2022-05-05 09:23:01 -0700

    Export spell manager from core server package

--- packages/core/server.ts
+++ packages/core/server.ts
@@ -3,11 +3,13 @@ import { initSharedEngine } from './src/engine'
 import { Task } from './src/plugins/taskPlugin/task'
 import { ThothComponent } from './src/thoth-component'
 import SpellRunner from './src/spellManager/SpellRunner'
+import SpellManager from './src/spellManager/SpellManager'
 
 export { getComponents } from './src/components/components'
 export { Task } from './src/plugins/taskPlugin/task'
 export { initSharedEngine }
 export { SpellRunner }
+export { SpellManager }
 export * from './src/utils/chainHelpers'
 
 export default {
