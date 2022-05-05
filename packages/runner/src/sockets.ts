9be6933cab1bcba2a024f542338d1bd40ff580b2
Michael Sharpe <m.sharpe@project89.org>
2022-05-05 09:23:34 -0700

    Pull in spell manager to handle sockets in runner

--- packages/runner/src/sockets.ts
+++ packages/runner/src/sockets.ts
@@ -1,9 +1,13 @@
 import io from 'socket.io'
+import { SpellManager } from '@latitudegames/thoth-core/dist/server'
+
+const managerMap = {}
 
 const handleSockets = (io: io.Server) => {
   io.on('connection', function (socket: io.Socket) {
     console.log('handshake headers', socket.handshake.headers)
     // Authenticate with the auth headers here
+    const spellManager = new SpellManager()
   })
 }
 
