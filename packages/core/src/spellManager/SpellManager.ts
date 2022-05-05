213340af492f449500fa7d67ca5375f0cf05b843
Michael Sharpe <m.sharpe@project89.org>
2022-05-05 14:39:30 -0700

    Pass socket in to spell manager

--- packages/core/src/spellManager/SpellManager.ts
+++ packages/core/src/spellManager/SpellManager.ts
@@ -1,5 +1,11 @@
+import io from 'socket.io'
 import SpellRunner from './SpellRunner'
 
 export default class SpellManager {
   spellRunnerMap: Map<number, SpellRunner> = new Map()
+  socket: io.Socket
+
+  constructor(socket: io.Socket) {
+    this.socket = socket
+  }
 }
