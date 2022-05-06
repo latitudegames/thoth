581c3e08d80ac59e56124d49b03b1471b55c8010
Jakob Grant <haruhunab1320@gmail.com>
2022-05-06 15:20:54 -0700

    define inrange component with constructor

--- packages/core/src/components/InRange.ts
+++ packages/core/src/components/InRange.ts
@@ -6,3 +6,14 @@ import { ThothComponent } from '../thoth-component'
 
 const info = `The In Range component takes either a manually input set of numbers or a dynamically generated set of numbers as a boundary. When supplied with a value to test its existance between the set range, will trigger 1 of 2 outputs. If the number exists within the range including the start and end number, will trigger the true output else will trigger the false output.`
 
+export class InRange extends ThothComponent<void> {
+  constructor() {
+    super('In Range')
+
+    this.task = {
+      outputs: { true: 'option', false: 'option' },
+    }
+    this.category = 'Logic'
+    this.info = info
+  }
+}
