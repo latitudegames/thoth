286a6befcb9025670fb04331b8585b1dd33b33cb
Jakob Grant <haruhunab1320@gmail.com>
2022-05-06 15:20:20 -0700

    add info to the inRange component

--- packages/core/src/components/InRange.ts
+++ packages/core/src/components/InRange.ts
@@ -4,3 +4,5 @@ import { NodeData, ThothNode, ThothWorkerInputs } from '../../types'
 import { triggerSocket, numSocket } from '../sockets'
 import { ThothComponent } from '../thoth-component'
 
+const info = `The In Range component takes either a manually input set of numbers or a dynamically generated set of numbers as a boundary. When supplied with a value to test its existance between the set range, will trigger 1 of 2 outputs. If the number exists within the range including the start and end number, will trigger the true output else will trigger the false output.`
+
