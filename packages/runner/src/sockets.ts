88878ca9f5ae8e890dabb0daa0018cbbf94057fb
Michael Sharpe <m.sharpe@project89.org>
2022-05-06 13:59:53 -0700

    Add console log for handshake

--- packages/runner/src/sockets.ts
+++ packages/runner/src/sockets.ts
@@ -26,6 +26,7 @@ const handleSockets = (app: any) => {
   return (io: any) => {
     // Another gross 'any' here
     io.on('connection', async function (socket: any) {
+      console.log('CONNECTION ESTABLISHED')
       const sessionId = socket.handshake.headers.authorization.split(' ')[1]
 
       if (!sessionId) throw new Error('No session id provided for handshake')
