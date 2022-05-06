32178f199d858af518abc0257ef999e5d66d1724
Jakob Grant <haruhunab1320@gmail.com>
2022-05-06 15:20:03 -0700

    add inputs to inRange component

---
+++ packages/core/src/components/InRange.ts
@@ -0,0 +1,6 @@
+import Rete from 'rete'
+
+import { NodeData, ThothNode, ThothWorkerInputs } from '../../types'
+import { triggerSocket, numSocket } from '../sockets'
+import { ThothComponent } from '../thoth-component'
+
