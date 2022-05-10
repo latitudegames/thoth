0f7903988d4c033c3991aab90e4e8ea9e2e8bb41
kimenyi kevin <kimenyikevin@gmail.com>
2022-05-10 12:30:05 +0200

    uncomment navBar

--- client/src/components/ThothPage/ThothPageWrapper.tsx
+++ client/src/components/ThothPage/ThothPageWrapper.tsx
@@ -1,12 +1,12 @@
 import ModalProvider from '../../contexts/ModalProvider'
-// import TabBar from '../TabBar/TabBar'
+import TabBar from '../TabBar/TabBar'
 import css from './pagewrapper.module.css'
 
 const ThothPageWrapper = ({ tabs, activeTab, ...props }) => {
   return (
     <ModalProvider>
       <div className={css['wrapper']}>
-        {/* <TabBar tabs={tabs} activeTab={activeTab} /> */}
+        <TabBar tabs={tabs} activeTab={activeTab} />
         {props.children}
       </div>
     </ModalProvider>
