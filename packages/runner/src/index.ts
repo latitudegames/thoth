a1db72243c22357df1df7dcfa3877b2a01085316
Michael Sharpe <m.sharpe@project89.org>
2022-05-11 13:58:58 -0700

    Import env to config

--- packages/runner/src/index.ts
+++ packages/runner/src/index.ts
@@ -1,13 +1,18 @@
-import logger from './logger';
-import app from './app';
+import 'dotenv/config'
+import logger from './logger'
+import app from './app'
 
-const port = app.get('port');
-const server = app.listen(port);
+const port = app.get('port')
+const server = app.listen(port)
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
+)
 
 server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+  logger.info(
+    'Feathers application started on http://%s:%d',
+    app.get('host'),
+    port
+  )
+)
