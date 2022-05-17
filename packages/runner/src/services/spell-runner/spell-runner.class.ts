486addd32b220d9501ac61d5153535af9c41d493
Sean Gillespie <info@spg.business>
2022-05-16 21:20:30 -0400

    Add intial logic to update method of spell runner class

--- packages/runner/src/services/spell-runner/spell-runner.class.ts
+++ packages/runner/src/services/spell-runner/spell-runner.class.ts
@@ -1,3 +1,4 @@
+import otJson0 from 'ot-json0'
 import {
   Id,
   NullableId,
@@ -84,15 +85,32 @@ export class SpellRunner implements ServiceMethods<Data> {
     return result || {}
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async update(id: NullableId, data: Data, params?: Params): Promise<Data> {
+  async update(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    spellId: string,
+    data: { diff: Record<string, unknown> },
+    params?: Params
+  ): Promise<Data> {
     if (!this.app.userSpellManagers) return {}
     if (!params) throw new Error('No params present in service')
 
     const { user } = params
 
-    if (!user) throw new Error('No user is present in service')
-    return data
+    if (!user) throw new Error('No user present in service')
+
+    const { diff } = data
+
+    const spellManager = this.app.userSpellManagers.get(user.id)
+    if (!spellManager) throw new Error('No spell manager found for user!')
+
+    const spellRunner = spellManager.getSpellRunner(spellId)
+    if (!spellRunner) throw new Error('No spell runner found!')
+
+    const spell = spellRunner.currentSpell
+    const updatedSpell = otJson0.type.apply(spell, diff)
+
+    spellManager.load(updatedSpell, true)
+    return updatedSpell
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
