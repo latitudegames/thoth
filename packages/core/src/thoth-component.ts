03f1eea1f010991a2a568180bb31e525121b7109
Sean Gillespie <info@spg.business>
2022-05-26 19:38:02 -0400

    Update ThothEditor import for thoth-component.ts

--- packages/core/src/thoth-component.ts
+++ packages/core/src/thoth-component.ts
@@ -1,6 +1,6 @@
 import { Node, Socket } from 'rete'
-
-import { PubSubBase, ThothEditor, ThothNode } from '../types'
+import { ThothEditor } from './editor'
+import { PubSubBase, ThothNode } from '../types'
 import { ThothEngineComponent } from './engine'
 import { Task, TaskOptions } from './plugins/taskPlugin/task'
 
