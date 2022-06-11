be08d6f94682dd4499e72ae45604e29214df537f
Sean Gillespie <info@spg.business>
2022-06-11 00:22:24 -0400

    Bump ts-loader to 9.3.0 in client devDeps and revert resolveTypeReferenceDirective workaround

--- packages/core/webpack.common.js
+++ packages/core/webpack.common.js
@@ -43,29 +43,6 @@ module.exports = () => {
             compilerOptions: {
               outDir: './dist',
             },
-            resolveTypeReferenceDirective: function (
-              directive,
-              containingFile,
-              compilerOptions,
-              moduleResolutionHost,
-              original
-            ) {
-              return directive === 'jest'
-                ? {
-                    resolvedTypeReferenceDirective: {
-                      resolvedFileName: path.join(
-                        __dirname,
-                        'node_modules/@types/jest/index.d.ts'
-                      ),
-                    },
-                  }
-                : original(
-                    directive,
-                    containingFile,
-                    compilerOptions,
-                    moduleResolutionHost
-                  )
-            },
           },
         },
         {
